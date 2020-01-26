56c57454aa8ef1c25c3df310e30d7969d015e817
Gregor Kaljulaid <gregor.kaljulaid@gmail.com>
2020-01-26 18:17:34 +0200

    Removed old versions.

--- old/tornTools-edge/scripts/shortcuts.js
+++
@@ -1,7 +0,0 @@
-window.addEventListener('keydown', function(event) {
-  if ('key' in event) {
-    console.log('KeyboardEvent.key:', event.key);
-  } else {
-    console.setStatus('KeyboardEvent.key is not supported.');
-  }
-});
