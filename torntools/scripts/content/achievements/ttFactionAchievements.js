8f256212572b8d8b8a99ab67e5fd24678dcf9537
Gregor Kaljulaid <gregor.kaljulaid@gmail.com>
2020-03-21 00:20:01 +0200

    Add time incrementer to achievement pages

--- torntools/scripts/content/achievements/ttFactionAchievements.js
+++ torntools/scripts/content/achievements/ttFactionAchievements.js
@@ -35,6 +35,14 @@ window.addEventListener('load', (event) => {
         }
 
         displayAchievements(achievements, show_completed, honors, medals, date);
+
+        let time_increase = setInterval(function(){
+            let seconds = parseInt(document.querySelector("#tt-awards-time").getAttribute("seconds"));
+            let new_time = time_ago(new Date() - (seconds+1)*1000);
+
+            document.querySelector("#tt-awards-time").innerText = new_time;
+            document.querySelector("#tt-awards-time").setAttribute("seconds", seconds+1);
+        }, 1000);
     });
 });
 
@@ -102,6 +110,8 @@ function displayAchievements(achievements, show_completed, honors, medals, date)
                         tt_header.innerText = "TT - Awards";
     
                         let tt_header_time = document.createElement("span");
+                        tt_header_time.id = "tt-awards-time";
+                        tt_header_time.setAttribute("seconds", (new Date() - Date.parse(date))/1000);
                         tt_header_time.setAttribute("style", `
                             font-size: 10px;
                             color: orange;
@@ -237,60 +247,7 @@ function displayAchievements(achievements, show_completed, honors, medals, date)
             }
         }
     }
-    function time_ago(time) {
-
-        switch (typeof time) {
-            case 'number':
-                break;
-            case 'string':
-                time = +new Date(time);
-                break;
-            case 'object':
-                if (time.constructor === Date) time = time.getTime();
-                break;
-            default:
-                time = +new Date();
-        }
-        var time_formats = [
-            [60, 'seconds', 1], // 60
-            [120, '1 minute ago', '1 minute from now'], // 60*2
-            [3600, 'minutes', 60], // 60*60, 60
-            [7200, '1 hour ago', '1 hour from now'], // 60*60*2
-            [86400, 'hours', 3600], // 60*60*24, 60*60
-            [172800, 'Yesterday', 'Tomorrow'], // 60*60*24*2
-            [604800, 'days', 86400], // 60*60*24*7, 60*60*24
-            [1209600, 'Last week', 'Next week'], // 60*60*24*7*4*2
-            [2419200, 'weeks', 604800], // 60*60*24*7*4, 60*60*24*7
-            [4838400, 'Last month', 'Next month'], // 60*60*24*7*4*2
-            [29030400, 'months', 2419200], // 60*60*24*7*4*12, 60*60*24*7*4
-            [58060800, 'Last year', 'Next year'], // 60*60*24*7*4*12*2
-            [2903040000, 'years', 29030400], // 60*60*24*7*4*12*100, 60*60*24*7*4*12
-            [5806080000, 'Last century', 'Next century'], // 60*60*24*7*4*12*100*2
-            [58060800000, 'centuries', 2903040000] // 60*60*24*7*4*12*100*20, 60*60*24*7*4*12*100
-        ];
-        var seconds = (+new Date() - time) / 1000,
-            token = 'ago',
-            list_choice = 1;
     
-        if (seconds == 0) {
-            return 'Just now'
-        }
-        if (seconds < 0) {
-            seconds = Math.abs(seconds);
-            token = 'from now';
-            list_choice = 2;
-        }
-        var i = 0,
-            format;
-        while (format = time_formats[i++])
-            if (seconds < format[0]) {
-                if (typeof format[2] == 'string')
-                    return format[list_choice];
-                else
-                    return Math.floor(seconds / format[2]) + ' ' + format[1] + ' ' + token;
-            }
-        return time;
-    }
     function getStatus(){
         let hdr = document.querySelector("h2.header___30pTh");
         
@@ -326,4 +283,59 @@ function flying() {
 
 function hours(x) {
 	return Math.floor(x / 60 / 60); // seconds, minutes
+}
+
+function time_ago(time) {
+
+    switch (typeof time) {
+        case 'number':
+            break;
+        case 'string':
+            time = +new Date(time);
+            break;
+        case 'object':
+            if (time.constructor === Date) time = time.getTime();
+            break;
+        default:
+            time = +new Date();
+    }
+    var time_formats = [
+        [60, 'seconds', 1], // 60
+        [120, '1 minute ago', '1 minute from now'], // 60*2
+        [3600, 'minutes', 60], // 60*60, 60
+        [7200, '1 hour ago', '1 hour from now'], // 60*60*2
+        [86400, 'hours', 3600], // 60*60*24, 60*60
+        [172800, 'Yesterday', 'Tomorrow'], // 60*60*24*2
+        [604800, 'days', 86400], // 60*60*24*7, 60*60*24
+        [1209600, 'Last week', 'Next week'], // 60*60*24*7*4*2
+        [2419200, 'weeks', 604800], // 60*60*24*7*4, 60*60*24*7
+        [4838400, 'Last month', 'Next month'], // 60*60*24*7*4*2
+        [29030400, 'months', 2419200], // 60*60*24*7*4*12, 60*60*24*7*4
+        [58060800, 'Last year', 'Next year'], // 60*60*24*7*4*12*2
+        [2903040000, 'years', 29030400], // 60*60*24*7*4*12*100, 60*60*24*7*4*12
+        [5806080000, 'Last century', 'Next century'], // 60*60*24*7*4*12*100*2
+        [58060800000, 'centuries', 2903040000] // 60*60*24*7*4*12*100*20, 60*60*24*7*4*12*100
+    ];
+    var seconds = (+new Date() - time) / 1000,
+        token = 'ago',
+        list_choice = 1;
+
+    if (seconds == 0) {
+        return 'Just now'
+    }
+    if (seconds < 0) {
+        seconds = Math.abs(seconds);
+        token = 'from now';
+        list_choice = 2;
+    }
+    var i = 0,
+        format;
+    while (format = time_formats[i++])
+        if (seconds < format[0]) {
+            if (typeof format[2] == 'string')
+                return format[list_choice];
+            else
+                return Math.floor(seconds / format[2]) + ' ' + format[1] + ' ' + token;
+        }
+    return time;
 }
