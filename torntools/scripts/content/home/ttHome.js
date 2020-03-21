39c9ec12545903bf75227529de20aa5df234b97e
Gregor Kaljulaid <gregor.kaljulaid@gmail.com>
2020-03-21 12:04:36 +0200

    Display networth change details on home.php

--- torntools/scripts/content/home/ttHome.js
+++ torntools/scripts/content/home/ttHome.js
@@ -4,25 +4,27 @@ window.addEventListener('load', (event) => {
     if(flying())
         return
 
-    chrome.storage.local.get(["settings", "userdata", "torndata"], function(data) {
-        const settings = data.settings;
-        const show_networth = settings.pages.home.networth;
+    chrome.storage.local.get(["settings", "userdata", "torndata", "networth"], function(data) {
+		const settings = data.settings;
+		const networth = data.networth;
+		const show_networth = settings.pages.home.networth;
+		
+		console.log(networth);
 
         if(!show_networth)
             return
 
         let user_networth = data.userdata.networth.total;
-        displayNetworth(parseInt(user_networth));
-    
+		displayNetworth(parseInt(user_networth));
+		displayNetworthChange(networth);
     });
 });
 
 function displayNetworth(user_networth){
-
-    // find Networth slot in General Information
-	const gen_info_box = document.querySelector("#item4741013");
-	const inner_box = gen_info_box.children[1].children[0].children[0];
-	const last_item = inner_box.children[inner_box.children.length-1];
+	// find Networth slot in General Information
+	let gen_info_box = getGenInfoBox();
+	let inner_box = gen_info_box.children[1].children[0].children[0];
+	let last_item = inner_box.children[inner_box.children.length-1];
 
     // remove class "last" from last element
 	last_item.removeAttribute("class");
@@ -54,6 +56,61 @@ function displayNetworth(user_networth){
 	inner_box.appendChild(li);
 }
 
+function displayNetworthChange(networth){
+	if(!networth)
+		return;
+
+	let headings = ["Type", "Value", "Change"];
+	let types = ["Points", "Vault", "Items", "Bazaar", "Properties", "Stock Market", "Company", "Bookie"];
+	
+	let li = document.createElement("li");
+	let table = document.createElement("table");
+	let tr_h = document.createElement("tr");
+
+	table.style.width = "100%";
+	li.style.padding = "3px";
+	tr_h.style.paddingBottom = "5px";
+
+	for(let heading of headings){
+		let td = document.createElement("td");
+		td.style.fontWeight = "600";
+		td.style.paddingBottom = "5px";
+		td.innerText = heading;
+		tr_h.appendChild(td);
+	}
+
+	table.appendChild(tr_h);
+
+	for(let type of types){
+		let tr = document.createElement("tr");
+		for(let heading of headings){
+			let td = document.createElement("td");
+			td.style.paddingBottom = "2px";
+			let value = networth.current.value[type.replace(" ", "").toLowerCase()];
+			let change = 0;
+
+			if(networth.previous.value && Object.keys(networth.previous.value).length != 0)
+				change = value - (networth.previous.value[type.replace(" ", "").toLowerCase()]);
+
+			if(heading == "Type")
+				td.innerText = type;
+			else if(heading == "Value")
+				td.innerText = "$" + String(numberWithCommas(value));
+			else if(heading == "Change")
+				td.innerText = "$" + String(numberWithCommas(change));
+			tr.appendChild(td);
+		}
+		table.appendChild(tr);
+	}
+
+	li.appendChild(table);
+	
+	let gen_info_box = getGenInfoBox();
+	let inner_box = gen_info_box.children[1].children[0].children[0];
+	inner_box.appendChild(li);
+	
+}
+
 function flying() {
 	try {	
 		if(document.querySelector("#skip-to-content").innerText === "Traveling"){
@@ -66,4 +123,14 @@ function flying() {
 
 function numberWithCommas(x) {
 	return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+}
+
+function getGenInfoBox(){
+	let headings = document.querySelectorAll("h5");
+	let gen_info_box;
+	for(let heading of headings){
+		if(heading.innerText == "General Information")
+			gen_info_box = heading.parentElement.parentElement;
+	}
+	return gen_info_box;
 }
