8cb88b14fefa72f91e0108f95fa404b8cc11db3e
Gregor Kaljulaid <gregor.kaljulaid@gmail.com>
2020-04-12 01:03:51 +0300

    Add Creator message on Mephiles' profile

--- torntools/scripts/content/profile/ttProfile.js
+++ torntools/scripts/content/profile/ttProfile.js
@@ -9,12 +9,19 @@ window.addEventListener('load', (event) => {
         const show_profile = settings.pages.profile.show;
         let allies = data.allies;
 
-        if(!show_profile)
-            return
+        profileLoaded().then(function(loaded){
+            if(!loaded)
+                return;
 
-        setTimeout(function(){
+            displayCreator();
+
+            if(!show_profile)
+                return
+
             let user_faction = data.userdata.faction.faction_name;
             let profile_faction = document.querySelector(".basic-information ul.basic-list li:nth-of-type(3) div:nth-of-type(2)").innerText;
+
+            console.log(allies);
     
             if(user_faction == profile_faction){
                 showWarning('user');
@@ -27,9 +34,45 @@ window.addEventListener('load', (event) => {
                     return;
                 }
             }
-        }, 1000);
+        });
     });
 });
+
+function displayCreator(){
+    let name = document.querySelector("#skip-to-content");
+
+    if(name.innerText == "Mephiles' Profile"){
+        let span1 = document.createElement("span");
+        span1.innerText = " - Thanks for using ";
+        span1.style.fontSize = "17px";
+        span1.style.color = "#888888";
+
+        let span2 = document.createElement("span");
+        span2.innerText = "TornTools";
+        span2.style.color = "#39a539";
+
+        span1.appendChild(span2);
+        name.appendChild(span1);
+    }
+}
+
+function profileLoaded(){
+    let promise = new Promise(function(resolve, reject){
+        let counter = 0;
+        setInterval(function(){
+            if(document.querySelector(".basic-information"))
+                return resolve(true);
+            else if(counter > 1000)
+                return resolve(false);
+            else
+                counter+=1;
+        }, 100);
+    });
+
+    return promise.then(function(data){
+        return data;
+    });
+}
 
 function showWarning(type){
     let title = document.querySelector(".profile-left-wrapper .title-black");
