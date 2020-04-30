d05e1a1733f2a2620a0fa1a9f82efd4ff3a4f998
Gregor Kaljulaid <gregor.kaljulaid@gmail.com>
2020-04-30 21:40:12 +0300

    Recode ttTrade.js

--- torntools/scripts/content/trade/ttTrade.js
+++ torntools/scripts/content/trade/ttTrade.js
@@ -1,169 +1,102 @@
-window.addEventListener('load', (event) => {
-    console.log("TT - Trade");
+window.addEventListener('load', async (event) => {
+	console.log("TT - Trade");
+	
+	if(await flying())
+		return;
 
-    chrome.storage.local.get(["settings", "itemlist"], function(data) {
-        const settings = data.settings;
-        const itemlist = data.itemlist.items;
-        const show_trade = settings.pages.trade.calculator;
-
-        if(!show_trade)
+    local_storage.get(["settings", "itemlist"], function([settings, itemlist]) {
+        if(!settings.pages.trade.calculator)
             return;
 
-        let done = false;
-        // trade site checker
-        let checker = setInterval(function(){
-            if(tradeView()){
-				if(!done){
-					createWindow(itemlist);
-					done = true;
-				}
-            } else {
-                done = false;
-            }
-        }, 1000);
-    });
+		let looking = true;
+		let checker = setInterval(function(){
+			if(tradeView() && looking){
+				looking = false
+				setTimeout(Main, 1000);
+			} else if(!tradeView())
+				looking = true;
+		}, 500);
 
+        function Main(){
+			console.log("Trade view!");
+
+			// Show values of adds
+			let logs = doc.findAll(".log li div");
+			for(let log of logs){
+				let text = log.innerText;
+				let total_value = 0;
+
+				if(text.indexOf("added") > -1){
+					text = text.replace(" added", "").replace(" to the trade", "").replace(log.find("a").innerText+" ", "");
+					let items = text.split(",");
+
+					for(let item of items){
+						let name = item.split(" x")[0].trim();
+						let quantity = parseInt(item.split(" x")[1]);
+
+						for(let id in itemlist.items){
+							if(itemlist.items[id].name == name){
+								for(let i = 0; i < quantity; i++){
+									total_value += itemlist.items[id].market_value;
+								}
+							}
+						}
+					}
+
+					let value_span = doc.new("span");
+						value_span.setClass("tt-add-value");
+						value_span.innerText = `$${numberWithCommas(total_value, shorten=false)}`;
+					
+					log.appendChild(value_span);
+				}
+			}
+
+			// Show values of sides
+			for(let side of [doc.find(".user.left .cont .color2 .desc"), doc.find(".user.right .cont .color2 .desc")]){ // color2 - items
+				let LIs = side.findAll("li .name");
+				let total_value = 0;
+
+				for(let li of LIs){
+					let name = li.innerText.split(" x")[0].trim();
+					let quantity = li.innerText.split(" x")[1] || 1;
+
+					for(let id in itemlist.items){
+						if(itemlist.items[id].name == name){
+							let item_value = itemlist.items[id].market_value;
+							console.log(name, item_value)
+							total_value += item_value;
+
+							let span = doc.new("span");
+								span.setClass("tt-side-item-value");
+								span.innerText = `$${numberWithCommas((item_value * quantity), shorten=false)}`;
+							li.appendChild(span);
+						}
+					}
+
+				}
+
+				if(!total_value == 0){
+					let div = doc.new("div");
+						div.setClass("tt-side-value");
+						div.innerText = `Items value: `;
+					let span = doc.new("span");
+						span.innerText = `$${numberWithCommas(total_value, shorten=false)}`;
+
+					div.appendChild(span);
+					side.parentElement.parentElement.parentElement.appendChild(div);
+				}
+			}
+        }
+    });
 });
 
-function createWindow(itemlist){
-	const leftSide = document.querySelector(".trade-cont .user.left");
-	const rightSide = document.querySelector(".trade-cont .user.right");
-    const adds = document.querySelectorAll(".log li div");
-    
-    const leftItems = getLeftItemIDs(itemlist);
-    const rightItems = getRightItemIDs(itemlist);
-    
-    const leftsidevalue = getItemsValue(leftItems, itemlist);
-    displayValue(leftSide, leftsidevalue);
-
-    const rightsidevalue = getItemsValue(rightItems, itemlist);
-    displayValue(rightSide, rightsidevalue);
-
-    // show values for individual adds
-    for (let add of adds) {
-        let text = add.innerText;
-        if (text.indexOf("added") > -1) {
-
-            // remove unwanted parts
-            text = text.replace(/ added/g, "")
-            text = text.replace(/ to the trade./g, "")
-            text = text.replace(text.split(" ")[0] + " ", "")
-            var items = text.split(", ");
-
-            var resultItems = {}
-
-            for (let item of items) {
-                let name = item.split(" x")[0];
-                let q = item.split(" x")[1];
-                let id;
-
-                for (let i in itemlist) {
-                    if (itemlist[i]["name"] === name) {
-                        resultItems[i] = q;
-                    }
-                }
-            }
-
-            if (Object.keys(resultItems).length) {
-                var addValue = getItemsValue(resultItems, itemlist);
-                displayLogValue(add, addValue);
-            }
-        }
-    }
-}
-
-function getLeftItemIDs(itemlist) {
-	var result = {};
-
-	let items = document.querySelectorAll(".user.left .cont .color2 .desc li .name.left");
-
-	if (items[0].innerText !== "No items in trade") {
-		for (let item of items) {
-			let parts = item.innerText.split(" x");
-			let name = parts[0].trim();
-			let q = parts[1];
-			if (!q) {
-				q = "1"
-			}
-
-			for (let id in itemlist) {
-				if (itemlist[id]["name"] === name) {
-					result[id] = q;
-				}
-			}
-		}
-		return result;
-	}
-	return "none";
-}
-
-function getRightItemIDs(itemlist) {
-	var result = {};
-
-	let items = document.querySelectorAll(".user.right .cont .color2 .desc li .name.left");
-	if (items.length === 0) {
-		return "none"
-	}
-
-	for (let item of items) {
-		let parts = item.innerText.split(" x");
-		let name = parts[0];
-		let q = parts[1];
-
-		for (let id in itemlist) {
-			if (itemlist[id]["name"] === name) {
-				result[id] = q;
-			}
-		}
-	}
-	return result;
-}
-
-function displayValue(side, x) {
-	let div = document.createElement("div");
-	let span = document.createElement("span");
-
-	div.style.height = "30px";
-	div.style.lineHeight = "30px";
-	div.style.border = "0.5px solid black";
-	div.style.fontWeight = "600";
-	div.style.paddingLeft = "15px";
-	div.innerText = "Items value: "
-
-	span.style.color = "#678c00",
-		span.style.fontWeight = "400";
-	span.innerText = "$" + String(numberWithCommas(x))
-
-	div.appendChild(span);
-	side.appendChild(div);
-}
-
-function getItemsValue(items, itemlist) {
-	if (items === "none") {
-		return 0;
-	}
-	var value = 0;
-
-	for (let id in items) {
-		value += parseInt(items[id]) * parseInt(itemlist[id]["market_value"]);
-	}
-	return value;
-}
-
-function displayLogValue(add, x) {
-	let span = document.createElement("span");
-
-	span.style.float = "right";
-	span.style.color = "#678c00";
-	span.innerText = "$" + String(numberWithCommas(x))
-
-	add.appendChild(span);
-}
-
 function tradeView(){
-    const tradeContainer = document.querySelector(".trade-cont");
-	if (!tradeContainer) {
-		return false;
+	let arguments = window.location.hash.replace("#", "").replace("/", "").split("&");
+
+	for(let argument of arguments){
+		if(argument.split("=")[0] == "step" && (argument.split("=")[1] == "view" || argument.split("=")[1] == "initiateTrade")){
+			return true;
+		}
 	}
-	return true
+	return false;
 }
