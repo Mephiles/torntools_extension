3aecbe693fca3f79dbc1b401d0a02a9c718c63cc
Gregor Kaljulaid <gregor.kaljulaid@gmail.com>
2020-05-06 21:22:27 +0300

    Add Color Codes to Navbar (disabled)

--- torntools/scripts/content/global/ttGlobal.js
+++ torntools/scripts/content/global/ttGlobal.js
@@ -15,5 +15,43 @@ window.addEventListener('load', async (event) => {
                 href: chrome.runtime.getURL("/views/settings.html")
             });
         }
+
+        // showColorCodes();
     })
 });
+
+
+function showColorCodes(){
+    let dictionary = {
+        "home": "#8ad2f7",
+        "items": "#91a96be0",
+        "city": "#cc3ecc9e",
+        "job": "#a255279e",
+        "gym": "#7ed426",
+        "properties": "#71717173",
+        "education": "#8ad2f7",
+        "crimes": "#e4755b",
+        "missions": "#e4755b",
+        "newspaper": "#71717173",
+        "jail": "#a255279e",
+        "hospital": "#71717173",
+        "casino": "#e4755b",
+        "forums": "#8ad2f7",
+        "hall_of_fame": "#c5c242",
+        "my_faction": "#91a96be0",
+        "recruit_citizens": "#71717173",
+        "competitions": "#c5c242"
+    }
+
+    let navbar_content = doc.find("h2=Areas").nextElementSibling;
+
+    for(let key in dictionary){
+        let cell = navbar_content.find(`#nav-${key}`);
+
+        let span = doc.new("span");
+            span.setClass("nav-color-code");
+            span.style.backgroundColor = dictionary[key];
+
+        cell.appendChild(span);
+    }
+}
