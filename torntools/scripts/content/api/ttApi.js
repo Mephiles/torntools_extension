7e080afe5de1df317f2c42225e19b5b284c92e4b
Gregor Kaljulaid <gregor.kaljulaid@gmail.com>
2020-05-06 11:22:18 +0300

    Set API response types to Pretty by default

--- torntools/scripts/content/api/ttApi.js
+++ torntools/scripts/content/api/ttApi.js
@@ -6,7 +6,14 @@ window.addEventListener('load', (event) => {
 
 		let demo_page_checker = setInterval(function(){
 			if(document.querySelector("#demo").style.display != "none"){
+				// apply API key
 				doc.find("#api_key").focus();
+
+				// set resonse type to pretty
+				for(let type_pretty of doc.findAll("input[value=pretty]")){
+					type_pretty.checked=true
+				}
+
 				clearInterval(demo_page_checker);
 			}
 		}, 500)
