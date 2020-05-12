cf4c97986daa736bd3d9695616f33e359d3c00cc
Gregor Kaljulaid <gregor.kaljulaid@gmail.com>
2020-05-12 22:27:25 +0300

    Add API preferences

--- torntools/scripts/content/api/ttApi.js
+++ torntools/scripts/content/api/ttApi.js
@@ -1,21 +1,25 @@
 window.addEventListener('load', (event) => {
 	console.log("TT - API");
 	
-	local_storage.get("api_key", function(api_key){
-		doc.find("#api_key").value = api_key;
+	local_storage.get(["api_key", "settings"], function([api_key, settings]){
+		if(settings.pages.api.key){
+			doc.find("#api_key").value = api_key;
 
-		let demo_page_checker = setInterval(function(){
-			if(document.querySelector("#demo").style.display != "none"){
-				// apply API key
-				doc.find("#api_key").focus();
+			let demo_page_checker = setInterval(function(){
+				if(document.querySelector("#demo").style.display != "none"){
+					// apply API key
+					doc.find("#api_key").focus();
 
-				// set resonse type to pretty
-				for(let type_pretty of doc.findAll("input[value=pretty]")){
-					type_pretty.checked=true
+					clearInterval(demo_page_checker);
 				}
+			}, 500);
+		}
 
-				clearInterval(demo_page_checker);
+		if(settings.pages.api.pretty){
+			// set resonse type to pretty
+			for(let type_pretty of doc.findAll("input[value=pretty]")){
+				type_pretty.checked=true
 			}
-		}, 500)
+		}
 	});
 });
