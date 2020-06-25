0418154028eb6824637ad00658f8672a298d73cb
Gregor Kaljulaid <gregor.kaljulaid@gmail.com>
2020-06-25 15:00:30 +0300

    Make index load faster

--- torntools/views/index/index.js
+++ torntools/views/index/index.js
@@ -1,7 +1,7 @@
 window.addEventListener("load", function(){
 	console.log("Start Index");
 
-	DBloaded().then(function(){
+	local_storage.get(["api_key", "settings"], function([api_key, settings]){
 		if(api_key){
 			console.log("Loading next window");
 			// Change window
@@ -22,3 +22,82 @@ window.addEventListener("load", function(){
 		}
 	});
 });
+
+const local_storage = {
+    get: function (key, callback) {
+        let promise = new Promise(function (resolve, reject) {
+            if (Array.isArray(key)) {
+                let arr = [];
+                chrome.storage.local.get(key, function (data) {
+                    for (let item of key) {
+                        arr.push(data[item]);
+                    }
+                    return resolve(arr);
+                });
+            } else if (key == null) {
+                chrome.storage.local.get(null, function (data) {
+                    return resolve(data);
+                });
+            } else {
+                chrome.storage.local.get([key], function (data) {
+                    return resolve(data[key]);
+                });
+            }
+        });
+
+        promise.then(function (data) {
+            callback(data);
+        });
+    },
+    set: function (object, callback) {
+        chrome.storage.local.set(object, function () {
+            callback ? callback() : null;
+        });
+    },
+    change: function (keys_to_change, callback) {
+        for(let top_level_key of Object.keys(keys_to_change)){
+            chrome.storage.local.get(top_level_key, function (data) {
+                let database = data[top_level_key];
+                database = recursive(database, keys_to_change[top_level_key]);
+    
+                function recursive(parent, keys_to_change){
+                    for(let key in keys_to_change){
+                        if(key in parent && typeof keys_to_change[key] == "object" && !Array.isArray(keys_to_change[key])){
+                            parent[key] = recursive(parent[key], keys_to_change[key]);
+                        } else {
+                            parent[key] = keys_to_change[key];
+                        }
+                    }
+                    return parent;
+                }
+    
+                chrome.storage.local.set({[top_level_key]: database}, function () {
+                    callback ? callback() : null;
+                });
+            });
+        }
+    },
+    clear: function (callback) {
+        chrome.storage.local.clear(function () {
+            callback ? callback() : null;
+        });
+    },
+    reset: function (callback) {
+        chrome.storage.local.get(["api_key"], function (data) {
+            let api_key = data.api_key;
+            chrome.storage.local.clear(function () {
+                chrome.storage.local.set(STORAGE, function () {
+                    chrome.storage.local.set({
+                        "api_key": api_key
+                    }, function () {
+                        chrome.storage.local.get(null, function (data) {
+                            console.log("Storage cleared");
+                            console.log("New storage", data);
+                            callback ? callback() : null;
+                        });
+                    });
+                });
+            });
+        });
+    }
+}
