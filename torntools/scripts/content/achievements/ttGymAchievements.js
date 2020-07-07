32ecf2a4bc9f4022962e6cc4baa3a41e10f3d14d
Gregor Kaljulaid <gregor.kaljulaid@gmail.com>
2020-07-07 17:58:24 +0300

    Fix Gym achievements numbers

--- torntools/scripts/content/achievements/ttGymAchievements.js
+++ torntools/scripts/content/achievements/ttGymAchievements.js
@@ -3,33 +3,32 @@ navbarLoaded().then(function(){
     
     let show_completed = settings.achievements.completed;
 
-    if(!settings.achievements.show)
-        return;
+    if(!settings.achievements.show) return;
     
     // object of all the achievements on this page
     let achievements = {
         "Strength": {
-            "stats": parseInt(userdata.strength),
+            "stats": parseInt(userdata.strength.replace(/,/g, "")),
             "keyword": "strength",
             "incl": ["gain"]
         },
         "Speed": {
-            "stats": parseInt(userdata.speed),
+            "stats": parseInt(userdata.speed.replace(/,/g, "")),
             "keyword": "speed",
             "incl": ["gain"]
         },
         "Defense": {
-            "stats": parseInt(userdata.defense),
+            "stats": parseInt(userdata.defense.replace(/,/g, "")),
             "keyword": "defense",
             "incl": ["gain"]
         },
         "Dexterity": {
-            "stats": parseInt(userdata.dexterity),
+            "stats": parseInt(userdata.dexterity.replace(/,/g, "")),
             "keyword": "dexterity",
             "incl": ["gain"]
         },
         "Total": {
-            "stats": parseInt(userdata.total),
+            "stats": parseInt(userdata.total.replace(/,/g, "")),
             "keyword": "total stats"
         },
     }
