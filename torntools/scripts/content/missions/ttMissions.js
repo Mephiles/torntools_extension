3405a118fbd0d1fa6bff7ece9511acb9ee4f7bae
Kobe De Peuter <kobe.depeuter@student.ap.be>
2020-07-29 14:18:18 +0200

    Possibly show mission reward calculations after buying a reward.
    Close #43

--- torntools/scripts/content/missions/ttMissions.js
+++ torntools/scripts/content/missions/ttMissions.js
@@ -1,64 +1,27 @@
 requireDatabase().then(function(){
+    if (!settings.pages.missions.rewards) return;
+
+    doc.find("head").appendChild(doc.new({
+        type: "script",
+        attributes: {type: "text/javascript", src: chrome.runtime.getURL("/scripts/content/missions/ttMissionsInject.js")}
+    }));
+
+    window.addEventListener("tt-xhr", (event) => {
+        const {page, json, xhr} = event.detail;
+        console.log("DKK XHR", page, json)
+        if (page !== "loader") return;
+
+        const params = new URLSearchParams(xhr.requestBody);
+        if (params.get("sid") !== "missionsRewards") return;
+        else console.log("DKK SID", params.get("sid"))
+
+        rewardsLoaded().then(showRewards);
+    });
+
     rewardsLoaded().then(function(){
         console.log("TT - Missions");
 
-        if(!settings.pages.missions.rewards){
-            return;
-        }
-
-        let user_points = parseInt(doc.find(".total-mission-points").innerText.replace(",", ""));
-        let reward_items = doc.findAll(".rewards-list li");
-        
-        for(let item of reward_items){
-            let info = JSON.parse(item.getAttribute("data-ammo-info"));
-            let item_id = info.image;
-            let price_points = info.points;
-            let quantity = info.amount;
-
-            // Show if user can buy
-            let actions_wrap = item.find(".act-wrap");
-                actions_wrap.style.boxSizing = "border-box";
-                actions_wrap.style.borderColor = "black";
-                actions_wrap.style.borderImage = "none";
-
-            if(user_points < price_points)
-                actions_wrap.style.borderTop = "1px solid red";
-            else
-                actions_wrap.style.borderTop = "1px solid #2ef42e";
-
-            if(!item_id || typeof item_id == "string")
-                continue;
-
-            let market_price = itemlist.items[item_id].market_value;
-            item.style.height = "160px";  // to fit value info
-
-            // Show one item price
-            let one_item_price = doc.new("span");
-                one_item_price.innerText = `$${numberWithCommas(market_price)}`;
-                one_item_price.setClass("tt-one-item-price");
-
-            item.find(".img-wrap").appendChild(one_item_price);
-
-            // Show total & point value
-            let value_div = doc.new("div");
-            
-            let div_total_value = doc.new({type: "div", text: "Total value: ", class: "tt-total-value"});
-            if(mobile) div_total_value.style.marginTop = "66px";
-            let span_total_value = doc.new("span");
-                span_total_value.innerText = `$${numberWithCommas(quantity*market_price)}`;
-
-            let div_point_value = doc.new("div");
-                div_point_value.innerText = "Point value: ";
-                div_point_value.setClass("tt-point-value");
-            let span_point_value = doc.new("span");
-                span_point_value.innerText = `$${numberWithCommas(((quantity*market_price)/price_points).toFixed())}`;
-
-            div_total_value.appendChild(span_total_value);
-            div_point_value.appendChild(span_point_value);
-            value_div.appendChild(div_total_value);
-            value_div.appendChild(div_point_value);
-            actions_wrap.insertBefore(value_div, actions_wrap.find(".actions"));
-        }
+        showRewards();
     });
 });
 
@@ -72,3 +35,61 @@ function rewardsLoaded(){
         }, 100);
     });
 }
+
+function showRewards() {
+    let user_points = parseInt(doc.find(".total-mission-points").innerText.replace(",", ""));
+    let reward_items = doc.findAll(".rewards-list li");
+
+    for(let item of reward_items){
+        let info = JSON.parse(item.getAttribute("data-ammo-info"));
+        let item_id = info.image;
+        let price_points = info.points;
+        let quantity = info.amount;
+
+        // Show if user can buy
+        let actions_wrap = item.find(".act-wrap");
+        actions_wrap.style.boxSizing = "border-box";
+        actions_wrap.style.borderColor = "black";
+        actions_wrap.style.borderImage = "none";
+
+        if(user_points < price_points)
+            actions_wrap.style.borderTop = "1px solid red";
+        else
+            actions_wrap.style.borderTop = "1px solid #2ef42e";
+
+        if(!item_id || typeof item_id == "string")
+            continue;
+
+        let market_price = itemlist.items[item_id].market_value;
+        item.style.height = "160px";  // to fit value info
+
+        // Show one item price
+        let one_item_price = doc.new("span");
+        one_item_price.innerText = `$${numberWithCommas(market_price)}`;
+        one_item_price.setClass("tt-one-item-price");
+
+        item.find(".img-wrap").appendChild(one_item_price);
+
+        // Show total & point value
+        let value_div = doc.new("div");
+
+        let div_total_value = doc.new({type: "div", text: "Total value: ", class: "tt-total-value"});
+        if(mobile) div_total_value.style.marginTop = "66px";
+        let span_total_value = doc.new("span");
+        span_total_value.innerText = `$${numberWithCommas(quantity*market_price)}`;
+
+        let div_point_value = doc.new("div");
+        div_point_value.innerText = "Point value: ";
+        div_point_value.setClass("tt-point-value");
+        let span_point_value = doc.new("span");
+        span_point_value.innerText = `$${numberWithCommas(((quantity*market_price)/price_points).toFixed())}`;
+
+        div_total_value.appendChild(span_total_value);
+        div_point_value.appendChild(span_point_value);
+        value_div.appendChild(div_total_value);
+        value_div.appendChild(div_point_value);
+        actions_wrap.insertBefore(value_div, actions_wrap.find(".actions"));
+    }
+}
+
+
