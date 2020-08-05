467fdd458f851106ac41904f8ff371b831efd308
Kobe De Peuter <kobe.depeuter@student.ap.be>
2020-08-05 15:02:05 +0200

    Simplify and format the stock code.

--- torntools/scripts/content/stockexchange/ttStockexchange.js
+++ torntools/scripts/content/stockexchange/ttStockexchange.js
@@ -1,39 +1,38 @@
-requireDatabase().then(function(){
-	stocksLoaded().then(function(){
+requireDatabase().then(function () {
+    stocksLoaded().then(function () {
         console.log("TT - Stock Exchange");
 
-        let stocks = doc.findAll(".stock-list>.item");
+        const isPortfolio = getSearchParameters().get("step") === "portfolio";
 
-        // Open torntools redirect
-        let torntools_redirect = getSearchParameters().has("torntools_redirect") ? getSearchParameters().get("torntools_redirect").replace(/%20/g, " ") : undefined;
-        if(torntools_redirect){
-            for(let stock of stocks){
-                let stock_name = stock.find(".name").innerText;
-        
-                if(stock_name == torntools_redirect){
-                    stock.firstElementChild.click();
+        if (isPortfolio) {
+
+        } else {
+            let ttRedirect = getSearchParameters().has("torntools_redirect") ? getSearchParameters().get("torntools_redirect").replace(/%20/g, " ") : undefined;
+
+            if (ttRedirect || settings.pages.stockexchange.acronyms) {
+                for (let stock of doc.findAll(".stock-list > .item")) {
+                    const heading = stock.firstElementChild; // heading
+
+                    const name = heading.find(".name").innerText;
+
+                    // Open torntools redirect
+                    if (ttRedirect && name === ttRedirect) stock.firstElementChild.click();
+
+                    if (settings.pages.stockexchange.acronyms) {
+                        const acronym = stock.getAttribute("data-stock").toUpperCase();
+
+                        stock.find(".name").innerText = `(${acronym}) ${name}`;
+                    }
                 }
-            }
-        }
-
-        // Add acronyms
-        if(settings.pages.stockexchange.acronyms){
-            for(let stock of stocks){
-                stock = stock.firstElementChild; // heading
-                let stock_id = stock.getAttribute("action").split("ID=")[1];
-                let acronym = torndata.stocks[stock_id].acronym
-                
-                let text = `(${acronym}) ${stock.find(".name").innerText}`;
-                stock.find(".name").innerText = text;
             }
         }
     });
 });
 
-function stocksLoaded(){
-    return new Promise(function(resolve, reject){
-        let checker = setInterval(function(){
-            if(doc.find(".stock-list .item")){
+function stocksLoaded() {
+    return new Promise(function (resolve, reject) {
+        let checker = setInterval(function () {
+            if (doc.find(".stock-list .item, .portfolio-list-shares > .item-wrap")) {
                 resolve(true);
                 return clearInterval(checker);
             }
