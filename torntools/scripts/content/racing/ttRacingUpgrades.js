8667e504e10fbe3f96706205d210b6a5740668b8
Kobe De Peuter <kobe.depeuter@student.ap.be>
2020-08-30 22:59:38 +0200

    Change racing upgrade showing.

--- torntools/scripts/content/racing/ttRacingUpgrades.js
+++ torntools/scripts/content/racing/ttRacingUpgrades.js
@@ -4,54 +4,37 @@ requireDatabase().then(() => {
 
 		if (!settings.pages.racing.upgrades) return;
 
-		Main();
+		showUpgrades();
 
 		// start checking again when left site
 		for (let category of doc.findAll(".categories li")) {
 			category.addEventListener("click", () => {
-				upgradeView().then(Main);
+				upgradeView().then(showUpgrades);
 			});
 		}
 	});
 });
 
-function Main() {
-	let items = document.querySelectorAll(".pm-items-wrap .d-wrap .pm-items .unlock");
-	for (let item of items) {
-		item.style.position = "relative";
-		let title = item.find(".title");
-		title.style.fontSize = "11px";
-
-		let properties = item.findAll(".properties");
-		for (let property of properties) {
-			let span = doc.new("span");
-			span.setClass("tt-upgrade-text");
-			span.style.top = `${7 + (10 * [...properties].indexOf(property))}px`;
-
-			let name = property.find(".name").innerText.trim();
-			let stat_gray = parseInt(property.find(".bar-gray-light-wrap-d").style.width);
-			let stat_color = parseInt(property.find(".bar-color-wrap-d").style.width);
-			let difference = stat_color - stat_gray;
-
-			if (property.find(".negative")) {
-				difference = `-${difference}`;
-				span.style.color = "#ff4444";
-			} else
-				difference = `+${difference}`;
-
-			span.innerText = `${difference} ${name}`;
-			title.appendChild(span);
-		}
-	}
+function upgradeView() {
+	return requireElement(".pm-categories-wrap");
 }
 
-function upgradeView() {
-	return new Promise((resolve) => {
-		let checker = setInterval(() => {
-			if (document.querySelector(".pm-categories-wrap")) {
-				resolve(true);
-				return clearInterval(checker);
-			}
-		}, 100);
-	});
+function showUpgrades() {
+	let items = document.querySelectorAll(".pm-items-wrap .d-wrap .pm-items .unlock");
+	for (let item of items) {
+		for (let property of item.findAll(".properties")) {
+			let statOld = parseInt(property.find(".bar-gray-light-wrap-d").style.width);
+			let statNew = parseInt(property.find(".bar-color-wrap-d").style.width);
+			let difference = statNew - statOld;
+
+			if (isNaN(difference)) continue;
+
+			const bar = property.find(".bar-gray-d");
+
+			if (difference > 0) bar.classList.add("positive");
+			else if (difference < 0) bar.classList.add("negative");
+
+			bar.innerText = `${difference}%`
+		}
+	}
 }
