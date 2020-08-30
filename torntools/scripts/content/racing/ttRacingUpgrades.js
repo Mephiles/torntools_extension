b02738f7dc9db92f42a983782ac64f25c0023317
Kobe De Peuter <kobe.depeuter@student.ap.be>
2020-08-30 23:52:35 +0200

    Add more racing information.

--- torntools/scripts/content/racing/ttRacingUpgrades.js
+++ torntools/scripts/content/racing/ttRacingUpgrades.js
@@ -21,7 +21,11 @@ function upgradeView() {
 
 function showUpgrades() {
 	let items = document.querySelectorAll(".pm-items-wrap .d-wrap .pm-items .unlock");
+
+	let parts = [];
 	for (let item of items) {
+		parts.push(item.getAttribute("data-part"));
+
 		for (let property of item.findAll(".properties")) {
 			let statOld = parseInt(property.find(".bar-gray-light-wrap-d").style.width);
 			let statNew = parseInt(property.find(".bar-color-wrap-d").style.width);
@@ -39,4 +43,41 @@ function showUpgrades() {
 			bar.innerText = `${difference}%`
 		}
 	}
+
+	parts = parts.filter((value, index, self) => self.indexOf(value) === index);
+	let needed = [];
+	parts.forEach(part => {
+		if (doc.find(`.pm-items .bought[data-part="${part}"]`)) return;
+
+		const color = `#${(Math.random() * 0xfffff * 1000000).toString(16).slice(0, 6)}`
+		needed.push(`<span class="tt-race-upgrade-needed" style="color: ${color};">${part}</span>`);
+
+		let category;
+		for (let item of doc.findAll(`.pm-items .unlock[data-part="${part}"]`)) {
+			if (!category) category = findParent(item, { class: "pm-items-wrap" }).getAttribute("category");
+
+			item.find(".status").style['background-color'] = color;
+			item.find(".status").classList.add("tt-modified");
+		}
+
+		const elCategory = doc.find(`.pm-categories > li[data-category="${category}"]`);
+		if (elCategory.find(".tt-race-need-icon")) {
+			elCategory.find(".tt-race-need-icon").innerText = parseInt(elCategory.find(".tt-race-need-icon").innerText) + 1;
+		} else {
+			elCategory.find(".bg-hover").appendChild(doc.new({ type: "div", class: "tt-race-need-icon", text: 1 }));
+		}
+	})
+
+	doc.find("#racingAdditionalContainer > .info-msg-cont .msg").appendChild(doc.new({
+		type: "div",
+		html: `
+			<p class="tt-race-upgrades">
+				<br/>
+				<br/>
+				${needed.length ? `
+				<strong>${needed.length}</strong> part available to upgrade: <strong>${needed.join(", ")}</strong>
+				` : "Your car is <strong style='color: #789e0c;'>FULLY UPGRADED</strong>"}
+			</p>
+		`,
+	}));
 }
