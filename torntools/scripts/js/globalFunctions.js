586af7fb73b8f6100b34cb417ebc4bfd80817a3f
DeKleineKobini <kobedepeuter@outlook.com>
2020-09-22 13:13:55 +0200

    Add some debugging to notifications.

--- torntools/scripts/js/globalFunctions.js
+++ torntools/scripts/js/globalFunctions.js
@@ -3039,7 +3039,7 @@ function notifyUser(title, message, url) {
 
 		chrome.notifications.create(notificationOptions, function (id) {
 			notificationLinkRelations[id] = url;
-			console.log("   Notified!");
+			console.log("   Notified!", notificationOptions);
 		});
 
 		if (settings.notifications_tts) {
