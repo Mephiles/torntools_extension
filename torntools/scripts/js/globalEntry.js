3a50edd1bcf510cb8d92c5cb290ed11e4a65c3de
wootty2000 <2o4Ae0WaEZoa>
2020-10-01 00:58:14 +0100

    Add refill award trackers

--- torntools/scripts/js/globalEntry.js
+++ torntools/scripts/js/globalEntry.js
@@ -10,13 +10,18 @@ function mobileChecker() {
 			return;
 		}
 
-		window.addEventListener("DOMContentLoaded", () => {
+
+		if (document.readyState === "complete" || document.readyState === "loaded") check();
+		else window.addEventListener("DOMContentLoaded", check);
+
+		function check() {
 			const browserWidth = window.innerWidth;
+
 
 			if (browserWidth <= 600) resolve(true);
 			else resolve(false);
-		});
-	});
+		}
+ 	});
 }
 
 requireDatabase(false).then(async () => {
