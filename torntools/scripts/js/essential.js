a41c536d5bcce45f6f11f5bbb297a3314c7a793c
DeKleineKobini <kobedepeuter@outlook.com>
2020-10-02 20:56:30 +0200

    Improve entry script loading times.

---
+++ torntools/scripts/js/essential.js
@@ -0,0 +1,181 @@
+const doc = document;
+
+const DATABASE_STATUSES = {
+	NOT_INITIALIZED: 0,
+	LOADING: 1,
+	LOADED: 2,
+	ENTRY: 3,
+	FAILED: 99,
+};
+
+let database_status = DATABASE_STATUSES.NOT_INITIALIZED;
+
+Document.prototype.find = function (type) {
+	if (type.indexOf("=") > -1 && type.indexOf("[") === -1) {
+		let key = type.split("=")[0];
+		let value = type.split("=")[1];
+
+		for (let element of document.querySelectorAll(key)) {
+			if (element.innerText === value) {
+				return element;
+			}
+		}
+
+		try {
+			this.querySelector(type);
+		} catch (err) {
+			return undefined;
+		}
+	}
+	return this.querySelector(type);
+};
+Element.prototype.find = function (type) {
+	if (type.indexOf("=") > -1 && type.indexOf("[") === -1) {
+		let key = type.split("=")[0];
+		let value = type.split("=")[1];
+
+		for (let element of document.querySelectorAll(key)) {
+			if (element.innerText === value) {
+				return element;
+			}
+		}
+
+		try {
+			this.querySelector(type);
+		} catch (err) {
+			return undefined;
+		}
+	}
+	return this.querySelector(type);
+};
+
+function getPageStatus() {
+	return new Promise((resolve) => {
+		let checker = setInterval(function () {
+			let page_heading = doc.find("#skip-to-content, .title___2sbYr, .nonFullScreen .content-title h4");
+			let message = doc.find("div[role='main'] > .info-msg-cont");
+
+			// Page heading
+			if (page_heading) {
+				switch (page_heading.innerText) {
+					case "Please Validate":
+						resolve("captcha");
+						return clearInterval(checker);
+					case "Error":
+						resolve("blocked");
+						return clearInterval(checker);
+				}
+			}
+
+			// Page info message
+			if (message) {
+				if (message.innerText.includes("a page which is blocked when")) {
+					resolve("blocked");
+					return clearInterval(checker);
+				}
+			}
+
+			if (page_heading || message) {
+				resolve("okay");
+				return clearInterval(checker);
+			}
+		});
+	});
+}
+
+const ttStorage = {
+	get: function (key, callback) {
+		new Promise((resolve) => {
+			if (Array.isArray(key)) {
+				let arr = [];
+				chrome.storage.local.get(key, function (data) {
+					for (let item of key) {
+						arr.push(data[item]);
+					}
+					return resolve(arr);
+				});
+			} else if (key === null) {
+				chrome.storage.local.get(null, function (data) {
+					return resolve(data);
+				});
+			} else {
+				chrome.storage.local.get([key], function (data) {
+					return resolve(data[key]);
+				});
+			}
+		}).then(function (data) {
+			callback(data);
+		});
+	},
+	set: function (object, callback) {
+		chrome.storage.local.set(object, function () {
+			callback ? callback() : null;
+		});
+	},
+	change: function (keys_to_change, callback) {
+		for (let top_level_key of Object.keys(keys_to_change)) {
+			chrome.storage.local.get(top_level_key, function (data) {
+				let database = data[top_level_key];
+				database = recursive(database, keys_to_change[top_level_key]);
+
+				function recursive(parent, keys_to_change) {
+					for (let key in keys_to_change) {
+						if (parent && key in parent && typeof keys_to_change[key] === "object" && !Array.isArray(keys_to_change[key])) {
+							parent[key] = recursive(parent[key], keys_to_change[key]);
+						} else if (parent) {
+							parent[key] = keys_to_change[key];
+						} else {
+							parent = { [key]: keys_to_change[key] };
+						}
+					}
+					return parent;
+				}
+
+				chrome.storage.local.set({ [top_level_key]: database }, function () {
+					callback ? callback() : null;
+				});
+			});
+		}
+	},
+	clear: function (callback) {
+		chrome.storage.local.clear(function () {
+			callback ? callback() : null;
+		});
+	},
+	reset: function (callback) {
+		chrome.storage.local.get(["api_key"], function (data) {
+			let api_key = data.api_key;
+			chrome.storage.local.clear(function () {
+				chrome.storage.local.set(STORAGE, function () {
+					chrome.storage.local.set({
+						api_key: api_key,
+					}, function () {
+						chrome.storage.local.get(null, function (data) {
+							console.log("Storage cleared");
+							console.log("New storage", data);
+							callback ? callback() : null;
+						});
+					});
+				});
+			});
+		});
+	},
+};
+
+function getCurrentPage() {
+	const pages = {
+		index: "home",
+		jailview: "jail",
+		hospitalview: "hospital",
+		item: "items",
+		profiles: "profile",
+		factions: "faction",
+	};
+
+	let page = window.location.pathname.substring(1);
+
+	if (page.endsWith(".php")) page = page.substring(0, page.length - 4);
+	else if (page.endsWith(".html")) page = page.substring(0, page.length - 5);
+
+	return pages[page] || page;
+}
--- torntools/scripts/js/globalFunctions.js
+++ torntools/scripts/js/globalFunctions.js
@@ -6,8 +6,6 @@ console.log("TT - Loading global functions.");
 
 chrome = typeof browser !== "undefined" ? browser : chrome;
 
-const doc = document;
-
 const ttConsole = new ttCustomConsole();
 
 const HIGHLIGHT_PLACEHOLDERS = {
@@ -1752,45 +1750,6 @@ let injectedFetch = false;
  * Add prototype functions.
  */
 
-Document.prototype.find = function (type) {
-	if (type.indexOf("=") > -1 && type.indexOf("[") === -1) {
-		let key = type.split("=")[0];
-		let value = type.split("=")[1];
-
-		for (let element of document.querySelectorAll(key)) {
-			if (element.innerText === value) {
-				return element;
-			}
-		}
-
-		try {
-			this.querySelector(type);
-		} catch (err) {
-			return undefined;
-		}
-	}
-	return this.querySelector(type);
-};
-Element.prototype.find = function (type) {
-	if (type.indexOf("=") > -1 && type.indexOf("[") === -1) {
-		let key = type.split("=")[0];
-		let value = type.split("=")[1];
-
-		for (let element of document.querySelectorAll(key)) {
-			if (element.innerText === value) {
-				return element;
-			}
-		}
-
-		try {
-			this.querySelector(type);
-		} catch (err) {
-			return undefined;
-		}
-	}
-	return this.querySelector(type);
-};
-
 Document.prototype.findAll = function (type) {
 	return this.querySelectorAll(type);
 };
@@ -1864,85 +1823,6 @@ String.prototype.replaceAll = function (text, replace) {
 /*
  * Load some functions.
  */
-
-const ttStorage = {
-	get: function (key, callback) {
-		new Promise((resolve) => {
-			if (Array.isArray(key)) {
-				let arr = [];
-				chrome.storage.local.get(key, function (data) {
-					for (let item of key) {
-						arr.push(data[item]);
-					}
-					return resolve(arr);
-				});
-			} else if (key === null) {
-				chrome.storage.local.get(null, function (data) {
-					return resolve(data);
-				});
-			} else {
-				chrome.storage.local.get([key], function (data) {
-					return resolve(data[key]);
-				});
-			}
-		}).then(function (data) {
-			callback(data);
-		});
-	},
-	set: function (object, callback) {
-		chrome.storage.local.set(object, function () {
-			callback ? callback() : null;
-		});
-	},
-	change: function (keys_to_change, callback) {
-		for (let top_level_key of Object.keys(keys_to_change)) {
-			chrome.storage.local.get(top_level_key, function (data) {
-				let database = data[top_level_key];
-				database = recursive(database, keys_to_change[top_level_key]);
-
-				function recursive(parent, keys_to_change) {
-					for (let key in keys_to_change) {
-						if (parent && key in parent && typeof keys_to_change[key] === "object" && !Array.isArray(keys_to_change[key])) {
-							parent[key] = recursive(parent[key], keys_to_change[key]);
-						} else if (parent) {
-							parent[key] = keys_to_change[key];
-						} else {
-							parent = { [key]: keys_to_change[key] };
-						}
-					}
-					return parent;
-				}
-
-				chrome.storage.local.set({ [top_level_key]: database }, function () {
-					callback ? callback() : null;
-				});
-			});
-		}
-	},
-	clear: function (callback) {
-		chrome.storage.local.clear(function () {
-			callback ? callback() : null;
-		});
-	},
-	reset: function (callback) {
-		chrome.storage.local.get(["api_key"], function (data) {
-			let api_key = data.api_key;
-			chrome.storage.local.clear(function () {
-				chrome.storage.local.set(STORAGE, function () {
-					chrome.storage.local.set({
-						api_key: api_key,
-					}, function () {
-						chrome.storage.local.get(null, function (data) {
-							console.log("Storage cleared");
-							console.log("New storage", data);
-							callback ? callback() : null;
-						});
-					});
-				});
-			});
-		});
-	},
-};
 
 const infoBox = {
 	newRow: function (key, value, attr = {}) {
@@ -2852,24 +2732,6 @@ function sort(table, col, type) {
 	}
 }
 
-function getCurrentPage() {
-	const pages = {
-		index: "home",
-		jailview: "jail",
-		hospitalview: "hospital",
-		item: "items",
-		profiles: "profile",
-		factions: "faction",
-	};
-
-	let page = window.location.pathname.substring(1);
-
-	if (page.endsWith(".php")) page = page.substring(0, page.length - 4);
-	else if (page.endsWith(".html")) page = page.substring(0, page.length - 5);
-
-	return pages[page] || page;
-}
-
 function requireNavbar() {
 	return requireElement("#sidebar");
 }
@@ -2917,40 +2779,6 @@ function requireCondition(condition, attributes = {}) {
 				return clearInterval(checker);
 			}
 		}, attributes.delay);
-	});
-}
-
-function getPageStatus() {
-	return new Promise((resolve) => {
-		let checker = setInterval(function () {
-			let page_heading = doc.find("#skip-to-content, .title___2sbYr, .nonFullScreen .content-title h4");
-			let message = doc.find("div[role='main'] > .info-msg-cont");
-
-			// Page heading
-			if (page_heading) {
-				switch (page_heading.innerText) {
-					case "Please Validate":
-						resolve("captcha");
-						return clearInterval(checker);
-					case "Error":
-						resolve("blocked");
-						return clearInterval(checker);
-				}
-			}
-
-			// Page info message
-			if (message) {
-				if (message.innerText.includes("a page which is blocked when")) {
-					resolve("blocked");
-					return clearInterval(checker);
-				}
-			}
-
-			if (page_heading || message) {
-				resolve("okay");
-				return clearInterval(checker);
-			}
-		});
 	});
 }
 
