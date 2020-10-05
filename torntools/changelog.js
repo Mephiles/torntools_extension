fe4a1c807c2892ad88b6d0691d33124a42fff14f
raxty <commit@raxty.com>
2020-10-05 07:46:13 +0200

    Issue #214: Fix hide chat hidden by chats

--- torntools/changelog.js
+++ torntools/changelog.js
@@ -6,6 +6,7 @@ export default {
 		"Fixes": [
 			"Properly display item values for weapons and other single items. - DKK",
 			"Fix chain notifications going out too early. - DKK",
+			"Fix hide chat hidden by chats. - finally",
 		],
 		"Changes": [
 			"Added sell points award and refill award trackers. - wootty2000",
