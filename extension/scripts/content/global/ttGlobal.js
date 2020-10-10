cdb4f94f0a6c0608737623082f3a5468f5bb01a9
DeKleineKobini <kobedepeuter@outlook.com>
2020-10-11 00:26:26 +0200

    Add some error handling.

--- extension/scripts/content/global/ttGlobal.js
+++ extension/scripts/content/global/ttGlobal.js
@@ -10,13 +10,17 @@
 })();
 
 function loadGlobal() {
-	requireChatsLoaded().then(() => {
-		// TODO - Add search to chats.
-		// TODO - Block Zalgo in chats.
-	});
-	requireSidebar().then(() => {
-		// TODO - Show update notice.
-	});
+	requireChatsLoaded()
+		.then(() => {
+			// TODO - Add search to chats.
+			// TODO - Block Zalgo in chats.
+		})
+		.catch((reason) => console.error("TT failed during loading chats.", reason));
+	requireSidebar()
+		.then(() => {
+			// TODO - Show update notice.
+		})
+		.catch((reason) => console.error("TT failed during loading sidebar.", reason));
 	// TODO - Display custom developer console.
 	// TODO - Show Nuke Central Hospital revive request.
 	// TODO - Show last action in the mini profiles.
