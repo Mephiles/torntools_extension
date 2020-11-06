e3a199c9650352609059302aca1716843439d775
DeKleineKobini <kobedepeuter@outlook.com>
2020-11-06 15:46:40 +0100

    Invert key parameter condition.

--- torntools/scripts/js/globalFunctions.js
+++ torntools/scripts/js/globalFunctions.js
@@ -3160,7 +3160,7 @@ function fetchApi_v2(
 			const proxyKey = proxy_key;
 
 			let full_url;
-			if (location === "torntools" || location === "nukefamily" || location.includes("yata")) {
+			if (location !== "torn" && location !== "tornstats" && location !== "torn-proxy") {
 				full_url = `${base}${section || ""}`;
 			} else if (proxyKey || apiKey) {
 				full_url = `${base}${section}${objectid}${selections ? "selections=" + selections : ""}${
