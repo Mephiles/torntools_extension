a64207ae340cb70c0211970c54063983f3446e57
DeKleineKobini <kobedepeuter@outlook.com>
2020-11-30 14:39:29 +0100

    Solve stock issues.

--- extension/scripts/background.js
+++ extension/scripts/background.js
@@ -765,7 +765,7 @@ async function updateStocks() {
 	if (oldStocks) {
 		console.log("DKK stocks", Object.keys(settings.notifications.types.stocks).join(","));
 		for (let id in settings.notifications.types.stocks) {
-			const alerts = settings.notifications.types[id];
+			const alerts = settings.notifications.types.stocks[id];
 			console.log("DKK stock", id, alerts);
 
 			if (alerts.priceFalls && oldStocks[id].current_price > stocks[id].current_price && stocks[id].current_price <= alerts.priceFalls) {
