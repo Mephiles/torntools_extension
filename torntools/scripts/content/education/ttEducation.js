d3a59a54cedfa552824f6afceb2b8e96c12aee2f
Sashank999 <76554862+Sashank999@users.noreply.github.com>
2021-01-03 20:00:27 +0530

    Update ttEducation.js

--- torntools/scripts/content/education/ttEducation.js
+++ torntools/scripts/content/education/ttEducation.js
@@ -4,7 +4,7 @@ requireDatabase().then(() => {
 });
 
 function hideCompletedCategories() {
-	if (window.location.href.indexOf("step=main") > -1) {
+	if (window.location.href.includes("step=main")) {
 		for (let category of doc.find(".education").findAll(".ajax-act")) {
 			if (category.find(".bar-green-wrap-white-bg").style.width === "100%") category.style.opacity = "0.2";
 		}
