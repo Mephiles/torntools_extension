6822dba61e99c0533a9754e531d35a162d0b8fa2
Sashank999 <76554862+Sashank999@users.noreply.github.com>
2021-01-10 10:38:06 +0530

    Prettier Formatting

--- torntools/scripts/content/properties/ttProperties.js
+++ torntools/scripts/content/properties/ttProperties.js
@@ -1,16 +1,18 @@
 requireDatabase().then(() => {
 	console.log("TT - Properties");
 	let observer = new MutationObserver(addPropertyValues);
-	observer.observe(doc.find("div#properties-page-wrap"), {childList: true});
+	observer.observe(doc.find("div#properties-page-wrap"), { childList: true });
 	addPropertyValues();
 });
 
 function addPropertyValues() {
-	let propertyValue = null;
 	for (let property of doc.find(".properties-list").children) {
+		let propertyValue = null;
 		if (property.find("ul.info") !== null) {
-			propertyValue = numberWithCommas(property.find("ul.info").innerText.split("\n")[3].slice(1));
-			property.find(".title").innerText += ` ($${propertyValue})`
-		};
-	};
+			propertyValue = numberWithCommas(
+				property.find("ul.info").innerText.split("\n")[3].slice(1)
+			);
+			property.find(".title").innerText += ` ($${propertyValue})`;
+		}
+	}
 }
