4843461f15029cc9da99562b80bc7bc0a6b8707a
Sashank999 <76554862+Sashank999@users.noreply.github.com>
2021-01-16 17:51:02 +0530

    Changged some defaults

--- torntools/scripts/js/globalFunctions.js
+++ torntools/scripts/js/globalFunctions.js
@@ -1503,8 +1503,8 @@ const STORAGE = {
 				drug_details: true,
 				itemmarket_links: false,
 				highlight_bloodbags: "none",
-				show_missing_plushies: true,
-				show_missing_flowers: true,
+				show_missing_plushies: false,
+				show_missing_flowers: false,
 			},
 			travel: {
 				profits: true,
