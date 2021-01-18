5c22c39dbc07e3bdaf5436269015564a8d8fb9e2
Sashank999 <76554862+Sashank999@users.noreply.github.com>
2021-01-18 21:38:48 +0530

    Update changelog.js

--- torntools/changelog.js
+++ torntools/changelog.js
@@ -1,6 +1,6 @@
 export default {
 	"v5.5 - xxxxxx, xxth. 2021": {
-		Features: ["Show missing flowers and plushies in the inventory. - Sashank999"],
+		Features: ["Show missing flowers and plushies in the inventory. - Sashank999", "Disable some casino games. - Sashank999"],
 		Fixes: ["Use the new Torn classes. - Sashank999", "Solve some problems regarding the new class names. - DKK"],
 		Changes: ["Add ISO date formatting. - DKK", "Switch to new YATA domain. - DKK"],
 	},
