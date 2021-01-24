ba3d6e127493b8001f2b17c01dadee1c38fb4ef0
Sashank999 <76554862+Sashank999@users.noreply.github.com>
2021-01-24 15:43:28 +0530

    showECanGains()

--- torntools/scripts/content/items/ttItems.js
+++ torntools/scripts/content/items/ttItems.js
@@ -227,6 +227,7 @@ function initializeItems() {
 	if (settings.pages.items.highlight_bloodbags !== "none") highlightBloodBags();
 	showMissingPlushies();
 	showMissingFlowers();
+	showECanGains();
 }
 
 function itemsLoaded() {
@@ -756,4 +757,23 @@ function showMissingFlowers() {
 			doc.find(".main-items-cont-wrap").insertAdjacentElement("afterEnd", neededFlowersDiv);
 		}
 	}
+}
+
+function showECanGains() {
+	let facECanPerc = parseInt(userdata.faction_perks.filter((x) => /energy drinks/i.test(x)).map((x) => {
+		x.replace(/[^0-9\.]/g, "");
+	})[0]);
+	let jobECanPerc = parseInt(userdata.company_perks.filter((x) => /boost/i.test(x)).map((x) => {
+		x.replace(/[^0-9\.]/g, "");
+	})[0]);
+	doc.findAll("[data-category='Energy Drink']").forEach((ECan) => {
+		if (!ECan.find("span#tt-e-can")) {
+			let baseE = parseInt(itemlist.items[ECan.getAttribute("data-item")].effect.split(" ").map((x) => parseInt(x)).filter((x) => !isNaN(x))[0]);
+			let totalEnergy = baseE;
+			if (!isNaN(facECanPerc)) totalEnergy += (facECanPerc / 100) * baseE;
+			if (!isNaN(jobECanPerc)) totalEnergy += (jobECanPerc / 100) * baseE;
+			rawHTML = `<span id='tt-e-can' class='tt-item-price' style='padding-left: 5px;float: none;'>${totalEnergy}E</span>`;
+			ECan.find("span.qty.bold.t-hide").insertAdjacentHTML("afterEnd", rawHTML);
+		}
+	});
 }
