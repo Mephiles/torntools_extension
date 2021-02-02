0b58b39020638b618ec934bbd7e4271a68cb6653
Sashank999 <76554862+Sashank999@users.noreply.github.com>
2021-02-02 20:47:06 +0530

    Bug Fixes

--- torntools/scripts/content/loader/ttAttackLoader.js
+++ torntools/scripts/content/loader/ttAttackLoader.js
@@ -5,13 +5,16 @@ requireDatabase().then(() => {
 });
 
 function displayWarning() {
-	if (userdata.energy.current > userdata.energy.maximum && userdata.chain.current > 10 && settings.pages.attack.disable_attack_stacked_warning_until - new Date() <= 0) {
+	if (userdata.energy.current > userdata.energy.maximum && userdata.chain.current < 10 && new Date(settings.disable_attack_stacked_warning_until) - new Date() <= 0) {
 		let rawHTML = `<div class='tt-overlay-div'><span class='tt-overlay-text'>Warning! You have stacked energy. Beware!</span><button class="tt-silver-button tt-ok-button">OK</button><button class="tt-silver-button tt-dnd-button">I'm Chaining/Warring</button></div>`;
 		doc.find("a[href='#skip-to-content']").insertAdjacentHTML("afterEnd", rawHTML);
 		doc.find("button.tt-silver-button.tt-ok-button").addEventListener("click", (event) => event.target.parentElement.style.display = "none");
 		doc.find("button.tt-silver-button.tt-dnd-button").addEventListener("click", (event) => {
 			event.target.parentElement.style.display = "none";
-			settings.disable_attack_stacked_warning_until = new Date(new Date().getTime() + 30*60*1000);
+			ttStorage.get("settings", (settings) => {
+				settings.disable_attack_stacked_warning_until = new Date(new Date().getTime() + 30*60*1000).toString();
+				ttStorage.set({ settings: settings });
+			});
 		});
 	}
 }
