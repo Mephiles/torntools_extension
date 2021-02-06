bf026012c6b4d55ecb6b98112d262f9241248ac6
Sashank999 <76554862+Sashank999@users.noreply.github.com>
2021-02-06 20:09:49 +0530

    Update changelog.js

--- torntools/changelog.js
+++ torntools/changelog.js
@@ -1,7 +1,7 @@
 export default {
 	"v5.7 - xxxxxx, xx. 2021": {
-		Features: ["Add option to export War Report as CSV. - Sashank999"],
-		Fixes: ["Don't show the attack warning on attack logs. - Sashank999", "Fix achievements section not showing. - Sashank999", "Show a warning when you try to train while stacking. - Sashank999",],
+		Features: ["Add option to export War Report as CSV. - Sashank999", "Show employee effectiveness in red if has negatives in effectiveness. - Sashank999"],
+		Fixes: ["Don't show the attack warning on attack logs. - Sashank999", "Fix achievements section not showing. - Sashank999", "Show a warning when you try to train while stacking. - Sashank999"],
 		Changes: ["Only highlight chain timers over a configurable amount. - Sashank999", "Add 'I'm Chaining' button to stop warning for 30 minutes on Attack Page. - Sashank999"],
 	},
 	"v5.6.1 - January, xxth. 2021": {
