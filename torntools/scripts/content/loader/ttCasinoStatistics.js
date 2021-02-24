ac218d84e20695d3e318650241e66f9dffc5659b
Sashank999 <76554862+Sashank999@users.noreply.github.com>
2021-02-24 20:58:10 +0530

    Update ttCasinoStatistics.js

--- torntools/scripts/content/loader/ttCasinoStatistics.js
+++ torntools/scripts/content/loader/ttCasinoStatistics.js
@@ -7,11 +7,12 @@ requireDatabase().then(() => {
 });
 
 function addNetTotal(overallOrYour) {
-	requireElement(`div.stats-wrap.m-top10 div#${overallOrYour}-stats`).then(() => {
-		let totalWon, totalLost;
+	let totalWon, totalLost;
+	if (window.location.toString().includes("Poker") && overallOrYour === "overall") return;
+	requireElement(`div.stats-wrap.m-top10 div.${overallOrYour}-stats-wrap li.stat`).then(() => {
 		for (let x of doc.findAll(`div#${overallOrYour}-stats li.stat`)) {
-			if (x.innerText.includes("Total money won")) totalWon = x;
-			if (x.innerText.includes("Total money lost")) totalLost = x;
+			if (x.innerText.includes("Total money won") || x.innerText.includes("Total money gain")) totalWon = x;
+			if (x.innerText.includes("Total money lost") || x.innerText.includes("Total money loss")) totalLost = x;
 		}
 		let netTotal = "$" + numberWithCommas(parseInt(totalWon.parentElement.find("li.stat-value").innerText.replace(/[$,]/g, "")) - parseInt(totalLost.parentElement.find("li.stat-value").innerText.replace(/[$,]/g, "")), false);
 		totalLost.parentElement.parentElement.insertAdjacentHTML("afterEnd", `<li>
