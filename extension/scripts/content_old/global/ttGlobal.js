b929a76c91d97089344e5a333f561df6466d84f8
DeKleineKobini <kobedepeuter@outlook.com>
2021-03-08 22:42:19 +0100

    Move chat autocomplete to it's own feature file.

--- extension/scripts/content_old/global/ttGlobal.js
+++ extension/scripts/content_old/global/ttGlobal.js
@@ -7,39 +7,10 @@ let mouseX, mouseY;
 	await loadDatabase();
 	console.log("TT: Global - Loading script. ");
 
-	storageListeners.settings.push(loadGlobal);
-
-	loadGlobal();
 	loadGlobalOnce();
 
 	console.log("TT: Global - Script loaded.");
 })();
-
-function loadGlobal() {
-	requireChatsLoaded()
-		.then(() => {
-			new MutationObserver((mutations) => {
-				for (let mutation of mutations) {
-					for (let addedNode of mutation.addedNodes) {
-						if (addedNode.classList && addedNode.classList.contains("^=chat-box_")) {
-							setTimeout(() => {
-								addChatUsernameAutocomplete();
-							});
-						}
-					}
-				}
-			}).observe(document.find("#chatRoot"), { childList: true, subtree: true });
-
-			addChatUsernameAutocomplete();
-
-			document.find("#chatRoot").addEventListener("click", (event) => {
-				if (!hasParent(event.target, { class: "^=chat-box_" })) return;
-
-				addChatUsernameAutocomplete();
-			});
-		})
-		.catch((reason) => console.error("TT failed during loading chats.", reason));
-}
 
 function loadGlobalOnce() {
 	document.body.appendChild(document.newElement({ type: "div", class: "tt-overlay hidden" }));
@@ -100,49 +71,4 @@ function showMiniprofileInformation(information) {
 			miniProfile.style.top = `${profileY - heightDifference}px`;
 		}
 	});
-}
-
-function addChatUsernameAutocomplete() {
-	if (!settings.pages.chat.completeUsernames) return;
-
-	for (let chat of document.findAll("[class*='chat-box_']")) {
-		const messageList = chat.find("[class*='overview_']");
-		if (!messageList) continue;
-
-		const textarea = chat.find("[class*='chat-box-textarea_']");
-		if (!textarea || textarea.classList.contains("tt-chat-autocomplete")) continue;
-		textarea.classList.add("tt-chat-autocomplete");
-
-		let currentUsername, currentSearchValue;
-		textarea.addEventListener("keydown", (event) => {
-			if (event.key !== "Tab") {
-				currentUsername = null;
-				currentSearchValue = null;
-				return;
-			}
-			event.preventDefault();
-
-			const valueToCursor = textarea.value.substr(0, textarea.selectionStart);
-			const searchValueMatch = valueToCursor.match(/([^A-Za-z0-9\-_]?)([A-Za-z0-9\-_]*)$/);
-
-			if (currentSearchValue === null) currentSearchValue = searchValueMatch[2].toLowerCase();
-
-			const matchedUsernames = Array.from(messageList.findAll("[class*='message_'] > a"))
-				.map((message) => message.innerText.slice(0, -2))
-				.filter((username, index, array) => array.indexOf(username) === index && username.toLowerCase().includes(currentSearchValue))
-				.sort();
-			if (!matchedUsernames.length) return;
-
-			let index = currentUsername !== null ? matchedUsernames.indexOf(currentUsername) + 1 : 0;
-			if (index > matchedUsernames.length - 1) index = 0;
-
-			currentUsername = matchedUsernames[index];
-
-			let valueStart = searchValueMatch.index + searchValueMatch[1].length;
-			textarea.value = textarea.value.substring(0, valueStart) + currentUsername + textarea.value.substring(valueToCursor.length, textarea.value.length);
-
-			let selectionIndex = valueStart + currentUsername.length;
-			textarea.setSelectionRange(selectionIndex, selectionIndex);
-		});
-	}
 }
---
+++ extension/scripts/features/chatautocomplete/ttChatAutocomplete.js
@@ -0,0 +1,76 @@
+"use strict";
+
+(async () => {
+	let highlights;
+
+	const feature = featureManager.registerFeature(
+		"Chat Autocomplete",
+		"global",
+		() => settings.pages.chat.completeUsernames,
+		initialiseAutocomplete,
+		readSettings,
+		null,
+		{
+			storage: ["settings.pages.chat.completeUsernames"],
+		},
+		null
+	);
+
+	function initialiseAutocomplete() {
+		window.addEventListener(EVENT_CHANNELS.CHAT_OPENED, (event) => {
+			if (!feature.enabled()) return;
+
+			addAutocomplete(event.detail.chat);
+		});
+	}
+
+	async function readSettings() {
+		await requireChatsLoaded();
+
+		for (const chat of document.findAll("[class*='chat-box_']")) {
+			addAutocomplete(chat);
+		}
+	}
+
+	function addAutocomplete(chat) {
+		const messages = chat.find("[class*='overview_']");
+		if (!messages) return;
+
+		const textarea = chat.find("textarea:not(.tt-chat-autocomplete)");
+		if (!textarea) return;
+		textarea.classList.add("tt-chat-autocomplete");
+
+		let currentUsername, currentSearchValue;
+		textarea.addEventListener("keydown", (event) => {
+			if (event.key !== "Tab") {
+				currentUsername = null;
+				currentSearchValue = null;
+				return;
+			}
+			event.preventDefault();
+
+			const valueBeforeCursor = textarea.value.substr(0, textarea.selectionStart);
+			const searchValueMatch = valueBeforeCursor.match(/([^A-Za-z0-9\-_]?)([A-Za-z0-9\-_]*)$/);
+
+			if (currentSearchValue === null) currentSearchValue = searchValueMatch[2].toLowerCase();
+
+			const matchedUsernames = Array.from(messages.findAll("[class*='message_'] > a"))
+				.map((message) => message.innerText.slice(0, -2))
+				.filter((username, index, array) => array.indexOf(username) === index && username.toLowerCase().startsWith(currentSearchValue))
+				.sort();
+			if (!matchedUsernames.length) return;
+
+			let index = currentUsername !== null ? matchedUsernames.indexOf(currentUsername) + 1 : 0;
+			if (index > matchedUsernames.length - 1) index = 0;
+
+			currentUsername = matchedUsernames[index];
+
+			const valueStart = searchValueMatch.index + searchValueMatch[1].length;
+			textarea.value =
+				textarea.value.substring(0, valueStart) + currentUsername + textarea.value.substring(valueBeforeCursor.length, textarea.value.length);
+
+			const selectionIndex = valueStart + currentUsername.length;
+			textarea.setSelectionRange(selectionIndex, selectionIndex);
+		});
+	}
+})();
