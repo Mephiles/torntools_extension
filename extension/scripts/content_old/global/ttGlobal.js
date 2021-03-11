b92d94821b3e8d53a8e4e644c2945777cd061c41
DeKleineKobini <kobedepeuter@outlook.com>
2021-03-11 18:45:18 +0100

    Move mini profile information to it's own feature file

--- extension/scripts/content_old/global/ttGlobal.js
+++ extension/scripts/content_old/global/ttGlobal.js
@@ -29,46 +29,4 @@ function loadGlobalOnce() {
 			countdown.dataset.seconds = seconds;
 		}
 	}, 1000);
-
-	if (settings.pages.global.miniProfileLastAction) {
-		addFetchListener((event) => {
-			if (!event.detail) return;
-			const { page, json, fetch } = event.detail;
-
-			const params = new URL(fetch.url).searchParams;
-			const step = params.get("step");
-
-			if (page === "profiles" && step === "getUserNameContextMenu") {
-				showMiniprofileInformation(json);
-			}
-		});
-		document.addEventListener("mousemove", (event) => {
-			mouseX = event.x;
-			mouseY = event.y;
-		});
-	}
-}
-
-function showMiniprofileInformation(information) {
-	const miniProfile = document.find("#profile-mini-root .mini-profile-wrapper");
-
-	const lastAction = formatTime({ seconds: information.user.lastAction.seconds }, { type: "wordTimer", showDays: true });
-
-	requireElement("div[class*='-profile-mini-_userProfileWrapper']", { parent: miniProfile }).then(() => {
-		const oldHeight = miniProfile.clientHeight;
-		const data = document.newElement({
-			type: "div",
-			class: "tt-mini-data",
-			children: [document.newElement({ type: "strong", text: "Last Action: " }), document.newElement({ type: "span", text: lastAction })],
-		});
-		miniProfile.find("div[class*='-profile-mini-_userProfileWrapper']").appendChild(data);
-
-		const profileBounding = miniProfile.getBoundingClientRect();
-		if (profileBounding.top < mouseY) {
-			const profileY = parseInt(miniProfile.style.top.replace("px", ""));
-			const heightDifference = miniProfile.clientHeight - oldHeight;
-
-			miniProfile.style.top = `${profileY - heightDifference}px`;
-		}
-	});
 }
