77859277b310016b4d6a1557cedff591b7a162cd
DeKleineKobini <kobedepeuter@outlook.com>
2021-03-11 19:31:25 +0100

    Move some global things to the new file.

--- extension/scripts/content/global/ttGlobal.js
+++ extension/scripts/content/global/ttGlobal.js
@@ -10,6 +10,8 @@
 		}
 	});
 
+	document.body.appendChild(document.newElement({ type: "div", class: "tt-overlay hidden" }));
+
 	requireChatsLoaded().then(() => {
 		new MutationObserver((mutations) => {
 			for (let mutation of mutations) {
@@ -29,4 +31,20 @@
 			}
 		}).observe(document.find("#chatRoot"), { childList: true, subtree: true });
 	});
+
+	setInterval(() => {
+		for (let countdown of document.findAll(".countdown.automatic[data-seconds]")) {
+			const seconds = parseInt(countdown.dataset.seconds) - 1;
+
+			if (seconds <= 0) {
+				countdown.removeAttribute("seconds-down");
+				countdown.innerText = "Ready";
+				continue;
+			}
+
+			countdown.innerText = formatTime({ seconds }, JSON.parse(countdown.dataset.timeSettings));
+			// noinspection JSValidateTypes
+			countdown.dataset.seconds = seconds;
+		}
+	}, 1000);
 })();
