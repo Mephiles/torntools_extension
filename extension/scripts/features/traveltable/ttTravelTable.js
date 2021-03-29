8ecc72343b7fc92cccfaf76b84f3645358623086
DeKleineKobini <kobedepeuter@outlook.com>
2021-03-29 21:20:54 +0200

    Update travel data.

--- extension/scripts/features/traveltable/ttTravelTable.js
+++ extension/scripts/features/traveltable/ttTravelTable.js
@@ -36,7 +36,7 @@
 						class: "legend",
 						html: `
 							<div class="top-row">
-								<div class="filter-icon">
+								<div class="legend-icon">
 									<i class="fas fa-chevron-${isOpen ? "down" : "right"}"></i>
 									<span>Filters</span>
 								</div>
@@ -46,19 +46,94 @@
 									<span class="table-type" type="advanced">Advanced</span>
 								</div>
 							</div>
-							<div class="filter-content ${isOpen ? "" : "hidden"}">
-							 	<span>Content</span>
+							<div class="legend-content ${isOpen ? "" : "hidden"}">
+								<div class="row">
+									<div>
+										<label for="travel-items">Travel items: </label>
+										<input id="travel-items" type="number" min="5"/>
+									</div>
+								</div>
+								<div class="heading">Items</div>
+								<div class="row">
+									<div class="checkbox-item">
+										<input id="travel-item-plushies" type="checkbox" name="item" category="plushie">
+										<label for="travel-item-plushies">Plushies</label>
+									</div>
+									<div class="checkbox-item">
+										<input id="travel-item-flowers" type="checkbox" name="item" category="flower">
+										<label for="travel-item-flowers">Flowers</label>
+									</div>
+									<div class="checkbox-item">
+										<input id="travel-item-drugs" type="checkbox" name="item" category="drug">
+										<label for="travel-item-drugs">Drugs</label>
+									</div>
+			                     	<div class="checkbox-item">
+										<input id="travel-item-other" type="checkbox" name="item" category="other">
+										<label for="travel-item-other">Other</label>
+									</div>
+								</div>
+								<div class="heading">Countries</div>
+								<div class="row">
+									<div class="radio-item">
+										<input id="travel-country-all" type="radio" name="travel-country" country="all">
+										<label for="travel-country-all">All</label>
+									</div>
+									<div class="radio-item">
+										<input id=travel-country-mexico" type="radio" name="travel-country" country="mexico">
+										<label for="travel-country-mexico">Mexico</label>
+									</div>
+									<div class="radio-item">
+										<input id="travel-country-caymans" type="radio" name="travel-country" country="cayman islands">
+										<label for="travel-country-caymans">Cayman Islands</label>
+									</div>
+									<div class="radio-item">
+										<input id="travel-country-canada" type="radio" name="travel-country" country="canada">
+										<label for="travel-country-canada">Canada</label>
+									</div>
+									<div class="radio-item">
+										<input id="travel-country-hawaii" type="radio" name="travel-country" country="hawaii">
+										<label for="travel-country-hawaii">Hawaii</label>
+									</div>
+									<div class="radio-item">
+										<input id="travel-country-uk" type="radio" name="travel-country" country="united kingdom">
+										<label for="travel-country-uk">United Kingdom</label>
+									</div>
+									<div class="radio-item">
+										<input id="travel-country-argentina" type="radio" name="travel-country" country="argentina">
+										<label for="travel-country-argentina">Argentina</label>
+									</div>
+									<div class="radio-item">
+										<input id="travel-country-switzerland" type="radio" name="travel-country" country="switzerland">
+										<label for="travel-country-switzerland">Switzerland</label>
+									</div>
+									<div class="radio-item">
+										<input id="travel-country-japan" type="radio" name="travel-country" country="japan">
+										<label for="travel-country-japan">Japan</label>
+									</div>
+									<div class="radio-item">
+										<input id="travel-country-china" type="radio" name="travel-country" country="china">
+										<label for="travel-country-china">China</label>
+									</div>
+									<div class="radio-item">
+										<input id="travel-country-uae" type="radio" name="travel-country" country="uae">
+										<label for="travel-country-uae">UAE</label>
+									</div>
+									<div class="radio-item">
+										<input id="travel-country-sa" type="radio" name="travel-country" country="south africa">
+										<label for="travel-country-sa">South Africa</label>
+									</div>
+								</div>
 							</div>
 						`,
 					})
 				);
 
-				content.find(".filter-icon").addEventListener("click", (event) => {
-					if (event.target.classList.contains("filter-icon")) return;
+				content.find(".legend-icon").addEventListener("click", (event) => {
+					if (event.target.classList.contains("legend-icon")) return;
 
-					const isOpen = !content.find(".filter-content").classList.toggle("hidden");
+					const isOpen = !content.find(".legend-content").classList.toggle("hidden");
 
-					const iconClasses = content.find(".filter-icon i").classList;
+					const iconClasses = content.find(".legend-icon i").classList;
 					if (isOpen) {
 						iconClasses.remove("fa-chevron-right");
 						iconClasses.add("fa-chevron-down");
@@ -89,6 +164,17 @@
 
 					ttStorage.change({ filters: { travel: { type: "advanced" } } });
 				});
+
+				content.find("#travel-items").value = 0;
+
+				function getTravelCount() {
+					let suitcase = 0;
+
+					// if (hasAPIData() && settings.apiUsage.user.perks) {
+					// }
+
+					return suitcase;
+				}
 			}
 		}
 
