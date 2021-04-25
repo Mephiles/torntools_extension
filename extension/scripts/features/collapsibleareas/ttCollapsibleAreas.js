3cfd85e9422bbf19ea2a9d8ec7877536d0efc2f9
Sashank999 <bandi.rao999@gmail.com>
2021-04-25 21:51:58 +0530

    All Changes done

--- extension/scripts/features/collapsibleareas/ttCollapsibleAreas.js
+++ extension/scripts/features/collapsibleareas/ttCollapsibleAreas.js
@@ -11,36 +11,34 @@
 		{
 			storage: ["settings.pages.sidebar.collapseAreas"],
 		},
-		null
+		() => {
+			if (mobile) return "Shouldn't be on mobile";
+		}
 	);
 
 	async function addCollapseIcon() {
 		await requireSidebar();
-		if (!document.find("[class='header-arrow']") && !mobile) {
-			let areasIElement = document.newElement({ type: "i", class: "icon fas fa-caret-down" });
-			let areasHeader = document.find("h2=Areas");
-			areasHeader.classList.add("tt-title-torn");
-			areasHeader.appendChild(areasIElement);
-			if (filters.containers.collapseAreas) areasHeader.classList.add("collapsed");
-			areasHeader.addEventListener("click", clickListener);
-		}
+		const areasIElement = document.newElement({ type: "i", class: "icon fas fa-caret-down" });
+		const areasHeader = document.find("h2=Areas");
+		areasHeader.classList.add("tt-title-torn");
+		areasHeader.appendChild(areasIElement);
+		if (filters.containers.collapseAreas) areasHeader.classList.add("collapsed");
+		areasHeader.addEventListener("click", clickListener);
 	}
 
 	async function removeCollapseIcon() {
 		await requireSidebar();
-		if (!document.find("[class='header-arrow']") && !mobile) {
-			let areasHeader = document.find("h2=Areas");
-			areasHeader.classList.remove("tt-title-torn");
-			areasHeader.find(".icon").remove();
-			if (areasHeader.classList.contains("collapsed")) areasHeader.classList.remove("collapsed");
-			areasHeader.removeEventListener("click", clickListener);
-		}
+		const areasHeader = document.find("h2=Areas");
+		areasHeader.classList.remove("tt-title-torn");
+		areasHeader.find(".icon").remove();
+		if (areasHeader.classList.contains("collapsed")) areasHeader.classList.remove("collapsed");
+		areasHeader.removeEventListener("click", clickListener);
 	}
 
 	async function clickListener() {
-		let areasHeader = document.find("h2=Areas");
+		const areasHeader = document.find("h2=Areas");
 		areasHeader.classList.toggle("collapsed");
-		let collapsed = areasHeader.classList.contains("collapsed");
+		const collapsed = areasHeader.classList.toggle("collapsed");
 		await ttStorage.change({ filters: { containers: { collapseAreas: collapsed } } });
 	}
 })();
