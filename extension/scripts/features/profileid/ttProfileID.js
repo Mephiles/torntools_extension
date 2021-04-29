c4e1e026d398aea4c7fcf3edef5e32ec47795bba
DeKleineKobini <kobedepeuter@outlook.com>
2021-04-29 21:28:16 +0200

    Improve name.

---
+++ extension/scripts/features/profileid/ttProfileID.js
@@ -0,0 +1,32 @@
+"use strict";
+
+(async () => {
+	featureManager.registerFeature(
+		"Profile ID",
+		"profile",
+		() => settings.pages.profile.idBesideProfileName,
+		null,
+		addID,
+		removeID,
+		{
+			storage: ["settings.pages.profile.idBesideProfileName"],
+		},
+		async () => {
+			await requireElement(".basic-info .info-table > *:first-child");
+		}
+	);
+
+	function addID() {
+		const skipToContent = document.find("h4#skip-to-content");
+		skipToContent.innerHTML = `${skipToContent.innerHTML.replace("'s Profile", "")} [${getUserID()}]`;
+	}
+
+	function removeID() {
+		const skipToContent = document.find("h4#skip-to-content");
+		skipToContent.innerText = skipToContent.innerText.replace(/ \[.*\]/g, "") + "'s Profile";
+	}
+
+	function getUserID() {
+		return document.find(".basic-information .profile-container ul.info-table .user-info-value > *:first-child").innerText.replace(/\D+/g, "");
+	}
+})();
