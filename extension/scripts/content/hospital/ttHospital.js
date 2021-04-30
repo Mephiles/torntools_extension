31b3cef809f473e8ed1d37013e80f2c8455923c2
DeKleineKobini <kobedepeuter@outlook.com>
2021-04-30 19:06:08 +0200

    Fix script.

--- extension/scripts/content/hospital/ttHospital.js
+++ extension/scripts/content/hospital/ttHospital.js
@@ -1,14 +1,11 @@
 "use strict";
 
 (async () => {
-	requireElement(".pagination-wrap .page-number:not([style*='display: none'])").then(() => {
-		document.addEventListener("click", (event) => {
-			if (event.target.classList.contains("page-number")) {
-				window.dispatchEvent(
-					new CustomEvent(EVENT_CHANNELS.HOSPITAL_SWITCH_PAGE, null);
-				);
+	document.addEventListener("click", (event) => {
+		if (event.target.classList.contains("page-number")) {
+			requireElement(".user-info-list-wrap > *:not(.last)").then(() => {
 				triggerCustomListener(EVENT_CHANNELS.HOSPITAL_SWITCH_PAGE, null);
-			};
-		})
+			});
+		}
 	});
 })();
