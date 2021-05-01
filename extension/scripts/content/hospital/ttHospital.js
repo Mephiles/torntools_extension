878af5bfb50eccd38a14753f5f8764c0be86edf0
Sashank999 <bandi.rao999@gmail.com>
2021-05-01 10:30:37 +0530

    Remove logging

--- extension/scripts/content/hospital/ttHospital.js
+++ extension/scripts/content/hospital/ttHospital.js
@@ -3,9 +3,7 @@
 (() => {
 	document.addEventListener("click", (event) => {
 		if (event.target.classList.contains("page-number") || event.target.classList.contains("page-nb")) {
-			console.log("hi");
 			requireElement(".user-info-list-wrap .last #iconTray li").then(() => {
-				console.log("hi2");
 				triggerCustomListener(EVENT_CHANNELS.HOSPITAL_SWITCH_PAGE, null);
 			});
 		}
