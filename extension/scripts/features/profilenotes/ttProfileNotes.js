d2aebd550d7fa3d799e28c6d8fbdd41c66fe507c
Sashank999 <bandi.rao999@gmail.com>
2021-05-01 19:44:00 +0530

    All changes done

--- extension/scripts/features/profilenotes/ttProfileNotes.js
+++ extension/scripts/features/profilenotes/ttProfileNotes.js
@@ -12,20 +12,48 @@
 			storage: ["settings.pages.profile.notes"],
 		},
 		async () => {
-			await requireElement(".profile-wrapper.m-top10:not(.medals-wrapper)");
+			await requireElement(".profile-information-wrapper .box-value");
 		}
 	);
 
 	function addNotes() {
+		const userID = getUserID();
 		const { content } = createContainer("Profile Notes", {
-			nextElement: document.find(".profile-wrapper.m-top10:not(.medals-wrapper)"),
+			nextElement: document.find(".profile-wrapper.medals-wrapper"),
 		});
-		const textBox = document.nextElement({ type: "textarea" });
+		const textBox = document.newElement({ type: "textarea" });
+		if (notes.profile[userID].text) textBox.value = notes.profile[userID].text;
+		textBox.style.height = notes.profile[userID] && notes.profile[userID].height ? notes.profile[userID].height : "17px";
 		content.appendChild(textBox);
-		textBox.addEventListener("input", (event) => = event.target.value);
+		textBox.addEventListener("input", (event) => ttStorage.change({
+			notes: {
+				profile: {
+					[userID]: {
+						text: event.target.value,
+						height: event.target.style.height,
+					}
+				}
+			}
+		}));
+		new MutationObserver(() => {
+			ttStorage.change({
+				notes: {
+					profile: {
+						[userID]: {
+							text: findContainer("Profile Notes").find("textarea").value,
+							height: event.target.style.height,
+						}
+					}
+				}
+			});
+		}).observe(textBox, { attributes : true, attributeFilter : ['style'] });
 	}
 
 	function removeNotes() {
-		
+		removeContainer("Profile Notes");
+	}
+
+	function getUserID() {
+		return document.find(".basic-info .user-info-value .bold").innerText.match(/(?<=\[).*(?=\])/g)[0];
 	}
 })();
