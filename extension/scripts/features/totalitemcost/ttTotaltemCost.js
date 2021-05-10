5f221fea1ff487555427a7e16d9fda46027e7ff9
Sashank999 <bandi.rao999@gmail.com>
2021-05-10 09:05:53 +0530

    Now everything works

--- extension/scripts/features/totalitemcost/ttTotaltemCost.js
+++ extension/scripts/features/totalitemcost/ttTotaltemCost.js
@@ -26,18 +26,24 @@
 	function addPrice() {
 		requireElement("[class*='buyMenu_'] [class*='price_']").then(() => {
 			if (document.find("#tt-total-cost")) return;
-			const bazaarItemPrice = parseInt(document.find("[class*='buyMenu_'] [class*='price_']").innerText.split("$")[1].replaceAll(",", ""));
 			document.find("[class*='buyMenu_'] [class*='amount_']").insertAdjacentElement("beforeend", document.newElement({type: "span", id: "tt-total-cost"}));
-			document.find("[class*='buyMenu_'] [class*='buyForm_'] input[class*='numberInput_']").addEventListener(
-				"keyup",
-				(event) => {
-					if (document.find("#tt-total-cost")) document.find("#tt-total-cost").innerHTML = "$" + formatNumber(bazaarItemPrice * event.target.value)
-				},
+			const inputElement = document.find("[class*='buyMenu_'] [class*='buyForm_'] input[class*='numberInput_']");
+			changeTotalPrice(inputElement.value);
+			inputElement.addEventListener(
+				"input",
+				(event) => changeTotalPrice(event.target.value),
 			);
 		});
 	}
 
 	function removePrice() {
 		document.findAll("#tt-total-cost").forEach(x => x.remove());
+	}
+
+	function changeTotalPrice(value) {
+		const bazaarItemStock = parseInt(document.find("[class*='buyMenu_'] [class*='amount_']").innerText.split(")")[0].replace(/\D+/g, ""));
+		const bazaarItemPrice = parseInt(document.find("[class*='buyMenu_'] [class*='price_']").innerText.split("$")[1].replaceAll(",", ""));
+		if (value > bazaarItemStock) value = bazaarItemStock;
+		if (document.find("#tt-total-cost")) document.find("#tt-total-cost").innerHTML = "$" + formatNumber(bazaarItemPrice * value)
 	}
 })();
