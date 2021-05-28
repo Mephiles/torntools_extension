63aaf8eeebd76e92f1ac0c35f7976f778c01c7d0
DeKleineKobini <kobedepeuter@outlook.com>
2021-05-28 13:28:19 +0200

    Improve code.

--- extension/scripts/features/agetowords/ttAgeToWords.js
+++ extension/scripts/features/agetowords/ttAgeToWords.js
@@ -16,34 +16,34 @@
 
 	async function addWords() {
 		await requireElement(".box-info.age .box-value");
+
 		const ageDiv = document.find(".box-info.age");
 		ageDiv.find(".box-name").classList.add("hidden");
 		const age = parseInt(document.find(".box-info.age .box-value").innerText.replace(/\n/g, ""));
 		const dateCurrent = new Date();
 		const utimeTarget = dateCurrent.getTime() + age * 86400 * 1000;
 		const dateTarget = new Date(utimeTarget);
-		let diffYear = parseInt(dateTarget.getUTCFullYear() - dateCurrent.getUTCFullYear());
-		let diffMonth = parseInt(dateTarget.getUTCMonth() - dateCurrent.getUTCMonth());
-		let diffDay = parseInt(dateTarget.getUTCDate() - dateCurrent.getUTCDate());
+		let diffYear = dateTarget.getUTCFullYear() - dateCurrent.getUTCFullYear();
+		let diffMonth = dateTarget.getUTCMonth() - dateCurrent.getUTCMonth();
+		let diffDay = dateTarget.getUTCDate() - dateCurrent.getUTCDate();
 		const daysInMonth = [31, dateTarget.getUTCFullYear() % 4 ? 29 : 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
-		let dateString = "";
+		let dateString;
 		while (true) {
-			dateString = "";
-			dateString += (diffYear > 0) ? `${diffYear} year${diffYear > 1 ? "s " : " "}` : "";
+			dateString = diffYear > 0 ? `${diffYear} year${applyPlural(diffYear)}` : "";
 
 			if (diffMonth < 0) {
 				diffYear -= 1;
 				diffMonth += 12;
 				continue;
 			}
-			dateString += diffMonth > 0 ? `${diffMonth} month${diffMonth > 1 ? "s " : " "}` : "";
+			dateString += diffMonth > 0 ? `${diffMonth} month${applyPlural(diffMonth)}` : "";
 
 			if (diffDay < 0) {
 				diffMonth -= 1;
 				diffDay += daysInMonth[(11 + dateTarget.getUTCMonth()) % 12];
 				continue;
 			}
-			dateString += diffDay > 0 ? `${diffDay} day${diffDay > 1 ? "s " : " "}` : "";
+			dateString += diffDay > 0 ? `${diffDay} day${applyPlural(diffDay)}` : "";
 			break;
 		}
 		ageDiv.find(".block-value").insertAdjacentElement(
