1e6ec62258ffd13b0228eebf81efbe5761eed41c
Sashank999 <76554862+Sashank999@users.noreply.github.com>
2021-06-09 23:28:14 +0530

    Custom links mobile (#217)
    * Hover Effect + Sorting in Settings gives white border
    
    * Custom links for mobile - Initial
    
    * Complete
    
    * Background color change
    
    * Add slight padding to the mobile custom links.
    
    Co-authored-by: DeKleineKobini <kobedepeuter@outlook.com>

--- extension/scripts/features/custom-links/ttCustomLinks.js
+++ extension/scripts/features/custom-links/ttCustomLinks.js
@@ -22,82 +22,108 @@
 			showOutside("under", "customLinksUnder");
 			showInside();
 		} else {
-			// FIXME - Mobile custom links.
-		}
+			const oldCustomLinksContainer = document.find(".tt-custom-links-container");
+			if (oldCustomLinksContainer) oldCustomLinksContainer.remove();
 
-		function showOutside(filter, id) {
-			if (!settings.customLinks.filter((link) => link.location === filter).length) {
-				removeContainer("Custom Links", { id });
-				return;
-			}
-
-			const { content } = createContainer("Custom Links", {
-				id,
-				class: "tt-custom-link-container",
-				applyRounding: false,
-				contentBackground: false,
-				[filter === "above" ? "nextElement" : "previousElement"]: findParent(document.find("h2=Areas"), { class: "^=sidebar-block_" }),
+			const customLinksContainer = document.newElement({
+				type: "div",
+				class: "tt-custom-links-container",
 			});
 
-			for (const link of settings.customLinks.filter((link) => link.location === filter)) {
-				content.appendChild(
-					document.newElement({
-						type: "div",
-						class: "pill",
-						children: [
-							document.newElement({
-								type: "a",
-								href: link.href,
-								text: link.name,
-								attributes: {
-									target: link.newTab ? "_blank" : "_self",
-								},
-							}),
-						],
-					})
+			settings.customLinks.forEach((link) => {
+				customLinksContainer.insertAdjacentHTML(
+					"beforeend",
+					`<div class="tt-slide">
+						<a href="${link.href}" class="tt-mobile-link" target="${link.newTab ? "_blank" : ""}">
+							<span>${link.name}</span>
+						</a>
+					</div>`
 				);
-			}
+			});
+
+			document.find("#sidebar [class*='content_'] [class*='user-information-mobile_']").insertAdjacentElement("beforebegin", customLinksContainer);
+
+			document.find(".content-wrapper[role='main']").insertAdjacentHTML("afterbegin", "<div class='dummy-div'></div>");
+		}
+	}
+
+	function showOutside(filter, id) {
+		if (!settings.customLinks.filter((link) => link.location === filter).length) {
+			removeContainer("Custom Links", { id });
+			return;
 		}
 
-		function showInside() {
-			for (const link of document.findAll(".custom-link")) link.remove();
+		const { content } = createContainer("Custom Links", {
+			id,
+			class: "tt-custom-link-container",
+			applyRounding: false,
+			contentBackground: false,
+			[filter === "above" ? "nextElement" : "previousElement"]: findParent(document.find("h2=Areas"), { class: "^=sidebar-block_" }),
+		});
 
-			const areas = findParent(document.find("h2=Areas"), { class: "^=sidebar-block_" });
-			for (const link of settings.customLinks.filter((link) => link.location !== "above" && link.location !== "under")) {
-				const locationSplit = link.location.split("_");
-
-				const location = locationSplit.splice(1).join("_");
-				const area = ALL_AREAS.filter((area) => area.class === location);
-				if (!area) continue;
-				let target = areas.find(`#nav-${area[0].class}`);
-				if (!target) continue;
-
-				if (locationSplit[0] === "under") target = target.nextSibling;
-
-				const pill = document.newElement({
+		for (const link of settings.customLinks.filter((link) => link.location === filter)) {
+			content.appendChild(
+				document.newElement({
 					type: "div",
-					class: "pill custom-link",
+					class: "pill",
 					children: [
 						document.newElement({
 							type: "a",
 							href: link.href,
 							text: link.name,
 							attributes: {
-								target: link.newTab ? "_blank" : "",
+								target: link.newTab ? "_blank" : "_self",
 							},
 						}),
 					],
-				});
-				const parent = areas.find("div[class*='toggle-content_']");
-				if (target) parent.insertBefore(pill, target);
-				else parent.appendChild(pill);
-			}
+				})
+			);
+		}
+	}
+
+	function showInside() {
+		for (const link of document.findAll(".custom-link")) link.remove();
+
+		const areas = findParent(document.find("h2=Areas"), { class: "^=sidebar-block_" });
+		for (const link of settings.customLinks.filter((link) => link.location !== "above" && link.location !== "under")) {
+			const locationSplit = link.location.split("_");
+
+			const location = locationSplit.splice(1).join("_");
+			const area = ALL_AREAS.filter((area) => area.class === location);
+			if (!area) continue;
+			let target = areas.find(`#nav-${area[0].class}`);
+			if (!target) continue;
+
+			if (locationSplit[0] === "under") target = target.nextSibling;
+
+			const pill = document.newElement({
+				type: "div",
+				class: "pill custom-link",
+				children: [
+					document.newElement({
+						type: "a",
+						href: link.href,
+						text: link.name,
+						attributes: {
+							target: link.newTab ? "_blank" : "",
+						},
+					}),
+				],
+			});
+			const parent = areas.find("div[class*='toggle-content_']");
+			if (target) parent.insertBefore(pill, target);
+			else parent.appendChild(pill);
 		}
 	}
 
 	function removeLinks() {
-		removeContainer("Custom Links", { id: "customLinksAbove" });
-		removeContainer("Custom Links", { id: "customLinksUnder" });
+		if (mobile) {
+			const customLinksContainer = document.find(".tt-custom-links-container");
+			if (customLinksContainer) customLinksContainer.remove();
+		} else {
+			removeContainer("Custom Links", { id: "customLinksAbove" });
+			removeContainer("Custom Links", { id: "customLinksUnder" });
+		}
 
 		for (const link of document.findAll(".custom-link")) link.remove();
 	}
