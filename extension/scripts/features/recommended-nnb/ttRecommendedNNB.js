a7b5eed4b921e2a003572b5dd7d1d7aadda1d886
DeKleineKobini <kobedepeuter@outlook.com>
2021-06-17 12:41:34 +0200

    Multiple fixes for recommended nnb. (#266)
    * Fix line-height on mobile.
    
    * Remove recommended nnb when disabling the feature.
    
    * Start recommended nnb.
    
    * Update recommended nnb.

--- extension/scripts/features/recommended-nnb/ttRecommendedNNB.js
+++ extension/scripts/features/recommended-nnb/ttRecommendedNNB.js
@@ -11,8 +11,8 @@
 		"faction",
 		() => settings.pages.faction.recommendedNnb,
 		initialiseListeners,
-		null,
-		null,
+		startFeature,
+		removeRecommendedNNB,
 		{
 			storage: ["settings.pages.faction.recommendedNnb"],
 		},
@@ -27,7 +27,7 @@
 		"Bomb Threat": "25-30",
 		"Planned Robbery": "30-40",
 		"Rob a money train": "40-50",
-		"Take over a cruise liner": "40-50",
+		"Take over a cruise liner": "40-55",
 		"Hijack a plane": "55-60",
 		"Political Assassination": "~60",
 	};
@@ -40,14 +40,30 @@
 		});
 	}
 
+	function startFeature() {
+		if (!document.find(".faction-crimes-wrap")) return;
+
+		showRecommendedNNB();
+	}
+
 	async function showRecommendedNNB() {
 		const parent = document.find(".faction-crimes-wrap .begin-wrap");
+		parent.classList.add("tt-modified");
 
 		const heading = parent.find(".plan-crimes[role=heading]");
 		heading.appendChild(document.newElement({ type: "span", class: "tt-recommended-nnb", text: mobile ? "NNB" : "Recommended NNB" }));
 
 		for (const crime of parent.findAll(".crimes-list .item-wrap .plan-crimes")) {
 			crime.appendChild(document.newElement({ type: "span", class: "tt-recommended-nnb", text: ORGANIZED_CRIMES[crime.innerText] }));
+		}
+	}
+
+	function removeRecommendedNNB() {
+		const parent = document.find(".faction-crimes-wrap .begin-wrap");
+		parent.classList.remove("tt-modified");
+
+		for (const nnb of document.findAll(".tt-recommended-nnb")) {
+			nnb.remove();
 		}
 	}
 })();
