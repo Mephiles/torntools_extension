269920ec40b5fe364ddc137b7ff0282e0759630b
DeKleineKobini <kobedepeuter@outlook.com>
2021-07-05 16:16:01 +0200

    Cleanup code.

--- extension/scripts/features/csv-challenge-contributions/ttCSVChallengeContributions.js
+++ extension/scripts/features/csv-challenge-contributions/ttCSVChallengeContributions.js
@@ -17,7 +17,7 @@
 	);
 
 	function addListener() {
-		addXHRListener(({ detail: { page, xhr, json } }) => {
+		addXHRListener(({ detail: { page, json } }) => {
 			if (!json || !json.contributors || page !== "factions") return;
 			if (feature.enabled()) addCSVContainer();
 		});
@@ -25,49 +25,51 @@
 
 	async function addCSVContainer() {
 		if (document.find(".faction-tabs .ui-tabs-active").dataset.case !== "upgrades") return;
+
 		await requireElement("#factions #faction-upgrades .body #stu-confirmation .description-wrap");
+
 		const descriptionWrap = document.find("#factions #faction-upgrades .body #stu-confirmation .description-wrap");
 		const contributionsWrap = descriptionWrap.find(".contributions-wrap");
-		if (!findContainer("Export Challenge Contributions") && contributionsWrap) {
-			const exportButtonDiv = document.newElement({
-				type: "div",
-				id: "ttExportButton",
-				children: [
-					document.newElement({ type: "i", class: "fa fa-table" }),
-					document.newElement({ type: "span", class: "text", text: "CSV" }),
-					document.newElement({ type: "a", id: "ttExportLink" }),
-				],
+		if (!contributionsWrap) return;
+
+		const exportButtonDiv = document.newElement({
+			type: "div",
+			id: "ttExportButton",
+			children: [
+				document.newElement({ type: "i", class: "fa fa-table" }),
+				document.newElement({ type: "span", class: "text", text: "CSV" }),
+				document.newElement({ type: "a", id: "ttExportLink" }),
+			],
+		});
+		createContainer("Export Challenge Contributions", {
+			nextElement: contributionsWrap,
+			onlyHeader: true,
+			applyRounding: false,
+			collapsible: false,
+		}).options.appendChild(exportButtonDiv);
+		descriptionWrap.find("#ttExportButton").addEventListener("click", () => {
+			const upgradeName = descriptionWrap.find("[role='alert'] .name").innerText;
+			let totalTable = "data:text/csv;charset=utf-8," + "Number;Name;Profile Link;Ex Member;Contributions\r\n" + upgradeName + "\r\n";
+			contributionsWrap.findAll(".flexslides li:not(.slide)").forEach((memberLi) => {
+				const memberName = memberLi.find(".player a");
+				const memberLabel = memberName.ariaLabel;
+				totalTable +=
+					memberLi.find(".numb").innerText +
+					";" +
+					memberLabel.match(/.*(?= \()/)[0] +
+					";" +
+					memberName.href +
+					";" +
+					(memberLi.classList.contains("ex-member") ? "No" : "Yes") +
+					";" +
+					memberLabel.match(/(?<= \().*(?=\))/)[0] +
+					"\r\n";
 			});
-			createContainer("Export Challenge Contributions", {
-				nextElement: contributionsWrap,
-				onlyHeader: true,
-				applyRounding: false,
-				collapsible: false,
-			}).options.appendChild(exportButtonDiv);
-			descriptionWrap.find("#ttExportButton").addEventListener("click", () => {
-				const upgradeName = descriptionWrap.find("[role='alert'] .name").innerText;
-				let totalTable = "data:text/csv;charset=utf-8," + "Number;Name;Profile Link;Ex Member;Contributions\r\n" + upgradeName + "\r\n";
-				contributionsWrap.findAll(".flexslides li:not(.slide)").forEach((memberLi) => {
-					const memberName = memberLi.find(".player a");
-					const memberLabel = memberName.ariaLabel;
-					totalTable +=
-						memberLi.find(".numb").innerText +
-						";" +
-						memberLabel.match(/.*(?= \()/)[0] +
-						";" +
-						memberName.href +
-						";" +
-						(memberLi.classList.contains("ex-member") ? "No" : "Yes") +
-						";" +
-						memberLabel.match(/(?<= \().*(?=\))/)[0] +
-						"\r\n";
-				});
-				const ttExportLink = descriptionWrap.find("#ttExportButton #ttExportLink");
-				ttExportLink.setAttribute("href", encodeURI(totalTable));
-				ttExportLink.setAttribute("download", `${upgradeName} Contributors.csv`);
-				ttExportLink.click();
-			});
-		}
+			const ttExportLink = descriptionWrap.find("#ttExportButton #ttExportLink");
+			ttExportLink.setAttribute("href", encodeURI(totalTable));
+			ttExportLink.setAttribute("download", `${upgradeName} Contributors.csv`);
+			ttExportLink.click();
+		});
 	}
 
 	function removeCSVContainer() {
