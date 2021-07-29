cdf53f8640b53666fe47b787f472ac69220192ab
Sashank999 <76554862+Sashank999@users.noreply.github.com>
2021-07-30 00:52:26 +0530

    Fix NaN Racing Issue (#381)

--- extension/scripts/features/car-win-percentage/ttCarWinPercentage.js
+++ extension/scripts/features/car-win-percentage/ttCarWinPercentage.js
@@ -26,11 +26,9 @@
 		await requireElement(".enlisted-stat");
 		if (document.find(".tt-win-percentage")) return;
 		document.findAll(".enlist-info").forEach((stat) => {
-			const values = stat
-				.find(".enlisted-stat")
-				.innerText.split("• ")
-				.filter((x) => x.includes("Races"))
-				.map((x) => x.split(":")[1].trim());
+			const values = stat.find(".enlisted-stat").innerText.match(/(?<=• Races won: )\d*(?= • W)|(?<=• Races entered: )\d*(?= • )/g);
+			const percentage = ((values[0] / values[1]) * 100).toFixed(2);
+			if (percentage !== "NaN") 
 			stat.find(".enlisted-stat").insertAdjacentHTML(
 				"beforeend",
 				`<li class="tt-win-percentage">• Win Percentage: ${((values[0] / values[1]) * 100).toFixed(2)}% </li>`
