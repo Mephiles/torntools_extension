8c6c7ef5a29975f4ac90b90fa04554203282ddbc
DeKleineKobini <DeKleineKobini@users.noreply.github.com>
2021-07-29 19:22:51 +0000

    Apply prettier changes.

--- extension/scripts/features/car-win-percentage/ttCarWinPercentage.js
+++ extension/scripts/features/car-win-percentage/ttCarWinPercentage.js
@@ -28,11 +28,11 @@
 		document.findAll(".enlist-info").forEach((stat) => {
 			const values = stat.find(".enlisted-stat").innerText.match(/(?<=• Races won: )\d*(?= • W)|(?<=• Races entered: )\d*(?= • )/g);
 			const percentage = ((values[0] / values[1]) * 100).toFixed(2);
-			if (percentage !== "NaN") 
-			stat.find(".enlisted-stat").insertAdjacentHTML(
-				"beforeend",
-				`<li class="tt-win-percentage">• Win Percentage: ${((values[0] / values[1]) * 100).toFixed(2)}% </li>`
-			);
+			if (percentage !== "NaN")
+				stat.find(".enlisted-stat").insertAdjacentHTML(
+					"beforeend",
+					`<li class="tt-win-percentage">• Win Percentage: ${((values[0] / values[1]) * 100).toFixed(2)}% </li>`
+				);
 		});
 	}
 
