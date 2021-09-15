e511a77af9cffd74e6e9dc406c70f84651041d88
DeKleineKobini <kobedepeuter@outlook.com>
2021-09-15 15:57:31 +0200

    Hide tutorials. (#441)

---
+++ extension/scripts/features/hide-tutorials/ttHideTutorials.entry.js
@@ -0,0 +1,20 @@
+"use strict";
+
+(async () => {
+	featureManager.registerFeature(
+		"Hide Tutorials",
+		"global",
+		() => settings.pages.global.hideTutorials,
+		undefined,
+		applyStyle,
+		applyStyle,
+		{
+			storage: ["settings.pages.global.hideTutorials"],
+		},
+		undefined
+	);
+
+	async function applyStyle() {
+		document.documentElement.style.setProperty("--torntools-hide-tutorials", settings.pages.global.hideTutorials ? "none" : "flex");
+	}
+})();
