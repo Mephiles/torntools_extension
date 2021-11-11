467b2921d9286cbc224ebb00286442bfa68a0aba
Sashank999 <76554862+Sashank999@users.noreply.github.com>
2021-11-11 14:57:43 +0530

    Fix Hide Level Upgrade hiding Bank Message. (#583)
    * Fix Hide Level Upgrade hiding Bank Message.
    
    * Fix empty info box - Hide Level Upgrade.

--- extension/scripts/features/hide-level-upgrade/ttHideLevelUpgrade.js
+++ extension/scripts/features/hide-level-upgrade/ttHideLevelUpgrade.js
@@ -6,10 +6,12 @@
 	async function hideUpgrade() {
 		await requireContent();
 
-		for (const info of document.findAll(".info-msg-cont")) {
-			if (!info.textContent.includes("Congratulations! You have enough experience to go up to level")) continue;
-
-			info.classList.add("tt-level-upgrade");
+		for (const li of document.findAll(".info-msg li")) {
+			if (li.textContent.includes("Congratulations! You have enough experience to go up to level")) {
+				if (li.parentElement.childElementCount > 1) li.classList.add("tt-level-upgrade");
+				else li.closest(".info-msg").classList.add("tt-level-upgrade");
+				break;
+			}
 		}
 	}
 
