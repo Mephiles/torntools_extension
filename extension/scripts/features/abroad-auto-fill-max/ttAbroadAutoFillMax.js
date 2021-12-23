e10aee3117a31d9c351edd0a34749d2b0cc93072
Kobe De Peuter <kobe.depeuter@inetum-realdolmen.world>
2021-12-23 12:18:50 +0100

    Fix auto max buy while abroad.

--- extension/scripts/features/abroad-auto-fill-max/ttAbroadAutoFillMax.js
+++ extension/scripts/features/abroad-auto-fill-max/ttAbroadAutoFillMax.js
@@ -21,21 +21,26 @@
 
 		const money = document.find(".info-msg-cont .msg .bold:nth-of-type(2)").textContent.getNumber();
 		if (money === 0) return;
-		const usedCapacity = document.find(".info-msg-cont .msg .bold:nth-of-type(3)").textContent.getNumber();
-		const maxTravelCapacity = document.find(".info-msg-cont .msg .bold:nth-of-type(4)").textContent.getNumber();
-		const travelCapacity = maxTravelCapacity - usedCapacity;
-		if (travelCapacity === 0) return;
+
+		const boughtItems = document.find(".info-msg-cont .msg .bold:nth-of-type(3)").textContent.getNumber();
+		const travelCapacity = document.find(".info-msg-cont .msg .bold:nth-of-type(4)").textContent.getNumber();
+
+		const leftCapacity = travelCapacity - boughtItems;
+		if (leftCapacity === 0) return;
+
 		document.findAll(".users-list > li").forEach((item) => {
 			const stock = item.find(".stck-amount").textContent.getNumber();
 			const price = item.find(".c-price").textContent.getNumber();
-			const input = item.find("input[id]");
 
-			const buyableStock = (money / price).dropDecimals();
-			if (buyableStock === 0 || buyableStock === 1) return;
-			const max = Math.min(stock, buyableStock, travelCapacity);
+			const affordableStock = (money / price).dropDecimals();
+			if (affordableStock === 0 || affordableStock === 1) return;
+
+			const max = Math.min(stock, affordableStock, leftCapacity);
+
+			const input = item.find("input[id]");
 			input.value = max;
 			input.setAttribute("value", max);
-			input.dispatchEvent(new Event("keyup"));
+			input.dispatchEvent(new Event("blur"));
 		});
 	}
 })();
