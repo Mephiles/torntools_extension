a67bda8a9b5c12a9568b143476bcbae7a2e4a20f
Sashank999 <76554862+Sashank999@users.noreply.github.com>
2022-02-06 16:04:27 +0530

    Fix Hospital Filter when switching pages.

--- extension/scripts/content/hospital/ttHospital.js
+++ extension/scripts/content/hospital/ttHospital.js
@@ -2,7 +2,7 @@
 
 (() => {
 	document.addEventListener("click", (event) => {
-		if (event.target.classList.contains("page-number") || event.target.classList.contains("page-nb")) {
+		if (event.target.classList.contains("page-number") || event.target.className.includes("pagination")) {
 			requireElement(".user-info-list-wrap .last #iconTray li").then(() => {
 				triggerCustomListener(EVENT_CHANNELS.HOSPITAL_SWITCH_PAGE, {});
 			});
