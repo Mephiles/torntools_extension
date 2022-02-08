1169c4d6985f1755aa37925ec591ea51215ec080
Sashank999 <76554862+Sashank999@users.noreply.github.com>
2022-02-08 19:47:20 +0530

    Fix Jail Page Changing.

--- extension/scripts/content/jail/ttJail.js
+++ extension/scripts/content/jail/ttJail.js
@@ -2,7 +2,11 @@
 
 (() => {
 	document.addEventListener("click", (event) => {
-		if (event.target.classList.contains("page-number") || event.target.className.includes("pagination")) {
+		if (
+			event.target.classList.contains("page-number") ||
+			event.target.classList.contains("page-nb") ||
+			event.target.className.includes("pagination")
+		) {
 			requireElement(".user-info-list-wrap .last #iconTray li").then(() => {
 				triggerCustomListener(EVENT_CHANNELS.JAIL_SWITCH_PAGE, null);
 			});
