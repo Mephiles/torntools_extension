ff27f3ca9b32b3146ee17fae014d53b451ff9a2e
Sashank999 <bandi.rao999@gmail.com>
2022-02-24 20:02:11 +0530

    Ranked War Filter w/o Stats Estimates.

--- extension/scripts/features/ranked-war-filter/ttRankedWarFilter.js
+++ extension/scripts/features/ranked-war-filter/ttRankedWarFilter.js
@@ -4,28 +4,29 @@
 	if (!getPageStatus().access) return;
 
 	const feature = featureManager.registerFeature(
-		"Userlist Filter",
-		"userlist",
-		() => settings.pages.userlist.filter,
+		"Ranked War Filter",
+		"faction",
+		() => settings.pages.faction.rankedWarFilter,
 		initialiseFilters,
 		addFilters,
 		removeFilters,
 		{
-			storage: ["settings.pages.userlist.filter"],
+			storage: ["settings.pages.faction.rankedWarFilter"],
 		},
 		null
 	);
 
 	function initialiseFilters() {
-		CUSTOM_LISTENERS[EVENT_CHANNELS.USERLIST_SWITCH_PAGE].push(() => {
-			if (!feature.enabled()) return;
-
-			filtering(false);
+		document.addEventListener("click", async (event) => {
+			const rankedWarItem = event.target.closest("[class*='warListItem__']");
+			if (rankedWarItem && rankedWarItem.find(":scope > [data-warid]")) {
+				addFilters(await requireElement(".descriptions .faction-war", { parent: rankedWarItem.parentElement }));
+			}
 		});
 		CUSTOM_LISTENERS[EVENT_CHANNELS.STATS_ESTIMATED].push(({ row }) => {
 			if (!feature.enabled()) return;
 
-			const content = findContainer("Userlist Filter", { selector: "main" });
+			const content = findContainer("Ranked War Filter", { selector: "main" });
 			const statsEstimates = localFilters["Stats Estimate"]?.getSelections(content);
 			if (!statsEstimates?.length) return;
 
@@ -34,15 +35,17 @@
 	}
 
 	const localFilters = {};
-	async function addFilters() {
-		await requireElement(".userlist-wrapper .user-info-list-wrap");
+	async function addFilters(rankedWarList) {
+		if (!rankedWarList) return;
 
-		const { content } = createContainer("Userlist Filter", {
-			class: "mt10",
-			nextElement: document.find(".users-list-title"),
+		const { content } = createContainer("Ranked War Filter", {
+			nextElement: rankedWarList,
 			compact: true,
 			filter: true,
+			applyRounding: false,
 		});
+
+		content.closest(".tt-filter").style = "background-color: #333; padding-top: 10px;";
 
 		const statistics = createStatistics("players");
 		content.appendChild(statistics.element);
@@ -55,7 +58,7 @@
 
 		const activityFilter = createFilterSection({
 			type: "Activity",
-			defaults: filters.userlist.activity,
+			defaults: filters.factionRankedWar.activity,
 			callback: () => filtering(true),
 		});
 		filterContent.appendChild(activityFilter.element);
@@ -66,11 +69,10 @@
 			checkboxes: [
 				{ id: "okay", description: "Okay" },
 				{ id: "hospital", description: "Hospital" },
-				{ id: "jail", description: "Jail" },
 				{ id: "traveling", description: "Traveling" },
 			],
-			defaults: filters.faction.status,
-			callback: applyFilter,
+			defaults: filters.factionRankedWar.status,
+			callback: () => filtering(true),
 		});
 		filterContent.appendChild(statusFilter.element);
 		localFilters["Status"] = { getSelections: statusFilter.getSelections };
@@ -82,8 +84,8 @@
 				min: 1,
 				max: 100,
 				step: 1,
-				valueLow: filters.userlist.levelStart,
-				valueHigh: filters.userlist.levelEnd,
+				valueLow: filters.factionRankedWar.levelStart,
+				valueHigh: filters.factionRankedWar.levelEnd,
 			},
 			callback: () => filtering(true),
 		});
@@ -98,7 +100,7 @@
 					...RANK_TRIGGERS.stats.map((trigger) => ({ id: trigger, description: trigger })),
 					{ id: "n/a", description: "N/A" },
 				],
-				defaults: filters.userlist.estimates,
+				defaults: filters.factionRankedWar.estimates,
 				callback: () => filtering(true),
 			});
 			filterContent.appendChild(estimatesFilter.element);
@@ -112,12 +114,11 @@
 	}
 
 	async function filtering(includeEstimates) {
-		await requireElement(".user-info-list-wrap");
-		await requireElement(".user-info-list-wrap .ajax-placeholder", { invert: true });
+		const membersWrap = await requireElement(".faction-war[class*='membersWrap__']");
 
-		const content = findContainer("Userlist Filter", { selector: "main" });
+		const content = findContainer("Ranked War Filter");
 		const activity = localFilters["Activity"].getSelections(content);
-		const special = localFilters["Special"].getSelections(content);
+		const status = localFilters["Status"].getSelections(content);
 		const levels = localFilters["Level Filter"].getStartEnd(content);
 		const levelStart = parseInt(levels.start);
 		const levelEnd = parseInt(levels.end);
@@ -126,75 +127,53 @@
 				? localFilters["Stats Estimate"]?.getSelections(content)
 				: undefined;
 
-		// Update level and time slider counters
+		// Update level slider counters
 		localFilters["Level Filter"].updateCounter(`Level ${levelStart} - ${levelEnd}`, content);
 
 		// Save filters
 		await ttStorage.change({
 			filters: {
-				userlist: {
+				factionRankedWar: {
 					activity: activity,
 					levelStart: levelStart,
 					levelEnd: levelEnd,
-					special: special,
+					status: status,
 					estimates: statsEstimates ?? filters.userlist.estimates,
 				},
 			},
 		});
 
 		// Actual Filtering
-		for (const li of document.findAll(".user-info-list-wrap > li")) {
-			filterRow(li, { activity, special, level: { start: levelStart, end: levelEnd }, statsEstimates }, false);
+		for (const li of membersWrap.findAll(".members-list > li")) {
+			filterRow(li, { activity, status, level: { start: levelStart, end: levelEnd }, statsEstimates }, false);
 		}
 
 		triggerCustomListener(EVENT_CHANNELS.FILTER_APPLIED);
 
 		localFilters["Statistics"].updateStatistics(
-			document.findAll(".user-info-list-wrap > li:not(.tt-hidden)").length,
-			document.findAll(".user-info-list-wrap > li").length,
+			membersWrap.findAll(".members-list > li:not(.tt-hidden)").length,
+			membersWrap.findAll(".members-list > li").length,
 			content
 		);
 	}
 
 	function filterRow(row, filters, individual) {
 		if (filters.activity) {
-			if (
-				filters.activity.length &&
-				!filters.activity.some(
-					(x) =>
-						x.trim() ===
-						row
-							.find("#iconTray li")
-							.getAttribute("title")
-							.match(/(?<=<b>).*(?=<\/b>)/g)[0]
-							.toLowerCase()
-							.trim()
-				)
-			) {
+			const activity = row.find(".member.icons [class*='userStatusWrap___']").id.split("_")[1].split("-")[0].trim();
+			if (filters.activity.length && !filters.activity.some((x) => x.trim() === activity)) {
 				hide("activity");
 				return;
 			}
 		}
-		if (filters.special) {
-			const match = Object.entries(filters.special)
-				.filter(([, value]) => value !== "both" && value !== "none")
-				.find(([key, value]) => {
-					const icons = getSpecialIcons(row);
-					const filterIcons = SPECIAL_FILTER_ICONS[key];
-
-					return (
-						(value === "yes" && !icons.some((foundIcon) => filterIcons.includes(foundIcon))) ||
-						(value === "no" && icons.some((foundIcon) => filterIcons.includes(foundIcon)))
-					);
-				});
-
-			if (match) {
-				hide(`special-${match[0]}`);
+		if (filters.status.length) {
+			if (!filters.status.includes(row.find(".status").textContent.toLowerCase().trim()))
+			{
+				hide("status");
 				return;
 			}
 		}
 		if (filters.level) {
-			const level = parseInt(row.find(".level .value").textContent);
+			const level = parseInt(row.find(".level").textContent);
 			if ((filters.level.start && level < filters.level.start) || (filters.level.end !== 100 && level > filters.level.end)) {
 				hide("level");
 				return;
@@ -221,7 +200,7 @@
 			}
 
 			if (individual) {
-				const content = findContainer("Userlist Filter", { selector: "main" });
+				const content = findContainer("Ranked War Filter", { selector: "main" });
 
 				localFilters["Statistics"].updateStatistics(
 					document.findAll(".user-info-list-wrap > li:not(.tt-hidden)").length,
@@ -240,7 +219,7 @@
 			}
 
 			if (individual) {
-				const content = findContainer("Userlist Filter", { selector: "main" });
+				const content = findContainer("Ranked War Filter", { selector: "main" });
 
 				localFilters["Statistics"].updateStatistics(
 					document.findAll(".user-info-list-wrap > li:not(.tt-hidden)").length,
@@ -252,7 +231,7 @@
 	}
 
 	function removeFilters() {
-		removeContainer("Userlist Filter");
-		document.findAll(".user-info-list-wrap > li.tt-hidden").forEach((x) => x.classList.remove("tt-hidden"));
+		removeContainer("Ranked War Filter");
+		document.findAll(".faction-war[class*='membersWrap__'] .tt-hidden").forEach((x) => x.classList.remove("tt-hidden"));
 	}
 })();
