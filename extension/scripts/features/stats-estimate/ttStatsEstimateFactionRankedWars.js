eb444c64d33d1a97072f7c8b7d88bcc18a9d1567
Sashank999 <bandi.rao999@gmail.com>
2022-03-07 16:02:16 +0530

    Fix RW Stats Est.s Torn is sorting.

--- extension/scripts/features/stats-estimate/ttStatsEstimateFactionRankedWars.js
+++ extension/scripts/features/stats-estimate/ttStatsEstimateFactionRankedWars.js
@@ -40,43 +40,15 @@
 	function observeWars() {
 		if (observer) observer.disconnect();
 
-		if (window.location.hash.includes("/war/rank")) requireElement(".f-war-list > .descriptions").then(observeDescription);
+		if (window.location.hash.includes("/war/rank")) requireElement(".f-war-list > .descriptions").then(() => showEstimates());
 
 		requireElement("ul.f-war-list").then(() => {
 			observer = new MutationObserver((mutations) => {
 				if (![...mutations].some((mutation) => [...(mutation.addedNodes ?? [])].some((node) => node.classList?.contains("descriptions")))) return;
 
-				observeDescription();
+				showEstimates();
 			});
 			observer.observe(document.find("ul.f-war-list"), { childList: true });
-		});
-	}
-
-	function observeDescription() {
-		showEstimates();
-
-		requireElement(".faction-war .members-list").then(() => {
-			new MutationObserver((mutations) => {
-				let shouldEstimate = false;
-
-				for (const mutation of mutations) {
-					for (const node of mutation.removedNodes) {
-						if (!node.classList?.contains("tt-estimated")) continue;
-
-						node.classList.remove("tt-estimated");
-						mutation.nextSibling?.remove();
-					}
-
-					for (const node of mutation.addedNodes) {
-						if (!node.classList?.contains("your") && !node.classList?.contains("enemy")) continue;
-
-						shouldEstimate = true;
-						break;
-					}
-				}
-
-				if (shouldEstimate) showEstimates();
-			}).observe(document.find(".faction-war .members-list"), { childList: true });
 		});
 	}
 
@@ -92,7 +64,7 @@
 					};
 				},
 				true,
-				undefined
+				(row) => row.find(".clear")
 			);
 		});
 	}
