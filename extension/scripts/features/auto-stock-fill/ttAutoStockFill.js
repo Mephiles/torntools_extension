db0fc51c8a9513aa359b98e3e79a00ad0a38e489
Sashank999 <bandi.rao999@gmail.com>
2022-05-30 20:50:31 +0530

    Change Auto Stock Fill to have a button.

--- extension/scripts/features/auto-stock-fill/ttAutoStockFill.js
+++ extension/scripts/features/auto-stock-fill/ttAutoStockFill.js
@@ -4,21 +4,42 @@
 	if (!getPageStatus().access) return;
 	if (!isOwnCompany) return;
 
-	const feature = featureManager.registerFeature("Auto Fill Stock", "companies", () => settings.pages.companies.autoStockFill, addListener, fillStock, null, {
-		storage: ["settings.pages.companies.autoStockFill"],
-	});
+	const feature = featureManager.registerFeature(
+		"Auto Fill Stock",
+		"companies",
+		() => settings.pages.companies.autoStockFill,
+		addListener,
+		addFillStockBtn,
+		() => document.findAll(".tt-fill-stock").forEach(x => x.remove()),
+		{
+			storage: ["settings.pages.companies.autoStockFill"],
+		}
+	);
 
 	function addListener() {
 		CUSTOM_LISTENERS[EVENT_CHANNELS.COMPANY_STOCK_PAGE].push(async () => {
 			if (!feature.enabled) return;
 
-			await fillStock(true);
+			await addFillStockBtn(true);
 		});
 	}
 
-	async function fillStock(add) {
+	async function addFillStockBtn(add) {
 		if (!add && getHashParameters().get("option") !== "stock") return;
 
+		(await requireElement("form[action*='stock']"))
+			.find(".order ~ a")
+			.insertAdjacentElement("afterend",
+				document.newElement({
+					type: "button",
+					class: "tt-btn tt-fill-stock",
+					text: "FILL STOCK",
+					events: { click: fillStock },
+				})
+			);
+	}
+
+	async function fillStock() {
 		const stockForm = await requireElement("form[action*='stock']");
 		const totalCapacity = parseInt(stockForm.find(".storage-capacity .max").dataset.initial);
 		const totalSoldDaily = stockForm.find(".stock-list > li.total .sold-daily").textContent.getNumber();
