d5e691f0628bf86c1bb867b5d3bd2bbf27925f99
DeKleineKobini <kobedepeuter@outlook.com>
2022-05-30 21:49:22 +0200

    Improve variable and function names.

--- extension/scripts/features/auto-stock-fill/ttAutoStockFill.js
+++ extension/scripts/features/auto-stock-fill/ttAutoStockFill.js
@@ -9,7 +9,7 @@
 		"companies",
 		() => settings.pages.companies.autoStockFill,
 		addListener,
-		addFillStockBtn,
+		addFillStockButton,
 		() => document.findAll(".tt-fill-stock").forEach((x) => x.remove()),
 		{
 			storage: ["settings.pages.companies.autoStockFill"],
@@ -20,11 +20,11 @@
 		CUSTOM_LISTENERS[EVENT_CHANNELS.COMPANY_STOCK_PAGE].push(async () => {
 			if (!feature.enabled) return;
 
-			await addFillStockBtn(true);
+			await addFillStockButton(true);
 		});
 	}
 
-	async function addFillStockBtn(add) {
+	async function addFillStockButton(add) {
 		if (!add && getHashParameters().get("option") !== "stock") return;
 
 		(await requireElement("form[action*='stock']")).find(".order ~ a").insertAdjacentElement(
@@ -39,14 +39,15 @@
 
 	async function fillStock() {
 		const stockForm = await requireElement("form[action*='stock']");
-		const totalCapacity = parseInt(stockForm.find(".storage-capacity .max").dataset.initial);
+		const totalCapacity = stockForm.find(".storage-capacity .max").dataset.initial.getNumber();
 		const totalSoldDaily = stockForm.find(".stock-list > li.total .sold-daily").textContent.getNumber();
+
 		stockForm.findAll(".stock-list > li:not([class]), .stock-list > li.new").forEach((stockItem) => {
-			const onOrder = stockItem.find(".delivery").lastChild.textContent.getNumber();
-			const inStock = stockItem.find(".stock").lastChild.textContent.getNumber();
+			const ordered = stockItem.find(".delivery").lastChild.textContent.getNumber();
+			const stock = stockItem.find(".stock").lastChild.textContent.getNumber();
 			const soldDaily = stockItem.find(".sold-daily").lastChild.textContent.getNumber();
 
-			const neededStock = ((soldDaily / totalSoldDaily) * totalCapacity - onOrder - inStock).dropDecimals();
+			const neededStock = ((soldDaily / totalSoldDaily) * totalCapacity - ordered - stock).dropDecimals();
 
 			updateReactInput(stockItem.find("input"), neededStock, REACT_UPDATE_VERSIONS.DEFAULT);
 		});
