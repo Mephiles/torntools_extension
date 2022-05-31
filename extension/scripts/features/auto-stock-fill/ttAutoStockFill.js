fba47a0b67bfb61f9e90a383025b07c96f1d8c43
Sashank999 <76554862+Sashank999@users.noreply.github.com>
2022-05-31 12:24:11 +0530

    Auto Fill Stock has a button. (#664)
    * Change Auto Stock Fill to have a button.
    
    * Apply prettier changes.
    
    * Improve button visuals.
    
    * Fix input updating.
    
    * Improve variable and function names.
    
    * Fix live reload.
    
    * Improve element query.
    
    * Apply prettier changes.
    
    Co-authored-by: DeKleineKobini <kobedepeuter@outlook.com>

--- extension/scripts/features/auto-stock-fill/ttAutoStockFill.js
+++ extension/scripts/features/auto-stock-fill/ttAutoStockFill.js
@@ -4,34 +4,54 @@
 	if (!getPageStatus().access) return;
 	if (!isOwnCompany) return;
 
-	const feature = featureManager.registerFeature("Auto Fill Stock", "companies", () => settings.pages.companies.autoStockFill, addListener, fillStock, null, {
-		storage: ["settings.pages.companies.autoStockFill"],
-	});
+	const feature = featureManager.registerFeature(
+		"Auto Fill Stock",
+		"companies",
+		() => settings.pages.companies.autoStockFill,
+		addListener,
+		addFillStockButton,
+		() => document.findAll(".tt-fill-stock").forEach((x) => x.remove()),
+		{
+			storage: ["settings.pages.companies.autoStockFill"],
+		},
+		undefined,
+		{ liveReload: true }
+	);
 
 	function addListener() {
 		CUSTOM_LISTENERS[EVENT_CHANNELS.COMPANY_STOCK_PAGE].push(async () => {
 			if (!feature.enabled) return;
 
-			await fillStock(true);
+			await addFillStockButton(true);
 		});
 	}
 
-	async function fillStock(add) {
+	async function addFillStockButton(add) {
 		if (!add && getHashParameters().get("option") !== "stock") return;
 
+		(await requireElement("form[action*='stock'] .order ~ a")).insertAdjacentElement(
+			"afterend",
+			document.newElement({
+				type: "div",
+				class: "tt-fill-stock-wrapper",
+				children: [document.newElement({ type: "button", class: "tt-btn tt-fill-stock", text: "FILL STOCK", events: { click: fillStock } })],
+			})
+		);
+	}
+
+	async function fillStock() {
 		const stockForm = await requireElement("form[action*='stock']");
-		const totalCapacity = parseInt(stockForm.find(".storage-capacity .max").dataset.initial);
+		const totalCapacity = stockForm.find(".storage-capacity .max").dataset.initial.getNumber();
 		const totalSoldDaily = stockForm.find(".stock-list > li.total .sold-daily").textContent.getNumber();
+
 		stockForm.findAll(".stock-list > li:not([class]), .stock-list > li.new").forEach((stockItem) => {
-			const onOrder = stockItem.find(".delivery").lastChild.textContent.getNumber();
-			const inStock = stockItem.find(".stock").lastChild.textContent.getNumber();
+			const ordered = stockItem.find(".delivery").lastChild.textContent.getNumber();
+			const stock = stockItem.find(".stock").lastChild.textContent.getNumber();
 			const soldDaily = stockItem.find(".sold-daily").lastChild.textContent.getNumber();
 
-			const neededStock = ((soldDaily / totalSoldDaily) * totalCapacity - onOrder - inStock).dropDecimals();
+			const neededStock = ((soldDaily / totalSoldDaily) * totalCapacity - ordered - stock).dropDecimals();
 
-			const input = stockItem.find("input");
-			input.value = neededStock;
-			input.dispatchEvent(new Event("blur"));
+			updateReactInput(stockItem.find("input"), neededStock, REACT_UPDATE_VERSIONS.DEFAULT);
 		});
 	}
 })();
