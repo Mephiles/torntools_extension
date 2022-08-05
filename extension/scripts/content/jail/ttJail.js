6583ab0dc14341247ee5904c5a003bb2a3715a8f
Sashank999 <bandi.rao999@gmail.com>
2022-08-05 21:20:32 +0530

    Fix Jail filters when page switching - try 1.
    Uses `hashchange` instead of `click` listener.

--- extension/scripts/content/jail/ttJail.js
+++ extension/scripts/content/jail/ttJail.js
@@ -1,11 +1,11 @@
 "use strict";
 
-(() => {
-	document.addEventListener("click", (event) => {
-		if (event.target.classList.contains("page-number") || event.target.classList.contains("page-nb") || event.target.className.includes("pagination")) {
-			requireElement(".user-info-list-wrap .last #iconTray li").then(() => {
-				triggerCustomListener(EVENT_CHANNELS.JAIL_SWITCH_PAGE, null);
-			});
-		}
-	});
-})();
+window.addEventListener("hashchange", e => {
+	const oldStart = getHashParameters((new URL(e.oldURL)).hash).get("start") || 0;
+	const newStart = getHashParameters((new URL(e.newURL)).hash).get("start") || 0;
+	if (oldStart !== newStart) {
+		requireElement(".user-info-list-wrap .last #iconTray li").then(() => {
+			triggerCustomListener(EVENT_CHANNELS.JAIL_SWITCH_PAGE, null);
+		});
+	}
+});
