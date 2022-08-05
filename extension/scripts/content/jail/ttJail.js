59e771e2c1dc29b3ce4e7d882fcc5ba9b6cd7011
Sashank999 <bandi.rao999@gmail.com>
2022-08-05 21:45:56 +0530

    Fix Jail Filter when page switching - try 2.

--- extension/scripts/content/jail/ttJail.js
+++ extension/scripts/content/jail/ttJail.js
@@ -1,8 +1,10 @@
 "use strict";
 
-window.addEventListener("hashchange", e => {
-	const oldStart = getHashParameters((new URL(e.oldURL)).hash).get("start") || 0;
-	const newStart = getHashParameters((new URL(e.newURL)).hash).get("start") || 0;
+window.addEventListener("hashchange", (e) => {
+	const oldHash = (new URL(e.oldURL)).hash || "#";
+	const newHash = (new URL(e.newURL)).hash || "#";
+	const oldStart = getHashParameters(oldHash).get("start");
+	const newStart = getHashParameters(newHash).get("start");
 	if (oldStart !== newStart) {
 		requireElement(".user-info-list-wrap .last #iconTray li").then(() => {
 			triggerCustomListener(EVENT_CHANNELS.JAIL_SWITCH_PAGE, null);
