49aabe3be5be229acf76ba87180afb0af0b0d6e6
Sashank999 <bandi.rao999@gmail.com>
2022-09-04 13:14:32 +0530

    Fix Custom Notification Sound messing up settings.

--- extension/pages/settings/settings.js
+++ extension/pages/settings/settings.js
@@ -639,7 +639,6 @@ async function setupPreferences(requireCleanup) {
 		_preferences.find("#notification-volume").value = settings.notifications.volume;
 		if (settings.notifications.sound === "custom") {
 			_preferences.find("#notification-sound-upload").classList.remove("tt-hidden");
-			_preferences.find("#notification-sound-upload + br").classList.remove("tt-hidden");
 		} else {
 			if (settings.notifications.sound === "mute" || settings.notifications.sound === "default") {
 				_preferences.find("#notification-volume").classList.add("tt-hidden");
