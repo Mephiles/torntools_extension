54e052bad80d331877656e402694dfbce384b059
Kobe De Peuter <kobedepeuter@outlook.com>
2022-11-09 23:55:21 +0100

    Store FF modifier.

--- extension/scripts/background.js
+++ extension/scripts/background.js
@@ -22,7 +22,7 @@ const notifications = {
 	chain: {},
 	chainCount: {},
 	stakeouts: {},
-	npcs: {},
+	npcs: {}
 };
 
 let npcUpdater;
@@ -32,7 +32,8 @@ let npcUpdater;
 	await loadDatabase();
 
 	notificationHistory = [];
-	ttStorage.set({ notificationHistory: [] }).then(() => {});
+	ttStorage.set({ notificationHistory: [] }).then(() => {
+	});
 
 	await checkUpdate();
 
@@ -141,16 +142,21 @@ async function convertDatabase() {
 								landing: stakeout.notifications.lands,
 								online: stakeout.notifications.online,
 								life: false,
-								offline: false,
-							},
-						},
+								offline: false
+							}
+						}
 					}))
 					.filter((result) => Object.values(result)[0] !== undefined)
 					.reduce((prev, current) => ({ ...prev, ...current }), {});
 			if (storage?.stock_alerts)
 				newStorage.settings.notifications.types.stocks = Object.entries(storage.stock_alerts)
 					.filter(([id]) => !isNaN(id) && !!parseInt(id))
-					.map(([id, alert]) => ({ [id]: { priceFalls: parseInt(alert.fall) || "", priceReaches: parseInt(alert.reach) || "" } }))
+					.map(([id, alert]) => ({
+						[id]: {
+							priceFalls: parseInt(alert.fall) || "",
+							priceReaches: parseInt(alert.reach) || ""
+						}
+					}))
 					.reduce((prev, current) => ({ ...prev, ...current }), {});
 
 			// Reset
@@ -339,7 +345,7 @@ async function updateUserdata() {
 			"workstats",
 			"skills",
 			"weaponexp",
-			"properties",
+			"properties"
 		]) {
 			if (!settings.apiUsage.user[selection]) continue;
 
@@ -430,7 +436,7 @@ async function updateUserdata() {
 					escapes: 0,
 					respect: [],
 					respect_base: [],
-					...attackHistory.history[enemyId],
+					...attackHistory.history[enemyId]
 				};
 
 				// Manipulate the data to be correct.
@@ -460,12 +466,12 @@ async function updateUserdata() {
 
 						let respect = attack.respect_gain;
 						if (respect !== 0) {
-							let hasBaseRespect = attack.modifiers;
+							let hasAccurateModifiers = attack.modifiers;
 
-							if (hasBaseRespect) {
+							if (hasAccurateModifiers) {
 								if (respect === attack.modifiers.chain_bonus) {
 									respect = 1;
-									hasBaseRespect = false;
+									hasAccurateModifiers = false;
 								} else {
 									if (attack.result === "Mugged") respect /= 0.75;
 
@@ -478,9 +484,10 @@ async function updateUserdata() {
 										attack.modifiers.chain_bonus /
 										(attack.modifiers.warlord_bonus || 1);
 								}
+								attackHistory.history[enemyId].latestFairFightModifier = attack.modifiers.fair_fight;
 							}
 
-							attackHistory.history[enemyId][hasBaseRespect ? "respect_base" : "respect"].push(respect);
+							attackHistory.history[enemyId][hasAccurateModifiers ? "respect_base" : "respect"].push(respect);
 						}
 
 						switch (attack.result) {
@@ -504,7 +511,13 @@ async function updateUserdata() {
 				}
 			}
 
-			await ttStorage.change({ attackHistory: { lastAttack, fetchData: false, history: { ...attackHistory.history } } });
+			await ttStorage.change({
+				attackHistory: {
+					lastAttack,
+					fetchData: false,
+					history: { ...attackHistory.history }
+				}
+			});
 		}
 	}
 
@@ -513,8 +526,8 @@ async function updateUserdata() {
 			userdata.userCrime = userdata.icons.icon85
 				? userdata.timestamp * TO_MILLIS.SECONDS + textToTime(userdata.icons.icon85.split("-").last().trim())
 				: userdata.icons.icon86
-				? userdata.timestamp * TO_MILLIS.SECONDS
-				: -1;
+					? userdata.timestamp * TO_MILLIS.SECONDS
+					: -1;
 		}
 	}
 
@@ -583,15 +596,25 @@ async function updateUserdata() {
 					url: LINKS.home,
 					type: "status",
 					key: Date.now(),
-					date: Date.now(),
+					date: Date.now()
 				});
 			} else if (previous === "Jail") {
 				await notifyUser("TornTools - Status", "You are out of the jail.", LINKS.home);
-				storeNotification({ title: "TornTools - Status", message: "You are out of the jail.", url: LINKS.home, date: Date.now() });
+				storeNotification({
+					title: "TornTools - Status",
+					message: "You are out of the jail.",
+					url: LINKS.home,
+					date: Date.now()
+				});
 			}
 		} else {
 			await notifyUser("TornTools - Status", userdata.status.description, LINKS.home);
-			storeNotification({ title: "TornTools - Status", message: userdata.status.description, url: LINKS.home, date: Date.now() });
+			storeNotification({
+				title: "TornTools - Status",
+				message: userdata.status.description,
+				url: LINKS.home,
+				date: Date.now()
+			});
 		}
 		await ttStorage.set({ notificationHistory });
 	}
@@ -604,7 +627,12 @@ async function updateUserdata() {
 			if (userdata.cooldowns[type] || !oldUserdata.cooldowns[type]) continue;
 
 			await notifyUser("TornTools - Cooldown", `Your ${type} cooldown has ended.`, LINKS.items);
-			storeNotification({ title: "TornTools - Cooldown", message: `Your ${type} cooldown has ended.`, url: LINKS.items, date: Date.now() });
+			storeNotification({
+				title: "TornTools - Cooldown",
+				message: `Your ${type} cooldown has ended.`,
+				url: LINKS.items,
+				date: Date.now()
+			});
 		}
 		await ttStorage.set({ notificationHistory });
 	}
@@ -618,7 +646,7 @@ async function updateUserdata() {
 			title: "TornTools - Traveling",
 			message: `You have landed in ${userdata.travel.destination}.`,
 			url: LINKS.home,
-			date: Date.now(),
+			date: Date.now()
 		});
 		await ttStorage.set({ notificationHistory });
 	}
@@ -638,7 +666,7 @@ async function updateUserdata() {
 			title: "TornTools - Education",
 			message: "You have finished your education course.",
 			url: LINKS.education,
-			date: Date.now(),
+			date: Date.now()
 		});
 		await ttStorage.set({ notificationHistory });
 	}
@@ -799,8 +827,20 @@ async function updateUserdata() {
 
 		const COOLDOWNS = [
 			{ name: "drug", title: "Drugs", setting: "cooldownDrug", memory: "drugs", enabled: "cooldownDrugEnabled" },
-			{ name: "booster", title: "Boosters", setting: "cooldownBooster", memory: "boosters", enabled: "cooldownBoosterEnabled" },
-			{ name: "medical", title: "Medical", setting: "cooldownMedical", memory: "medical", enabled: "cooldownMedicalEnabled" },
+			{
+				name: "booster",
+				title: "Boosters",
+				setting: "cooldownBooster",
+				memory: "boosters",
+				enabled: "cooldownBoosterEnabled"
+			},
+			{
+				name: "medical",
+				title: "Medical",
+				setting: "cooldownMedical",
+				memory: "medical",
+				enabled: "cooldownMedicalEnabled"
+			}
 		];
 
 		for (const cooldown of COOLDOWNS) {
@@ -855,7 +895,7 @@ async function showIconBars() {
 			happy: "#e3e338",
 			life: "#7b98ee",
 			chain: "#333",
-			travel: "#d961ee",
+			travel: "#d961ee"
 		};
 
 		let y = padding;
@@ -1027,19 +1067,19 @@ async function updateStakeouts() {
 			last_action: {
 				status: data.last_action.status,
 				relative: data.last_action.relative,
-				timestamp: data.last_action.timestamp * 1000,
+				timestamp: data.last_action.timestamp * 1000
 			},
 			life: {
 				current: data.life.current,
-				maximum: data.life.maximum,
+				maximum: data.life.maximum
 			},
 			status: {
 				state: data.status.state,
 				color: data.status.color,
 				until: data.status.until * 1000,
-				description: data.status.description,
+				description: data.status.description
 			},
-			isRevivable: data.revivable === 1,
+			isRevivable: data.revivable === 1
 		};
 	}
 	stakeouts.date = now;
@@ -1049,7 +1089,10 @@ async function updateStakeouts() {
 }
 
 async function updateTorndata() {
-	torndata = await fetchData("torn", { section: "torn", selections: ["education", "honors", "items", "medals", "pawnshop", "properties", "stats"] });
+	torndata = await fetchData("torn", {
+		section: "torn",
+		selections: ["education", "honors", "items", "medals", "pawnshop", "properties", "stats"]
+	});
 	if (!torndata || !Object.keys(torndata).length) throw new Error("Aborted updating due to an unexpected response.");
 	torndata.date = Date.now();
 
@@ -1070,14 +1113,14 @@ async function updateStocks() {
 
 			if (alerts.priceFalls && oldStocks[id].current_price > alerts.priceFalls && stocks[id].current_price <= alerts.priceFalls) {
 				const message = `(${stocks[id].acronym}) ${stocks[id].name} has fallen to ${formatNumber(stocks[id].current_price, {
-					currency: true,
+					currency: true
 				})} (alert: ${formatNumber(alerts.priceFalls, { currency: true })})!`;
 
 				await notifyUser("TornTools - Stock Alerts", message, LINKS.stocks);
 				storeNotification({ title: "TornTools -  Stock Alerts", message, url: LINKS.stocks, date: Date.now() });
 			} else if (alerts.priceReaches && oldStocks[id].current_price < alerts.priceFalls && stocks[id].current_price >= alerts.priceReaches) {
 				const message = `(${stocks[id].acronym}) ${stocks[id].name} has reached ${formatNumber(stocks[id].current_price, {
-					currency: true,
+					currency: true
 				})} (alert: ${formatNumber(alerts.priceReaches, { currency: true })})!`;
 
 				await notifyUser("TornTools - Stock Alerts", message, LINKS.stocks);
@@ -1169,7 +1212,7 @@ async function updateNPCs() {
 		17: "Easter Bunny",
 		19: "Jimmy",
 		20: "Fernando",
-		21: "Tiny",
+		21: "Tiny"
 	};
 
 	const now = Date.now();
@@ -1207,7 +1250,7 @@ async function updateNPCs() {
 		npcs = {
 			next_update: data.next_update * 1000,
 			service: "YATA",
-			targets: {},
+			targets: {}
 		};
 
 		for (let [id, hospital] of Object.entries(data.hosp_out)) {
@@ -1219,9 +1262,9 @@ async function updateNPCs() {
 					2: hospital + TO_MILLIS.MINUTES * 30,
 					3: hospital + TO_MILLIS.MINUTES * 90,
 					4: hospital + TO_MILLIS.MINUTES * 210,
-					5: hospital + TO_MILLIS.MINUTES * 450,
+					5: hospital + TO_MILLIS.MINUTES * 450
 				},
-				name: NPCS[id] ?? "Unknown",
+				name: NPCS[id] ?? "Unknown"
 			};
 
 			npcs.targets[id].current = getCurrentLevel(npcs.targets[id]);
@@ -1239,7 +1282,7 @@ async function updateNPCs() {
 		npcs = {
 			next_update: now + TO_MILLIS.MINUTES * 15,
 			service: "TornStats",
-			targets: {},
+			targets: {}
 		};
 
 		for (const npc of Object.values(data)
@@ -1251,9 +1294,9 @@ async function updateNPCs() {
 					2: npc.loot_2 * 1000,
 					3: npc.loot_3 * 1000,
 					4: npc.loot_4 * 1000,
-					5: npc.loot_5 * 1000,
+					5: npc.loot_5 * 1000
 				},
-				name: npc.name,
+				name: npc.name
 			};
 
 			npcs.targets[npc.torn_id].current = getCurrentLevel(npcs.targets[npc.torn_id]);
@@ -1293,7 +1336,10 @@ async function updateNPCs() {
 
 		let alerts = 0;
 
-		for (const { id, level, minutes } of settings.notifications.types.npcs.filter(({ level, minutes }) => level !== "" && minutes !== "")) {
+		for (const { id, level, minutes } of settings.notifications.types.npcs.filter(({
+																						   level,
+																						   minutes
+																					   }) => level !== "" && minutes !== "")) {
 			const npc = npcs.targets[id];
 			if (!npc) {
 				delete notifications.npcs[id];
@@ -1347,7 +1393,7 @@ function newNotification(title, message, link) {
 		title: `TornTools - ${title}`,
 		message,
 		url: link,
-		date: Date.now(),
+		date: Date.now()
 	};
 }
 
@@ -1395,7 +1441,8 @@ async function notifyUser(title, message, url) {
 			chrome.notifications.create(options, (id) => resolve(id));
 		});
 
-		if (notificationSound !== "default" && notificationSound !== "mute") notificationPlayer.play().then(() => {});
+		if (notificationSound !== "default" && notificationSound !== "mute") notificationPlayer.play().then(() => {
+		});
 
 		if (settings.notifications.link) notificationRelations[id] = url;
 	}
@@ -1405,7 +1452,7 @@ async function notifyUser(title, message, url) {
 			icon,
 			body: message,
 			requireInteraction,
-			data: { settings: {} },
+			data: { settings: {} }
 		};
 		if (silent) options.silent;
 
@@ -1446,7 +1493,8 @@ async function notifyUser(title, message, url) {
 	}
 }
 
-chrome.runtime.onConnect.addListener(() => {});
+chrome.runtime.onConnect.addListener(() => {
+});
 
 chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
 	switch (message.action) {
