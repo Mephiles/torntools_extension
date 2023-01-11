2fb45ef3d5faa095a33f8e6ae6a2a338a660e90f
Kobe De Peuter <kobedepeuter@outlook.com>
2023-01-11 18:59:37 +0100

    Fix faction armory tab detection if a tab is empty.

--- extension/scripts/content/factions/ttFactions.js
+++ extension/scripts/content/factions/ttFactions.js
@@ -77,7 +77,13 @@ const isOwnFaction = getSearchParameters().get("step") === "your";
 
 			triggerCustomListener(EVENT_CHANNELS.FACTION_ARMORY_TAB, { section: getCurrentSection() });
 			new MutationObserver((mutations) => {
-				if (!mutations.some((mutation) => mutation.addedNodes.length > 5)) return;
+				if (!mutations.some((mutation) =>{
+					const addedNodes = [...mutation.addedNodes];
+
+					return addedNodes
+						.filter((node) => node.nodeType === Node.ELEMENT_NODE)
+						.some((node) => node.classList.contains("item-list") || (node.tagName === "DIV" && node.classList.contains("p10")))
+				})) return;
 
 				const mutation = mutations.find((mutation) => mutation.target.id.includes("armoury-"));
 				if (!mutation) return;
