b26baa6896596517e7ab08f79d496dbd9ab22d6c
DeKleineKobini <DeKleineKobini@users.noreply.github.com>
2023-01-24 16:56:31 +0000

    Prettified Code!

--- extension/scripts/features/gym-steadfast/ttGymSteadfast.js
+++ extension/scripts/features/gym-steadfast/ttGymSteadfast.js
@@ -86,7 +86,12 @@
 			const box = properties.find(`[class*='${stat}___']`);
 			if (box.find(".tt-gym-steadfast")) continue;
 
-			const parent = document.newElement({ type: "div", class: "tt-gym-steadfast", style: { height: `${maxBonus * 12}px` }, attributes: {title: "Total additional gym gain."} });
+			const parent = document.newElement({
+				type: "div",
+				class: "tt-gym-steadfast",
+				style: { height: `${maxBonus * 12}px` },
+				attributes: { title: "Total additional gym gain." },
+			});
 			box.insertBefore(parent, box.firstElementChild);
 
 			for (const perk of perks) {
