9656832fd712adbc00f9d89bf03a53a5ad4a452c
Sashank999 <bandi.rao999@gmail.com>
2024-06-20 19:26:00 +0530

    Move to v7.2.4, fix racing features.

--- extension/scripts/features/racing-upgrades/ttRacingUpgrades.js
+++ extension/scripts/features/racing-upgrades/ttRacingUpgrades.js
@@ -20,7 +20,7 @@
 		addXHRListener(async ({ detail: { page, xhr, uri } }) => {
 			if (!feature.enabled()) return;
 
-			if (page === "loader" && uri) {
+			if (page === "page" && uri) {
 				const sid = uri.sid;
 				if (sid !== "racing") return;
 
@@ -33,7 +33,7 @@
 					car.classList.add("tt-modified");
 					car.addEventListener("click", () => requireElement(".pm-categories-wrap").then(showUpgrades));
 				}
-			} else if (page === "loader2") {
+			} else if (page === "page") {
 				const params = new URLSearchParams(xhr.requestBody);
 
 				const sid = params.get("sid");
@@ -45,7 +45,7 @@
 				const confirm = params.get("confirm");
 				if (confirm !== "1") return;
 
-				setTimeout(resetUpgrades, 250);
+				setTimeout(resetUpgrades, 1000);
 			}
 		});
 	}
