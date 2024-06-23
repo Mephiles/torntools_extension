fe9346b649415740fc0e9947929000962737ccf2
Sashank999 <bandi.rao999@gmail.com>
2024-06-23 09:34:50 +0530

    Merge `master`, changelog & Prettier changes.

--- extension/scripts/features/opened-supply-pack-value/ttOpenedSupplyPackValue.js
+++ extension/scripts/features/opened-supply-pack-value/ttOpenedSupplyPackValue.js
@@ -1,105 +1,84 @@
 "use strict";
 
 (async () => {
-    const page = getPage();
-    
-    const feature = featureManager.registerFeature(
-        "Opened supply pack total value",
-        "items",
-        () => settings.pages.items.openedSupplyPackValue,
-        addListener,
-        undefined,
-        removeTotalValueElement,
-        {storage: ["settings.pages.items.openedSupplyPackValue"]},
-        () => {
+	const page = getPage();
+
+	const feature = featureManager.registerFeature(
+		"Opened supply pack total value",
+		"items",
+		() => settings.pages.items.openedSupplyPackValue,
+		addListener,
+		undefined,
+		removeTotalValueElement,
+		{ storage: ["settings.pages.items.openedSupplyPackValue"] },
+		() => {
 			if (!hasAPIData()) return "No API access.";
-        }
-    );
+		}
+	);
 
-    const SUPPLY_PACK_ITEMS = [
-        364,
-        365,
-        370,
-        588,
-        815,
-        817,
-        818,
-        1035,
-        1057,
-        1078,
-        1079,
-        1080,
-        1081,
-        1082,
-        1083,
-        1112,
-        1113,
-        1114,
-        1115,
-        1116,
-        1117,
-        1118,
-        1119,
-        1120,
-        1121,
-        1122,
-        1293,
-        1298
-    ];
+	const SUPPLY_PACK_ITEMS = [
+		364, 365, 370, 588, 815, 817, 818, 1035, 1057, 1078, 1079, 1080, 1081, 1082, 1083, 1112, 1113, 1114, 1115, 1116, 1117, 1118, 1119, 1120, 1121, 1122,
+		1293, 1298,
+	];
 
-    function addListener() {
-        if (page !== "item") return;
+	function addListener() {
+		if (page !== "item") return;
 
-        let reqXID = 0;
-        let itemID = 0;
-        
-        addXHRListener(async ({ detail: { xhr, json } }) => {
-            if (!feature.enabled()) return;
+		let reqXID = 0;
+		let itemID = 0;
 
-            const params = new URLSearchParams(xhr.requestBody);
-            if (params.get("action") !== "use" && params.get("step") !== "useItem") return;
+		addXHRListener(async ({ detail: { xhr, json } }) => {
+			if (!feature.enabled()) return;
 
-            itemID = params.get("id")?.getNumber() ?? itemID;
-            if (isXIDRequestSupplyPack(itemID)) {
-                reqXID = (await requireElement(`[data-item="${itemID}"] .pack-open-msg input[type="hidden"]`)).value;
-            }
+			const params = new URLSearchParams(xhr.requestBody);
+			if (params.get("action") !== "use" && params.get("step") !== "useItem") return;
 
-            if (params.get("XID") === reqXID || isDrugPackUseRequest(params)) {
-                const totalOpenedValue = json?.items?.itemAppear?.reduce(
-                    (totalValue, item) => 
-                    totalValue += item.isMoney
-                                    ? item.moneyGain.substring(1).getNumber()
-                                    : torndata.items[item.ID].market_value * item.qty, 0);
+			itemID = params.get("id")?.getNumber() ?? itemID;
+			if (isXIDRequestSupplyPack(itemID)) {
+				reqXID = (await requireElement(`[data-item="${itemID}"] .pack-open-msg input[type="hidden"]`)).value;
+			}
 
-                    await showTotalValue(totalOpenedValue, itemID);
-            }
-        });
-    };
+			if (params.get("XID") === reqXID || isDrugPackUseRequest(params)) {
+				const totalOpenedValue = json?.items?.itemAppear?.reduce(
+					(totalValue, item) =>
+						(totalValue += item.isMoney ? item.moneyGain.substring(1).getNumber() : torndata.items[item.ID].market_value * item.qty),
+					0
+				);
 
-    async function showTotalValue(totalOpenedValue, itemID) {
-        await sleep(0.1 * TO_MILLIS.SECONDS);
-        const greenMsg = await requireElement(`[data-item="${itemID}"] .cont-wrap form p`);
-        
-        removeTotalValueElement();
+				await showTotalValue(totalOpenedValue, itemID);
+			}
+		});
+	}
 
-        const openedValueTextElement = document.newElement({id: "ttOpenedValueText", type: "strong", class: "tt-opened-supply-pack-value-text", text: `TornTools total value: ${formatNumber(totalOpenedValue, { currency: true })}`});
+	async function showTotalValue(totalOpenedValue, itemID) {
+		await sleep(0.1 * TO_MILLIS.SECONDS);
+		const greenMsg = await requireElement(`[data-item="${itemID}"] .cont-wrap form p`);
 
-        greenMsg.insertAdjacentElement('beforeend', openedValueTextElement);
-    }
+		removeTotalValueElement();
 
-    function isXIDRequestSupplyPack(itemID) {
-        return SUPPLY_PACK_ITEMS.includes(itemID) && !isDrugPack(itemID);
-    }
+		const openedValueTextElement = document.newElement({
+			id: "ttOpenedValueText",
+			type: "strong",
+			class: "tt-opened-supply-pack-value-text",
+			text: `TornTools total value: ${formatNumber(totalOpenedValue, { currency: true })}`,
+		});
 
-    function isDrugPack(itemID) {
-        return itemID === 370;
-    }
+		greenMsg.insertAdjacentElement("beforeend", openedValueTextElement);
+	}
 
-    function isDrugPackUseRequest(params) {
-        return params.get("item")?.getNumber() === 370 || params.get("itemID")?.getNumber() === 370;
-    }
+	function isXIDRequestSupplyPack(itemID) {
+		return SUPPLY_PACK_ITEMS.includes(itemID) && !isDrugPack(itemID);
+	}
 
-    function removeTotalValueElement() {
-        document.getElementById("ttOpenedValueText")?.remove();
-    }
+	function isDrugPack(itemID) {
+		return itemID === 370;
+	}
+
+	function isDrugPackUseRequest(params) {
+		return params.get("item")?.getNumber() === 370 || params.get("itemID")?.getNumber() === 370;
+	}
+
+	function removeTotalValueElement() {
+		document.getElementById("ttOpenedValueText")?.remove();
+	}
 })();
