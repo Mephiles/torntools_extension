3b81c4303b74180cd0335cadb7203474646a2375
Sashank999 <bandi.rao999@gmail.com>
2024-07-08 09:34:20 +0530

    Merge table functions into `ttTable.js`.

--- extension/scripts/global/elements/table/ttTable.js
+++ extension/scripts/global/elements/table/ttTable.js
@@ -12,6 +12,138 @@ function stringCellRenderer(value) {
 	};
 }
 
+function createTableHeaderCell(columnDef, options) {
+	let columnSortedCallback;
+	let currentDirection;
+
+	const sortIcon = document.newElement({
+		type: "i",
+		class: "fas tt-table-header-cell-sort-icon tt-hidden",
+	});
+
+	const headerCellElement = document.newElement({
+		type: "div",
+		class: ["tt-table-header-cell", ...(columnDef.class ? [columnDef.class] : [])],
+		style: {
+			...(options.stretchColumns ? { minWidth: `${columnDef.width}px`, flex: 1 } : { width: `${columnDef.width}px` }),
+		},
+		children: [
+			document.newElement({
+				type: "span",
+				text: columnDef.title,
+				class: "tt-table-header-cell-title",
+			}),
+			sortIcon,
+		],
+	});
+
+	if (columnDef.sortable) {
+		headerCellElement.addEventListener("click", _sortIconClicked);
+		headerCellElement.classList.add("tt-table-header-cell-clickable");
+	}
+
+	function setColumnSort(direction) {
+		if (!columnDef.sortable) {
+			return;
+		}
+
+		currentDirection = direction;
+		sortIcon.classList.remove("tt-hidden");
+
+		if (direction === COLUMN_SORT_DIRECTION.Asc) {
+			sortIcon.classList.remove("fa-caret-up");
+			sortIcon.classList.add("fa-caret-down");
+		} else {
+			sortIcon.classList.remove("fa-caret-down");
+			sortIcon.classList.add("fa-caret-up");
+		}
+	}
+
+	function clearColumnSort() {
+		sortIcon.classList.add("tt-hidden");
+	}
+
+	function onColumnSorted(callback) {
+		columnSortedCallback = callback;
+	}
+
+	function dispose() {
+		if (columnDef.sortable) {
+			headerCellElement.removeEventListener("click", _sortIconClicked);
+		}
+
+		columnSortedCallback = undefined;
+	}
+
+	function _sortIconClicked() {
+		const direction = !currentDirection
+			? COLUMN_SORT_DIRECTION.Asc
+			: currentDirection === COLUMN_SORT_DIRECTION.Asc
+				? COLUMN_SORT_DIRECTION.Desc
+				: COLUMN_SORT_DIRECTION.Asc;
+		setColumnSort(direction);
+		columnSortedCallback(direction);
+	}
+
+	return {
+		element: headerCellElement,
+		setColumnSort,
+		clearColumnSort,
+		onColumnSorted,
+		dispose,
+	};
+}
+
+function createTableCell(data, columnDef, cellRenderer, options) {
+	const cell = cellRenderer(data);
+	const cellElement = document.newElement({
+		type: "div",
+		class: ["tt-table-row-cell", ...(columnDef.class ? [columnDef.class] : [])],
+		style: {
+			...(options.stretchColumns ? { minWidth: `${columnDef.width}px`, flex: 1 } : { width: `${columnDef.width}px` }),
+		},
+		children: [cell.element],
+	});
+
+	function dispose() {
+		cell.dispose();
+	}
+
+	return {
+		element: cellElement,
+		dispose,
+	};
+}
+
+function createTableRow(rowData, tableColumnsDefs, options) {
+	let isHeader = options.hasHeaders && "header" in rowData;
+
+	let rowCells;
+	if (isHeader) {
+		rowCells = [createTableCell(rowData.header, { width: 0 }, options.cellRenderers[rowData.cellRenderer ?? "string"], options)];
+	} else {
+		rowCells = tableColumnsDefs.map((columnDef) =>
+			createTableCell(rowData[columnDef.id], columnDef, options.cellRenderers[rowData.cellRenderer ?? columnDef.cellRenderer], options)
+		);
+	}
+
+	const rowElement = document.newElement({
+		type: "div",
+		class: ["tt-table-row", ...[options.rowClass ? options.rowClass(rowData, isHeader) : null, isHeader ? "tt-table-row-header" : null]],
+		children: rowCells.map((cell) => cell.element),
+	});
+
+	function dispose() {
+		rowCells.forEach((rowCell) => rowCell.dispose());
+	}
+
+	return {
+		element: rowElement,
+		data: rowData,
+		dispose,
+	};
+}
+
 function createTable(tableColumnsDefs, tableRowsData, options = {}) {
 	options = {
 		stretchColumns: false,
