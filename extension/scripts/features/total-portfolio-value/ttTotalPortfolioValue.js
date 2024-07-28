16161441a1fb9d1bd5970fd19c3cf3bdfd0cf99a
Sashank999 <bandi.rao999@gmail.com>
2024-07-28 11:40:43 +0530

    Total Portfolio Value is now live and accurate...
    ...unless stocks transactions are made, in which case the profit will
    be accurate after the API data update (takes 30 seconds).

--- extension/scripts/features/total-portfolio-value/ttTotalPortfolioValue.js
+++ extension/scripts/features/total-portfolio-value/ttTotalPortfolioValue.js
@@ -1,7 +1,7 @@
 "use strict";
 
 (async () => {
-	featureManager.registerFeature(
+	const feature = featureManager.registerFeature(
 		"Total Portfolio Value and Profit",
 		"stocks",
 		() => settings.pages.stocks.valueAndProfit,
@@ -9,7 +9,7 @@
 		addProfitAndValue,
 		removeProfitAndValue,
 		{
-			storage: ["settings.pages.stocks.valueAndProfit"],
+			storage: ["settings.pages.stocks.valueAndProfit", "userdata.stocks"],
 		},
 		async () => {
 			await checkDevice();
@@ -18,18 +18,33 @@
 
 	async function addProfitAndValue() {
 		await requireElement("#stockmarketroot [class*='stock___']");
+
+		calculateAndShowProfits();
+
+		const observer = new MutationObserver(async (mutations) => {
+			if (!feature.enabled()) return;
+
+			await sleep(0.5);
+			calculateAndShowProfits();
+		});
+		observer.observe(document.find("#priceTab"), { attributeOldValue: true });
+	}
+
+	function calculateAndShowProfits() {
+		removeProfitAndValue();
+
 		const totalValue = [...document.findAll("[class*='stockOwned__'] [class*='value__']")].map((x) => x.textContent.getNumber()).totalSum();
+		const stockPrices = getStockPrices();
 		const profits = [...document.findAll("#stockmarketroot [class*='stockMarket__'] > ul[id]")]
 			.map((x) => {
 				const stockID = x.id;
-				const data = stockdata[stockID];
 				const userStockData = userdata.stocks[stockID];
 				if (!userStockData) return 0;
 
 				const boughtTotal = Object.values(userStockData.transactions).reduce((prev, trans) => prev + trans.bought_price * trans.shares, 0);
 				const boughtPrice = boughtTotal / userStockData.total_shares;
 
-				return Math.floor((data.current_price - boughtPrice) * userStockData.total_shares);
+				return Math.floor((stockPrices[stockID] - boughtPrice) * userStockData.total_shares);
 			})
 			.totalSum();
 
@@ -51,6 +66,14 @@
 			})
 		);
 		if (mobile) document.find("#stockmarketroot [class*='topSection__']").classList.add("tt-total-stock-value-wrap");
+	}
+
+	function getStockPrices() {
+		const data = {};
+		document.findAll("[class*='stockMarket__'] > ul[id]").forEach((stock) => {
+			data[stock.id] = parseFloat(stock.find("#priceTab > :first-child").textContent);
+		});
+		return data;
 	}
 
 	function removeProfitAndValue() {
