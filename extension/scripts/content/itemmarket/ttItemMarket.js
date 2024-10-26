32ea1150058d361bef813392d365e7dd76e41ecb
Kobe De Peuter <kobedepeuter@outlook.com>
2024-10-26 18:41:15 +0200

    Cleanup item market script.

--- extension/scripts/content/itemmarket/ttItemMarket.js
+++ extension/scripts/content/itemmarket/ttItemMarket.js
@@ -11,32 +11,34 @@
 		const step = params.get("step");
 
 		if (step === "getShopList" || step === "searchItem") {
-			requireElement("[class*='itemList___'],[class*='noItems___']").then((list) => {
-				if (isValidEntry(list)) triggerCustomListener(EVENT_CHANNELS.ITEMMARKET_CATEGORY_ITEMS, { list });
-			});
+			requireElement("[class*='itemList___'],[class*='noItems___']").then(handleItemList);
 		} else if (step === "getListing") {
-			requireElement("[class*='sellerList___']").then((list) => {
-				if (isValidEntry(list)) triggerCustomListener(EVENT_CHANNELS.ITEMMARKET_ITEMS, { list, item: fetch.body?.itemID });
-			});
+			requireElement("[class*='sellerList___']").then((list) => handleSellerList(list, fetch));
 		}
 	});
+
+	const hash = getHashParameters();
+	const view = hash.get("market/view");
+	if (view === "category" || view === "search") {
+		requireElement("[class*='itemList___'],[class*='noItems___']").then(handleItemList);
+	}
+	if (view === "search") {
+		requireElement("[class*='sellerList___']").then((list) => handleSellerList(list, fetch));
+	}
 
 	function isValidEntry(list) {
 		return !list.className.includes("[class*='noItems___']");
 	}
 
-	const hash = getHashParameters();
-	const view = hash.get("market/view");
-	if (view === "category" || view === "search") {
-		requireElement("[class*='itemList___'],[class*='noItems___']").then((list) => {
-			if (isValidEntry(list)) triggerCustomListener(EVENT_CHANNELS.ITEMMARKET_CATEGORY_ITEMS, { list });
-		});
-	}
-	if (view === "search") {
-		const item = parseInt(hash.get("itemID"));
+	function handleItemList(list) {
+		if (!isValidEntry(list)) return;
 
-		requireElement("[class*='sellerList___']").then((list) => {
-			if (isValidEntry(list)) triggerCustomListener(EVENT_CHANNELS.ITEMMARKET_ITEMS, { list, item });
-		});
+		triggerCustomListener(EVENT_CHANNELS.ITEMMARKET_CATEGORY_ITEMS, { list });
+	}
+
+	function handleSellerList(list, fetch) {
+		if (!isValidEntry(list)) return;
+
+		triggerCustomListener(EVENT_CHANNELS.ITEMMARKET_ITEMS, { list, item: fetch.body?.itemID });
 	}
 })();
