d904fa911200a5a52e2fc0b929fb5272da841e22
Kobe De Peuter <kobedepeuter@outlook.com>
2025-01-02 20:29:45 +0100

    Avoid errors on the bank page when there is no API key.

--- extension/scripts/features/bank-investment-due-time/ttBankInvestmentDueTime.js
+++ extension/scripts/features/bank-investment-due-time/ttBankInvestmentDueTime.js
@@ -55,7 +55,7 @@
 	}
 
 	function teardown() {
-		bankInvestmentFacade.dispose();
+		bankInvestmentFacade?.dispose();
 		bankInvestmentFacade = undefined;
 	}
 })();
--- extension/scripts/features/bank-investment-info/ttBankInvestmentInfo.js
+++ extension/scripts/features/bank-investment-info/ttBankInvestmentInfo.js
@@ -206,7 +206,7 @@
 	}
 
 	function teardown() {
-		bankInvestmentInfoContainer.dispose();
+		bankInvestmentInfoContainer?.dispose();
 		bankInvestmentInfoContainer = undefined;
 	}
 })();
