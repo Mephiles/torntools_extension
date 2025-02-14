94ea95ac429f7ac190193fe841ee31c3dab373f8
Travis Smith <travis@legomaster.net>
2025-02-14 17:08:10 -0500

    Adding feature to highlight items in the Bazaar when it's below the vendor price. (#835)
    - requires turning it on in options
    - requires an API key
    - works in FF & Chrome; including when the viewport is small (e.g., mobile sized)
    - updated manifest for new files
    - updated CONTRIBUTING for a bit more on testing in browsers
    - added option to settings & global defaults

--- extension/scripts/global/team.js
+++ extension/scripts/global/team.js
@@ -217,6 +217,13 @@ const TEAM = [
 		torn: 2383326,
 		color: "white", // No explicit color chosen.
 	},
+	{
+		name: "TravisTheTechie",
+		title: "Developer",
+		core: false,
+		torn: 3549588,
+		color: "firebrick", 
+	},
 ];
 
 const CONTRIBUTORS = TEAM.filter(({ title, color }) => title.includes("Developer") || color).reduce(
