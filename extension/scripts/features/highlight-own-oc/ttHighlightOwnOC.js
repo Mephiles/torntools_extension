213ab16ade9fd1bcb6273f8b1d1fbe014918d7bc
Kobe De Peuter <kobedepeuter@outlook.com>
2025-03-18 20:10:10 +0100

    Highlight your own OC2 crime.

--- extension/scripts/features/highlight-own-oc/ttHighlightOwnOC.js
+++ extension/scripts/features/highlight-own-oc/ttHighlightOwnOC.js
@@ -18,7 +18,6 @@
 		},
 		() => {
 			if (!hasAPIData()) return "No API access.";
-			else if (!!userdata.organizedCrime) return "No OC 1 data.";
 		}
 	);
 
@@ -26,21 +25,39 @@
 		CUSTOM_LISTENERS[EVENT_CHANNELS.FACTION_CRIMES].push(() => {
 			if (!feature.enabled()) return;
 
-			highlightCrime();
+			highlightCrime1();
+		});
+		CUSTOM_LISTENERS[EVENT_CHANNELS.FACTION_CRIMES2].push(() => {
+			if (!feature.enabled()) return;
+
+			highlightCrime2();
+		});
+		CUSTOM_LISTENERS[EVENT_CHANNELS.FACTION_CRIMES2_REFRESH].push(() => {
+			if (!feature.enabled()) return;
+
+			highlightCrime2();
 		});
 	}
 
 	function startFeature() {
 		if (!document.find(".faction-crimes-wrap")) return;
 
-		highlightCrime();
+		if (userdata.organizedCrime) highlightCrime2();
+		else highlightCrime1();
 	}
 
-	function highlightCrime() {
+	function highlightCrime1() {
 		const member = document.find(`.crimes-list > li.item-wrap .team > a[href="/profiles.php?XID=${userdata.player_id}"]`);
 		if (!member) return;
 
 		member.closest(".item-wrap").classList.add("tt-oc-highlight");
+	}
+
+	function highlightCrime2() {
+		const member = document.querySelector(`[class*='slotMenuItem___'][href="/profiles.php?XID=${userdata.player_id}"]`);
+		if (!member) return;
+
+		member.closest("[class*='contentLayer___']").classList.add("tt-oc-highlight");
 	}
 
 	function removeHighlight() {
