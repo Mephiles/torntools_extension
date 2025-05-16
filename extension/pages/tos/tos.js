bf55b3ef161260d8037cf915b80b807729dfb484
Kobe De Peuter <kobedepeuter@outlook.com>
2025-05-16 21:55:30 +0200

    Create a ToS page. (#866)
    * Create a ToS page.
    
    * List to the ToS on the settings page.
    
    * List to the ToS in the popup.

---
+++ extension/pages/tos/tos.js
@@ -0,0 +1,11 @@
+"use strict";
+
+(async () => {
+	await loadDatabase();
+
+	document.body.classList.add(getPageTheme());
+	storageListeners.settings.push(() => {
+		document.body.classList.remove("dark", "light");
+		document.body.classList.add(getPageTheme());
+	});
+})();
