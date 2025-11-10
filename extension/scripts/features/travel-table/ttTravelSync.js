7923fa44dc6248d7c253f85bbbbdf262088523ea
Kobe De Peuter <kobedepeuter@outlook.com>
2025-11-10 19:38:09 +0100

    Update travel sync logic.

--- extension/scripts/features/travel-table/ttTravelSync.js
+++ extension/scripts/features/travel-table/ttTravelSync.js
@@ -5,12 +5,12 @@
 	if (!getPageStatus().access) return;
 	if (getSearchParameters().has("page")) return;
 
-	featureManager.registerFeature(
+	const feature = featureManager.registerFeature(
 		"Travel Sync",
 		"travel",
 		() => settings.pages.travel.table,
+		initialise,
 		updatePrices,
-		null,
 		null,
 		{
 			storage: ["settings.pages.travel.table", "settings.external.yata", "settings.external.prometheus"],
@@ -20,47 +20,60 @@
 		}
 	);
 
-	function updatePrices() {
-		if (document.find(".info-msg-cont.red")) return;
+	function initialise() {
+		addFetchListener(({ detail: { page, json, fetch } }) => {
+			if (!feature.enabled()) return;
+			if (page !== "page" || !json) return;
 
+			const params = new URL(fetch.url).searchParams;
+			const sid = params.get("sid");
+			if (sid !== "travelData") return;
+
+			const step = params.get("step");
+			if (step !== "shop") return;
+
+			const items = json.stock.map((s) => ({ id: s.ID, quantity: s.stock, cost: s.price }));
+			const country = json.country;
+
+			syncData(items, country);
+		});
+	}
+
+	async function updatePrices() {
+		if (document.getElementById("travel-root")) {
+			// If the root is present, we'll be relying on the request interceptor.
+			return;
+		}
+
+		const items = Array.from(document.findAll(".users-list > li")).map((item) => ({
+			id: parseInt(item.find(".details").getAttribute("itemid")),
+			quantity: parseInt(item.find(".stck-amount").textContent.replaceAll(",", "")),
+			cost: parseInt(item.find(".cost .c-price").textContent.replace("$", "").replaceAll(",", "")),
+		}));
+		const country = document.find("#skip-to-content").textContent;
+
+		syncData(items, country);
+	}
+
+	function syncData(items, country) {
 		const data = {
 			client: "TornTools",
 			version: chrome.runtime.getManifest().version,
 			author_name: "Mephiles",
 			author_id: 2087524,
-			country: getCountryName(),
-			items: [],
+			country: country.trim().slice(0, 3).toLowerCase(),
+			items,
 		};
-
-		for (const item of document.findAll(".users-list > li")) {
-			const id = parseInt(item.find(".details").getAttribute("itemid"));
-			const quantity = parseInt(item.find(".stck-amount").textContent.replaceAll(",", ""));
-			const cost = parseInt(item.find(".cost .c-price").textContent.replace("$", "").replaceAll(",", ""));
-
-			data.items.push({ id, quantity, cost });
-		}
 
 		if (settings.external.yata) {
 			fetchData("yata", { section: "travel/import/", method: "POST", body: data, relay: true })
-				.then((response) => {
-					console.log("TT - Updated YATA abroad prices.", response);
-				})
-				.catch((error) => {
-					console.warn("TT - Failed to update YATA abroad prices.", error);
-				});
+				.then((response) => console.log("TT - Updated YATA abroad prices.", response))
+				.catch((error) => console.warn("TT - Failed to update YATA abroad prices.", error));
 		}
 		if (settings.external.prometheus) {
 			fetchData("prometheus", { section: "travel", method: "POST", body: data, relay: true })
-				.then((response) => {
-					console.log("TT - Updated Prometheus abroad prices.", response);
-				})
-				.catch((error) => {
-					console.warn("TT - Failed to update Prometheus abroad prices.", error);
-				});
-		}
-
-		function getCountryName() {
-			return document.find("#skip-to-content").textContent.trim().slice(0, 3).toLowerCase();
+				.then((response) => console.log("TT - Updated Prometheus abroad prices.", response))
+				.catch((error) => console.warn("TT - Failed to update Prometheus abroad prices.", error));
 		}
 	}
 })();
