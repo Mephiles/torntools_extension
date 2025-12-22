e8b42ccc2bea467d7a442e1bcebbdbbf3168fefe
Kobe De Peuter <kobedepeuter@outlook.com>
2025-12-23 00:11:12 +0100

    chore: remove 'use strict'

--- extension/pages/popup/popup.ts
+++ extension/pages/popup/popup.ts
@@ -1,5 +1,3 @@
-"use strict";
-
 const SETUP_PAGES = {
 	initialize: setupInitialize,
 	dashboard: setupDashboard,
--- extension/scripts/features/crimes2-burglary-filter/ttBurglaryFilter.ts
+++ extension/scripts/features/crimes2-burglary-filter/ttBurglaryFilter.ts
@@ -1,5 +1,3 @@
-"use strict";
-
 (async () => {
 	if (!getPageStatus().access) return;
 
