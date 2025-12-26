88951df480e13c7c76ab991a1895a7199b1b1c8f
Kobe De Peuter <kobedepeuter@outlook.com>
2025-12-27 00:29:21 +0100

    feat: more TypeScript

--- extension/scripts/features/auto-api-fill/ttAutoAPIFill.ts
+++ extension/scripts/features/auto-api-fill/ttAutoAPIFill.ts
@@ -1,12 +1,10 @@
-"use strict";
-
 (async () => {
 	await loadDatabase();
 
 	if (!hasAPIData()) return;
 	if (!settings.pages.api.autoFillKey) return;
 
-	const input = document.find("input#api_key");
+	const input = document.find<HTMLInputElement>("input#api_key");
 	if (input.value) return;
 
 	input.value = api.torn.key;
--- extension/scripts/features/auto-pretty/ttAutoPretty.ts
+++ extension/scripts/features/auto-pretty/ttAutoPretty.ts
@@ -1,11 +1,9 @@
-"use strict";
-
 (async () => {
 	await loadDatabase();
 
 	if (!settings.pages.api.autoPretty) return;
 
-	for (const prettyButton of document.findAll("input[value=pretty]")) {
+	for (const prettyButton of document.findAll<HTMLInputElement>("input[value=pretty]")) {
 		prettyButton.checked = true;
 	}
 })();
