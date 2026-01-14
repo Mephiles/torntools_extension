fbbbebc08a0bb5b710021bf0d135e9095b375ffd
Sashank999 <bandi.rao999@gmail.com>
2026-01-14 15:57:56 +0530

    Extend CD End Times to bars; use MutationObserver.

--- extension/scripts/features/cooldown-end-times/ttCooldownEndTimes.ts
+++ extension/scripts/features/cooldown-end-times/ttCooldownEndTimes.ts
@@ -12,57 +12,57 @@
 		null
 	);
 
-	async function addEndTimes() {
-		const statusIcons = await requireElement("#sidebarroot [class*='status-icons__']");
-		const { mobile, tablet } = await checkDevice();
-		const isTouchDevice = mobile || tablet;
+	const REQUIRED_TOOLTIP_TITLES = [
+		"Education",
+		"Reading Book",
+		"Racing",
+		"Drug Cooldown",
+		"Booster Cooldown",
+		"Medical Cooldown",
+		"Organized Crime",
+		"Bank Investment",
+	];
+	const BAR_TOOLTIP_TITLES = ["Energy increased by", "Nerve increased by", "Happy increased by", "Life increased by"];
+	const tooltipObserver = new MutationObserver((mutations: MutationRecord[]) => {
+		for (const mutation of mutations) {
+			if (!mutation.addedNodes.length) continue;
 
-		statusIcons.addEventListener(!isTouchDevice ? "mouseover" : "click", listener);
+			mutation.addedNodes.forEach((addedNode) => {
+				if (!(addedNode instanceof Element)) return;
+				if (!addedNode.getAttribute("id") || !addedNode.hasAttribute("data-floating-ui-portal")) return;
 
-		async function listener(event: MouseEvent) {
-			const target = event.target as Element;
-			if (!target.closest("li")?.matches("[class*='icon']") || !target.hasAttribute("aria-label")) return;
+				const tooltipElement = addedNode,
+					tooltipTitleElement = tooltipElement.getElementsByTagName("b")?.[0],
+					tooltipTitle = tooltipTitleElement?.textContent;
+				if (!tooltipTitle || (!REQUIRED_TOOLTIP_TITLES.includes(tooltipTitle) && BAR_TOOLTIP_TITLES.every((title) => !tooltipTitle.startsWith(title))))
+					return;
 
-			const iconName = target.getAttribute("aria-label").split(":")[0].trim();
-			if (
-				![
-					"Education",
-					"Reading Book",
-					"Racing",
-					"Drug Cooldown",
-					"Booster Cooldown",
-					"Medical Cooldown",
-					"Organized Crime",
-					"Bank Investment",
-				].includes(iconName)
-			)
-				return;
+				const timeElement =
+					tooltipElement.find("[class*='static-width___']")?.firstChild ?? // For cooldown icon tooltips.
+					tooltipElement.find("p:not([class])") ??
+					tooltipElement.find("p").lastChild; // For energy, nerve, happy, and life bar tooltips.
+				if (!timeElement) return;
 
-			const tooltip = await requireCondition(() => {
-				const tooltip = document.find("body > div[id][data-floating-ui-portal] [class*='tooltip__']");
-				const name = tooltip?.getElementsByTagName("b")[0]?.textContent;
-
-				if (name !== iconName) return false;
-
-				return tooltip;
+				[...tooltipElement.getElementsByClassName("tt-tooltip-end-times")].forEach((x) => x.remove());
+				const time = Date.now() + textToTime(timeElement.textContent);
+				tooltipTitleElement.parentElement.appendChild(
+					document.newElement({
+						type: "div",
+						class: "tt-tooltip-end-times",
+						text: `${formatDate(time, { showYear: true })} ${formatTime(time)}`,
+					})
+				);
 			});
-
-			const timeElement = tooltip.find("[class*='static-width___']")?.firstChild ?? tooltip.find("p:not([class])");
-			if (!timeElement) return;
-
-			removeEndTimes(tooltip);
-			const time = Date.now() + textToTime(timeElement.textContent);
-			tooltip.appendChild(
-				document.newElement({
-					type: "div",
-					class: "tt-tooltip-end-times",
-					text: `${formatDate(time, { showYear: true })} ${formatTime(time)}`,
-				})
-			);
 		}
+	});
+
+	async function addEndTimes() {
+		await requireElement("#sidebarroot [class*='status-icons__']");
+		tooltipObserver.observe(document.body, { childList: true });
 	}
 
-	function removeEndTimes(parent: Document | Element = document) {
-		[...parent.getElementsByClassName("tt-tooltip-end-times")].forEach((x) => x.remove());
+	async function removeEndTimes() {
+		[...document.getElementsByClassName("tt-tooltip-end-times")].forEach((x) => x.remove());
+		tooltipObserver.disconnect();
 	}
 })();
