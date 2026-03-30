8dfb9a7ac41deceae10946c86dfc6eeaac00d699
Sashank999 <bandi.rao999@gmail.com>
2026-03-30 22:00:44 +0530

    Reset other changes and use CSS accent-color

--- extension/scripts/content/global/ttGlobal.ts
+++ extension/scripts/content/global/ttGlobal.ts
@@ -15,7 +15,6 @@
 			"--tt-theme-background",
 			settings.themes.containers !== "alternative" ? "var(--tt-background-green)" : "var(--tt-background-alternative)"
 		);
-		document.documentElement.style.setProperty("--tt-green-tick-url", "url(" + chrome.runtime.getURL("/resources/images/svg-icons/green-tick.svg") + ")");
 		storageListeners.settings.push((oldSettings) => {
 			if (!oldSettings || !oldSettings.themes || !settings || !settings.themes || oldSettings.themes.containers !== settings.themes.containers) {
 				document.documentElement.style.setProperty("--tt-theme-color", settings.themes.containers !== "alternative" ? "#fff" : "#acea00");
