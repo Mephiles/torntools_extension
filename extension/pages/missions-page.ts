97c01a62852738332974330809b3bf8ff2b58e90
Kobe De Peuter <development@kobedepeuter.be>
2026-04-28 12:23:01 +0200

    fix: fix mission page features not refreshing after an action

--- extension/pages/missions-page.ts
+++ extension/pages/missions-page.ts
@@ -2,25 +2,24 @@ import { addXHRListener, EVENT_CHANNELS, triggerCustomListener } from "@/utils/c
 
 export function setupMissionsPage() {
 	addXHRListener(async ({ detail: { page, xhr, ...detail } }) => {
-		if (!("uri" in detail)) return;
+		if (page !== "page" || !("uri" in detail)) return;
+
 		const { uri } = detail;
 
-		if (page === "loader" || page === "loader2") {
-			const params = new URLSearchParams(xhr.requestBody);
-			let sid = params.get("sid");
-			if (!sid && uri && (uri.sid || uri["?sid"])) sid = uri.sid || uri["?sid"];
+		const params = new URLSearchParams(xhr.requestBody);
+		let sid = params.get("sid");
+		if (!sid && uri && (uri.sid || uri["?sid"])) sid = uri.sid || uri["?sid"];
 
-			if (sid === "missionsRewards") {
-				new MutationObserver((_mutations, observer) => {
-					triggerCustomListener(EVENT_CHANNELS.MISSION_REWARDS);
-					observer.disconnect();
-				}).observe(document.querySelector("#viewMissionsRewardsContainer"), { childList: true });
-			} else if (sid === "missions" || sid === "completeContract" || sid === "acceptMission") {
-				new MutationObserver((_mutations, observer) => {
-					triggerCustomListener(EVENT_CHANNELS.MISSION_LOAD);
-					observer.disconnect();
-				}).observe(document.querySelector("#missionsMainContainer"), { childList: true });
-			}
+		if (sid === "missionsRewards") {
+			new MutationObserver((_mutations, observer) => {
+				triggerCustomListener(EVENT_CHANNELS.MISSION_REWARDS);
+				observer.disconnect();
+			}).observe(document.querySelector("#viewMissionsRewardsContainer"), { childList: true });
+		} else if (sid === "missions" || sid === "completeContract" || sid === "acceptMission") {
+			new MutationObserver((_mutations, observer) => {
+				triggerCustomListener(EVENT_CHANNELS.MISSION_LOAD);
+				observer.disconnect();
+			}).observe(document.querySelector("#missionsMainContainer"), { childList: true });
 		}
 	});
 }
