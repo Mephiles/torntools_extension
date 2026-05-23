ec0e500449ffb5faeebfc02eae621315aecaf46a
Kobe De Peuter <development@kobedepeuter.be>
2026-05-24 01:41:16 +0200

    feat: move the tos page to Svelte

--- extension/entrypoints/tos/tos.ts
+++ extension/entrypoints/tos/tos.ts
@@ -1,12 +1,4 @@
-import { loadDatabase, storageListeners } from "@/utils/common/data/database";
-import { getPageTheme } from "@/utils/common/functions/pages";
+import { mount } from "svelte";
+import TOS from "@/entrypoints/tos/TOS.svelte";
 
-(async () => {
-	await loadDatabase();
-
-	document.body.classList.add(getPageTheme());
-	storageListeners.settings.push(() => {
-		document.body.classList.remove("dark", "light");
-		document.body.classList.add(getPageTheme());
-	});
-})();
+mount(TOS, { target: document.getElementById("app")! });
