2d4ea5888eb9f97638ca3b24c930ac143ed5006e
Kobe De Peuter <development@kobedepeuter.be>
2026-06-08 22:23:29 +0200

    chore: cleanup code

--- src/userscripts/entries/hospital-filter/hospital-filter.user.ts
+++ src/userscripts/entries/hospital-filter/hospital-filter.user.ts
@@ -1,7 +1,5 @@
-import { setupFactionsPage } from "@common/pages/factions-page";
 import { setupHospitalPage } from "@common/pages/hospital-page";
 import { FEATURE_MANAGER } from "@common/utils/context";
-import FactionMemberFilterFeature from "@features/faction-member-filter/faction-member-filter";
 import type { Feature } from "@features/feature";
 import HospitalFilterFeature from "@features/hospital-filter/hospital-filter";
 import { registerUserscriptContext } from "@userscripts/runtime/script-context";
@@ -10,7 +8,7 @@ import { registerUserscriptContext } from "@userscripts/runtime/script-context";
 	await registerUserscriptContext("tt_fmf");
 
 	const feature: Feature = new HospitalFilterFeature();
-	await setupHospitalPage();
+	setupHospitalPage();
 
 	FEATURE_MANAGER.registerFeature(feature);
 })();
