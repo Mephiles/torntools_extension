9b37f59f93e1080d4a85af0254652fa9ed58c416
Kobe De Peuter <development@kobedepeuter.be>
2026-06-09 08:57:51 +0200

    feat: missions hints userscript

---
+++ src/userscripts/entries/mission-hints/metadata.ts
@@ -0,0 +1,11 @@
+import type { UserscriptMetadata } from "@userscripts/entries/userscript-metadata";
+
+const metadata: UserscriptMetadata = {
+	name: "Mission Hints",
+	description: "Provide you with the task of the mission before accepting, and sometimes also an additional hint.",
+	version: "1.0.0",
+	matches: ["https://*.torn.com/page.php?sid=missions*"],
+	runAt: "document-end",
+};
+
+export default metadata;
