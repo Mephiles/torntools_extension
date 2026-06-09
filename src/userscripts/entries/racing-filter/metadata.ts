b2cfa549b4b2bbbaba40807a456aab3765a3ea7c
Kobe De Peuter <development@kobedepeuter.be>
2026-06-09 21:49:30 +0200

    chore: bump script versions

--- src/userscripts/entries/racing-filter/metadata.ts
+++ src/userscripts/entries/racing-filter/metadata.ts
@@ -3,7 +3,7 @@ import type { UserscriptMetadata } from "@userscripts/entries/userscript-metadat
 const metadata: UserscriptMetadata = {
 	name: "Racing Filter",
 	description: "Filter the racing competition list.",
-	version: "1.0.0",
+	version: "1.0.1",
 	matches: ["https://*.torn.com/page.php?sid=racing*"],
 	runAt: "document-end",
 };
