93c2b26412bab95ea8cdec384c5c09c427e5655a
Kobe De Peuter <development@kobedepeuter.be>
2026-06-09 08:47:50 +0200

    fix: injecters broke in the extension

--- src/extension/entrypoints/xhr--inject.ts
+++ src/extension/entrypoints/xhr--inject.ts
@@ -1,7 +1,13 @@
+import { setScriptInjector } from "@common/utils/context";
 import { injectXhrListeners, RequestListenerInjector } from "@common/utils/functions/script-injector";
 
 // noinspection JSUnusedGlobalSymbols
 export default defineUnlistedScript(() => {
+	setScriptInjector({
+		getWindow: () => window,
+		injectXHR: () => {},
+		injectFetch: () => {},
+	});
 	new RequestListenerInjector(injectXhrListeners).inject();
 
 	console.log("Script Injected - XHR Interception");
