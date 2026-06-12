dfc43c6e1b80740b72d33231a1871ab8ea217e1f
Kobe De Peuter <development@kobedepeuter.be>
2026-06-12 15:30:16 +0200

    fix: correct connect list

--- src/userscripts/entries/can-energy/metadata.ts
+++ src/userscripts/entries/can-energy/metadata.ts
@@ -3,10 +3,10 @@ import type { UserscriptMetadata } from "@userscripts/entries/userscript-metadat
 const metadata: UserscriptMetadata = {
 	name: "Can Energy",
 	description: "Show the amount of energy a can would give.",
-	version: "1.1.1",
+	version: "1.1.2",
 	matches: ["https://*.torn.com/item.php*"],
 	runAt: "document-end",
-	connect: ["torntools.tornplayground.eu"],
+	connect: ["api.torn.com", "torntools.tornplayground.eu"],
 };
 
 export default metadata;
