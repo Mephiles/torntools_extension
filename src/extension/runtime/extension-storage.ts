43ba66b9080fefde747a80a91743cc9cfcd673f8
Kobe De Peuter <development@kobedepeuter.be>
2026-06-14 23:44:08 +0200

    fix: use async over Promise

--- src/extension/runtime/extension-storage.ts
+++ src/extension/runtime/extension-storage.ts
@@ -6,22 +6,16 @@ export class TTExtensionStorage extends TornToolsStorage {
 	get(): Promise<Database>;
 	get<K extends DatabaseKey>(key: K): Promise<Database[K]>;
 	get<K extends readonly DatabaseKey[]>(keys: K): Promise<{ [I in keyof K]: K[I] extends DatabaseKey ? Database[K[I]] : never }>;
-	get(key?: DatabaseKey | DatabaseKey[]) {
-		return new Promise(async (resolve) => {
-			if (Array.isArray(key)) {
-				const data = await browser.storage.local.get(key);
+	async get(key?: DatabaseKey | DatabaseKey[]) {
+		if (Array.isArray(key)) {
+			const data = await browser.storage.local.get(key);
 
-				resolve(key.map((i) => data[i]));
-			} else if (key) {
-				const data = await browser.storage.local.get([key]);
-
-				resolve(data[key]);
-			} else {
-				const data = await browser.storage.local.get(null);
-
-				resolve(data);
-			}
-		});
+			return key.map((i) => data[i]);
+		} else if (key) {
+			return (await browser.storage.local.get([key]))[key];
+		} else {
+			return browser.storage.local.get(null);
+		}
 	}
 
 	set(object: { [key: string]: any }) {
@@ -36,25 +30,19 @@ export class TTExtensionStorage extends TornToolsStorage {
 		return browser.storage.local.clear();
 	}
 
-	reset(key?: "attackHistory" | "stakeouts" | "factionStakeouts"): Promise<void> {
-		return new Promise(async (resolve) => {
-			if (["attackHistory", "stakeouts", "factionStakeouts"].includes(key)) {
-				await this.set({ [key]: getDefaultStorage(DEFAULT_STORAGE)[key] });
+	async reset(key?: "attackHistory" | "stakeouts" | "factionStakeouts"): Promise<void> {
+		if (["attackHistory", "stakeouts", "factionStakeouts"].includes(key)) {
+			await this.set({ [key]: getDefaultStorage(DEFAULT_STORAGE)[key] });
+		} else {
+			const apiKey = api ? api.torn.key : undefined;
 
-				resolve();
-			} else {
-				const apiKey = api ? api.torn.key : undefined;
+			await this.clear();
+			await this.set(getDefaultStorage(DEFAULT_STORAGE));
+			await this.change({ api: { torn: { key: apiKey } } });
 
-				await this.clear();
-				await this.set(getDefaultStorage(DEFAULT_STORAGE));
-				await this.change({ api: { torn: { key: apiKey } } });
-
-				console.log("Storage cleared");
-				console.log("New storage", await this.get());
-
-				resolve();
-			}
-		});
+			console.log("Storage cleared");
+			console.log("New storage", await this.get());
+		}
 	}
 
 	async getSize() {
