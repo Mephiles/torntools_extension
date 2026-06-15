12afac53f96a9b7fa9d0c902bc54c560cace3a09
Kobe De Peuter <development@kobedepeuter.be>
2026-06-15 22:26:19 +0200

    chore: bump script version

--- src/userscripts/entries/only-new-feed/metadata.ts
+++ src/userscripts/entries/only-new-feed/metadata.ts
@@ -3,7 +3,7 @@ import type { UserscriptMetadata } from "@userscripts/entries/userscript-metadat
 const metadata: UserscriptMetadata = {
 	name: "Only New Feed",
 	description: "Only show unread items in your forum feeds.",
-	version: "1.0.9",
+	version: "1.0.10",
 	matches: ["https://*.torn.com/forums.php*"],
 	runAt: "document-end",
 };
