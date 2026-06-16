e5e37206cce9e0286cda66198d9791b07c2821a5
Kobe De Peuter <development@kobedepeuter.be>
2026-06-16 22:29:13 +0200

    feat: candy happy userscript

---
+++ src/userscripts/entries/candy-happy/metadata.ts
@@ -0,0 +1,12 @@
+import type { UserscriptMetadata } from "@userscripts/entries/userscript-metadata";
+
+const metadata: UserscriptMetadata = {
+	name: "Candy Happy",
+	description: "Show the amount of happy candy would give.",
+	version: "1.0.0",
+	matches: ["https://*.torn.com/item.php*"],
+	runAt: "document-end",
+	connect: ["api.torn.com", "torntools.tornplayground.eu"],
+};
+
+export default metadata;
