8387f623ecec489c28769d60cc1252ab6ab29182
Kobe De Peuter <development@kobedepeuter.be>
2026-06-16 22:28:31 +0200

    fix: remove unused connect fields

--- src/userscripts/entries/drug-details/metadata.ts
+++ src/userscripts/entries/drug-details/metadata.ts
@@ -3,7 +3,7 @@ import type { UserscriptMetadata } from "@userscripts/entries/userscript-metadat
 const metadata: UserscriptMetadata = {
 	name: "Drug Details",
 	description: "Display the full details of drugs on various pages.",
-	version: "1.0.1",
+	version: "1.0.2",
 	matches: [
 		"https://*.torn.com/item.php*",
 		"https://*.torn.com/bazaar.php*",
@@ -13,7 +13,6 @@ const metadata: UserscriptMetadata = {
 		"https://*.torn.com/page.php?sid=ItemMarket*",
 	],
 	runAt: "document-end",
-	connect: ["torntools.tornplayground.eu"],
 };
 
 export default metadata;
