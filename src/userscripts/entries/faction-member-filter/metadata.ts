ae95f9698a8a1e85a5db43996889509ae54b279e
Manuel Cecchetto <manuel.cecchetto99@gmail.com>
2026-06-23 21:47:01 +0200

    fix: restore faction activity filters

--- src/userscripts/entries/faction-member-filter/metadata.ts
+++ src/userscripts/entries/faction-member-filter/metadata.ts
@@ -3,7 +3,7 @@ import type { UserscriptMetadata } from "@userscripts/entries/userscript-metadat
 const metadata: UserscriptMetadata = {
 	name: "Faction Member Filter",
 	description: "Filter the list of members in a faction.",
-	version: "1.0.1",
+	version: "1.0.2",
 	matches: ["https://*.torn.com/factions.php*"],
 	runAt: "document-end",
 };
