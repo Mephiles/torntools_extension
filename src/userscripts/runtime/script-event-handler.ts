a2bfea3c6d54ad3bc98d56a30fc7d1b76af23cd2
Kobe De Peuter <development@kobedepeuter.be>
2026-06-30 15:41:32 +0200

    fix: don't trigger the script events multiple times

--- src/userscripts/runtime/script-event-handler.ts
+++ src/userscripts/runtime/script-event-handler.ts
@@ -4,22 +4,10 @@ import type { CustomEventListener, EventHandler, EventPayloads } from "@common/u
 export const ScriptEventHandler: EventHandler & { eventRoot: EventTarget } = {
 	triggerEvent<T extends keyof EventPayloads>(channel: T, payload?: EventPayloads[T]) {
 		document.dispatchEvent(new CustomEvent<EventPayloads[T]>(`TT_${channel}`, { detail: payload }));
-		window.dispatchEvent(new CustomEvent<EventPayloads[T]>(`TT_${channel}`, { detail: payload }));
-		unsafeWindow.dispatchEvent(new CustomEvent<EventPayloads[T]>(`TT_${channel}`, { detail: payload }));
 	},
 
 	registerListener<T extends keyof EventPayloads>(channel: T, listener: CustomEventListener<T>) {
 		document.addEventListener(`TT_${channel}`, (event: Event) => {
-			if (!isCustomEvent<EventPayloads[T]>(event)) return;
-
-			listener(event.detail);
-		});
-		window.addEventListener(`TT_${channel}`, (event: Event) => {
-			if (!isCustomEvent<EventPayloads[T]>(event)) return;
-
-			listener(event.detail);
-		});
-		unsafeWindow.addEventListener(`TT_${channel}`, (event: Event) => {
 			if (!isCustomEvent<EventPayloads[T]>(event)) return;
 
 			listener(event.detail);
