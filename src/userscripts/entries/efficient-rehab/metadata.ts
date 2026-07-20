438e3799fc01ca8bfedd0282d2f03a968529a6e8
Kobe De Peuter <development@kobedepeuter.be>
2026-07-20 12:42:34 +0200

    chore: bump script version

--- src/userscripts/entries/efficient-rehab/metadata.ts
+++ src/userscripts/entries/efficient-rehab/metadata.ts
@@ -3,7 +3,7 @@ import type { UserscriptMetadata } from "@userscripts/entries/userscript-metadat
 const metadata: UserscriptMetadata = {
 	name: "Efficient Rehab",
 	description: "Select how many rehabs are efficient at your current level.",
-	version: "dev-0.0.1",
+	version: "1.0.0",
 	matches: ["https://*.torn.com/index.php?page=rehab*"],
 	runAt: "document-end",
 	connect: ["api.torn.com"],
