f1183b20ad31a8d1cbfce54f7a32265aba5be0b3
Kobe De Peuter <development@kobedepeuter.be>
2026-07-22 16:52:07 +0200

    fix: resolve landing time userscript not loading

--- src/userscripts/entries/landing-time/landing-time.user.ts
+++ src/userscripts/entries/landing-time/landing-time.user.ts
@@ -2,9 +2,11 @@ import { FEATURE_MANAGER } from "@common/utils/context";
 import type { Feature } from "@features/feature";
 import LandingTimeFeature from "@features/landing-time/landing-time";
 import { registerCoreUserscriptContext } from "@userscripts/runtime/context/script-core-context";
+import { registerDatabaseUserscriptContext } from "@userscripts/runtime/context/script-database-context";
 
 (async () => {
 	registerCoreUserscriptContext();
+	await registerDatabaseUserscriptContext("tt_lt");
 
 	const feature: Feature = new LandingTimeFeature();
 	FEATURE_MANAGER.registerFeature(feature);
