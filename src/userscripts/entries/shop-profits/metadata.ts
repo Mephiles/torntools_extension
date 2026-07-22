f2d2346a00aed4e314e847b00fa00fb0f871bf44
Kobe De Peuter <development@kobedepeuter.be>
2026-07-22 18:26:35 +0200

    chore: update userscript description

--- src/userscripts/entries/shop-profits/metadata.ts
+++ src/userscripts/entries/shop-profits/metadata.ts
@@ -2,7 +2,7 @@ import type { UserscriptMetadata } from "@userscripts/entries/userscript-metadat
 
 const metadata: UserscriptMetadata = {
 	name: "Shop Profits",
-	description: "Display the amount of profit per city item.",
+	description: "Display the amount of profit per city shop item.",
 	version: "1.0.0",
 	matches: ["https://*.torn.com/bigalgunshop.php*", "https://*.torn.com/shops.php*"],
 	runAt: "document-end",
