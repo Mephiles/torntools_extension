4cec521329af3d04482c8feee2264f21d11dc33d
Kobe De Peuter <development@kobedepeuter.be>
2026-07-27 15:53:35 +0200

    feat: create bank investment info userscript

---
+++ src/userscripts/entries/bank-investment-info/bank-investment-info.user.ts
@@ -0,0 +1,38 @@
+import { FEATURE_MANAGER } from "@common/utils/context";
+import { ttCache } from "@common/utils/data/cache";
+import { fetchData } from "@common/utils/functions/api-fetcher";
+import type { TornV1BankResponse } from "@common/utils/functions/api-v1.types";
+import { millisToNewDay } from "@common/utils/functions/torn";
+import BankInvestmentInfoFeature from "@features/bank-investment-info/bank-investment-info";
+import type { Feature } from "@features/feature";
+import { registerCoreUserscriptContext } from "@userscripts/runtime/context/script-core-context";
+import { registerDatabaseUserscriptContext } from "@userscripts/runtime/context/script-database-context";
+import { registerNetworkUserscriptContext } from "@userscripts/runtime/context/script-network-context";
+import { requiresAPIKey } from "@userscripts/runtime/script-fetch";
+
+(async () => {
+	registerCoreUserscriptContext();
+	await registerDatabaseUserscriptContext("tt_bii");
+	registerNetworkUserscriptContext();
+
+	const key = await requiresAPIKey();
+
+	await preFetchBankData(key);
+
+	const feature: Feature = new BankInvestmentInfoFeature();
+	FEATURE_MANAGER.registerFeature(feature);
+})();
+
+async function preFetchBankData(key: string) {
+	const cached = ttCache.get("bankInterest");
+	if (cached) return;
+
+	const data = await fetchData<TornV1BankResponse>("tornv2", {
+		section: "torn",
+		legacySelections: ["bank"],
+		key: key,
+		includeKey: true,
+	});
+
+	ttCache.set({ bankInterest: data.bank }, millisToNewDay());
+}
---
+++ src/userscripts/entries/bank-investment-info/metadata.ts
@@ -0,0 +1,12 @@
+import type { UserscriptMetadata } from "@userscripts/entries/userscript-metadata";
+
+const metadata: UserscriptMetadata = {
+	name: "Bank Investment Info",
+	description: "Display an overview of the bank profit per period.",
+	version: "1.0.0",
+	matches: ["https://*.torn.com/bank.php*"],
+	runAt: "document-end",
+	connect: ["api.torn.com"],
+};
+
+export default metadata;
