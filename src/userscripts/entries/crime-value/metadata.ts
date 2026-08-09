aa305d588ee4b48d26c1dc31d966e7752eb7a4ff
Kobe De Peuter <development@kobedepeuter.be>
2026-08-10 01:19:19 +0200

    feat: create crime value userscript

--- src/userscripts/entries/crime-value/metadata.ts
+++ src/userscripts/entries/crime-value/metadata.ts
@@ -1,9 +1,9 @@
 import type { UserscriptMetadata } from "@userscripts/entries/userscript-metadata";
 
 const metadata: UserscriptMetadata = {
-	name: "City Value",
+	name: "Crime Value",
 	description: "Display the value of the items part of the crime outcome.",
-	version: "1.0.0",
+	version: "1.0.1",
 	matches: ["https://*.torn.com/page.php?sid=crimes*"],
 	runAt: "document-end",
 	connect: ["torntools.tornplayground.eu"],
