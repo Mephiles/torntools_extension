9df48e2defe12c04e3b3cacc3f5ac702219be6af
Kobe De Peuter <development@kobedepeuter.be>
2026-08-12 08:44:36 +0200

    feat: special actions for faction quick items (#1097)

---
+++ src/common/features/quick-items/shared/quick-items-common.ts
@@ -0,0 +1,394 @@
+import { ITEM_RESOLVER, ttStorage } from "@common/utils/context.ts";
+import { settings } from "@common/utils/data/database.ts";
+import { createContainer } from "@common/utils/functions/containers.ts";
+import { elementBuilder, findAllElements, findParent, isElement, isHTMLElement, mobile, tablet } from "@common/utils/functions/dom.ts";
+import { formatTime } from "@common/utils/functions/formatting.ts";
+import { createSwipeSafeClickEvents } from "@common/utils/functions/gestures.ts";
+import { getEquipPosition, getItemEnergy, getUserEnergy, isEquipable } from "@common/utils/functions/torn.ts";
+import { PHEye, PHPlus, PHX } from "@common/utils/icons/phosphor-icons.ts";
+import { getSpecialAction, isSpecialAction, toggleSpecialQuickOptions } from "@features/quick-items/shared/special-actions.ts";
+import styles from "./quick-items-common.module.css";
+import "./quick-items-common.css";
+
+export type QuickItem = { id: QuickItemId };
+export type QuickItemId = number | string;
+
+export function parseQuickItemId(value: string): QuickItemId {
+	return Number.isNaN(parseInt(value)) ? value : parseInt(value);
+}
+
+export function buildResponseLinks(links: { title: string; url: string; class: string; attr: string }[]) {
+	const built = [elementBuilder({ type: "a", href: "#", class: "close-act t-blue h", text: "Close" })];
+	for (const link of links) {
+		built.push(
+			elementBuilder({
+				type: "a",
+				class: `t-blue h m-left10 ${link.class}`,
+				href: link.url,
+				text: link.title,
+				attributes: Object.fromEntries(
+					link.attr
+						.split(" ")
+						.filter((x) => !!x)
+						.map((x) => x.split("=")),
+				),
+			}),
+		);
+	}
+	return built;
+}
+
+export function formatResponseTimers(responseWrap: HTMLElement) {
+	for (const count of findAllElements(".counter-wrap", responseWrap)) {
+		count.classList.add("tt-modified");
+		count.textContent = formatTime({ seconds: parseInt(count.dataset.time) }, { type: "timer", daysToHours: true });
+	}
+}
+
+export interface QuickDragHandlerOptions {
+	containerId: string;
+	resolveQuickItem: (target: Element) => QuickItemId | null;
+	addQuickItem: (item: QuickItem, temporary?: boolean) => void;
+	saveQuickItems: () => Promise<void>;
+}
+
+export function createQuickDragHandlers(options: QuickDragHandlerOptions) {
+	const container = `#${options.containerId}`;
+
+	return {
+		onDragStart(event: DragEvent) {
+			event.dataTransfer.setData("text/plain", null);
+
+			setTimeout(() => {
+				document.querySelector(`${container} > main`).classList.add("drag-progress");
+				if (document.querySelector(`${container} .temp.item`) || !isElement(event.target)) return;
+
+				const id = options.resolveQuickItem(event.target);
+				if (id === null) return;
+
+				options.addQuickItem({ id }, true);
+			}, 10);
+		},
+		async onDragEnd() {
+			document.querySelector(`${container} .temp.item`)?.remove();
+			document.querySelector(`${container} > main`).classList.remove("drag-progress");
+
+			await options.saveQuickItems();
+		},
+	};
+}
+
+export function initialiseQuickItems() {
+	document.addEventListener("click", (event) => {
+		if (isElement(event.target) && event.target.classList.contains("close-act")) {
+			const responseWrap = findParent(event.target, { class: "response-wrap" });
+
+			if (responseWrap) responseWrap.style.display = "none";
+		}
+	});
+
+	setInterval(() => {
+		for (const timer of findAllElements(".counter-wrap.tt-modified")) {
+			const secondsLeft = Math.max(0, parseInt(timer.dataset.secondsLeft ?? timer.dataset.time) - 1);
+
+			timer.textContent = formatTime({ seconds: secondsLeft }, { type: "timer", daysToHours: true });
+			timer.dataset.secondsLeft = `${secondsLeft}`;
+		}
+	}, 1000);
+}
+
+export interface QuickItemsControllerOptions {
+	title: string;
+	containerClass?: string;
+	nextElement: () => HTMLElement;
+	getOverlayItems: () => HTMLElement[];
+	getSourceItems: () => HTMLElement[];
+	parseSourceItem: (element: HTMLElement) => QuickItem | null;
+	onEditToggle?: (editing: boolean) => void;
+	allowQuickItem: (id: QuickItemId, category: string | null) => boolean;
+	savedItems: () => QuickItem[];
+	storageKey: "items" | "factionItems";
+
+	// Temporary options
+	useQuickItem: (item: QuickItem, context: UseContext) => void | Promise<void>;
+	buildAdditionalItems?: (item: QuickItem, wrapper: HTMLElement) => void;
+}
+
+export interface UseContext {
+	itemWrap: HTMLElement;
+	responseWrap: HTMLElement;
+	innerContent: HTMLElement;
+}
+
+export function createQuickItemsController(options: QuickItemsControllerOptions) {
+	let controlledContainerElements:
+		| {
+				container: HTMLElement;
+				content: HTMLElement;
+				innerContent: HTMLElement;
+				responseWrap: HTMLElement;
+		  }
+		| undefined;
+	let isEditing = false;
+	let movingElement: Element | undefined;
+	const editListenerItems = new Set<HTMLElement>();
+
+	function create() {
+		const {
+			container,
+			content,
+			options: containerOptions,
+		} = createContainer(options.title, {
+			class: [options.containerClass, "tt-quick-items-common"],
+			nextElement: options.nextElement(),
+			allowDragging: true,
+			compact: true,
+		});
+
+		const innerContent = elementBuilder({ type: "div", class: ["inner-content", styles.quickItemsList] });
+		const responseWrap = elementBuilder({ type: "div", class: "response-wrap" });
+		content.append(innerContent, responseWrap);
+
+		containerOptions.append(
+			elementBuilder({
+				type: "div",
+				class: "option",
+				id: "edit-items-button",
+				children: [PHPlus(), "Edit"],
+				events: {
+					click: (event) => {
+						event.stopPropagation();
+						isEditing = (event.currentTarget as Element).classList.toggle("tt-overlay-item");
+
+						if (controlledContainerElements.content) {
+							findAllElements(".item", controlledContainerElements.content).forEach((item) => {
+								item.classList.toggle("tt-overlay-item", isEditing);
+								item.classList.toggle("removable", isEditing);
+							});
+							options.getOverlayItems().forEach((item) => item.classList.toggle("tt-overlay-item", isEditing));
+							options.onEditToggle?.(isEditing);
+							document.querySelector(".tt-overlay")?.classList.toggle("tt-hidden", !isEditing);
+							if (isEditing) attachEditListeners();
+							else detachEditListeners();
+						}
+					},
+				},
+			}),
+			elementBuilder({
+				type: "div",
+				class: "option",
+				id: "custom-actions-button",
+				children: [PHEye(), "Specials"],
+				events: {
+					click: (event) => {
+						event.stopPropagation();
+
+						toggleSpecialQuickOptions(content, addQuickItem, saveQuickItems);
+					},
+				},
+			}),
+		);
+
+		controlledContainerElements = { container, content, innerContent, responseWrap };
+
+		options.savedItems().forEach((item) => addQuickItem(item));
+
+		return { content, options: containerOptions };
+	}
+
+	function attachEditListeners() {
+		if (!isEditing) return;
+
+		const sourceItems = new Set(options.getSourceItems());
+		for (const item of editListenerItems) {
+			if (sourceItems.has(item)) continue;
+
+			item.removeEventListener("click", onItemClickQuickEdit);
+			editListenerItems.delete(item);
+		}
+
+		for (const item of sourceItems) {
+			item.addEventListener("click", onItemClickQuickEdit);
+			editListenerItems.add(item);
+		}
+	}
+
+	function detachEditListeners() {
+		for (const item of editListenerItems) item.removeEventListener("click", onItemClickQuickEdit);
+		editListenerItems.clear();
+	}
+
+	async function onItemClickQuickEdit(event: MouseEvent) {
+		if (!isEditing) return;
+
+		event.stopPropagation();
+		event.preventDefault();
+
+		if (!isHTMLElement(event.target)) return;
+
+		const item = options.parseSourceItem(event.target);
+
+		const itemElement = addQuickItem(item, false);
+		if (itemElement) itemElement.classList.add("tt-overlay-item", "removable");
+
+		await saveQuickItems();
+	}
+
+	function addQuickItem(item: QuickItem, temporary = false) {
+		const { id } = item;
+
+		if (controlledContainerElements.content.querySelector(`.item[data-id='${id}']`))
+			return controlledContainerElements.content.querySelector(`.item[data-id='${id}']`);
+		if (!options.allowQuickItem(id, typeof id === "number" ? ITEM_RESOLVER.getStaticItem(id)?.type : null)) return null;
+
+		const dataset: Record<string, any> = { id };
+		if (isSpecialAction(id)) {
+			const action = getSpecialAction(id);
+
+			dataset.action = action.name;
+		} else if (typeof id === "number" && isEquipable(id, ITEM_RESOLVER.getStaticItem(id)?.type)) {
+			dataset.equipPosition = getEquipPosition(id, ITEM_RESOLVER.getStaticItem(id)?.type);
+		}
+
+		const itemWrap = elementBuilder({
+			type: "div",
+			class: ["item", styles.quickItem, temporary ? "temp" : null],
+			dataset,
+			events: {
+				...createSwipeSafeClickEvents(async () => {
+					if (itemWrap.classList.contains("removable")) {
+						itemWrap.remove();
+						itemWrap.dispatchEvent(new Event("mouseout"));
+						closeIcon.dispatchEvent(new Event("mouseout"));
+						await saveQuickItems();
+						return;
+					}
+
+					if (
+						settings.pages.items.energyWarning &&
+						typeof id === "number" &&
+						["Drug", "Energy Drink"].includes(ITEM_RESOLVER.getStaticItem(id)?.type)
+					) {
+						const received = getItemEnergy(id);
+						if (received) {
+							const [current, max] = getUserEnergy();
+							if (current > max && received + current > 1000) {
+								if (!confirm("Are you sure to use this item ? It will get you to more than 1000E.")) return;
+							}
+						}
+					}
+
+					await options.useQuickItem(item, {
+						itemWrap,
+						responseWrap: controlledContainerElements.responseWrap,
+						innerContent: controlledContainerElements.innerContent,
+					});
+				}),
+				dragstart(event) {
+					if (!isElement(event.currentTarget)) return;
+
+					event.dataTransfer.effectAllowed = "move";
+					event.dataTransfer.setDragImage(event.currentTarget, 0, 0);
+
+					movingElement = event.currentTarget;
+				},
+				async dragend() {
+					movingElement.classList.remove("temp");
+					movingElement = undefined;
+
+					await saveQuickItems();
+				},
+				dragover(event) {
+					event.preventDefault();
+				},
+				dragenter(event) {
+					if (movingElement === event.currentTarget || !isElement(event.currentTarget)) return;
+
+					const children = Array.from(controlledContainerElements.innerContent.children);
+
+					if (children.indexOf(movingElement) > children.indexOf(event.currentTarget))
+						controlledContainerElements.innerContent.insertBefore(movingElement, event.currentTarget);
+					else if (event.currentTarget.nextElementSibling) {
+						controlledContainerElements.innerContent.insertBefore(movingElement, event.currentTarget.nextElementSibling);
+					} else {
+						controlledContainerElements.innerContent.appendChild(movingElement);
+					}
+					movingElement.classList.add("temp");
+				},
+			},
+			attributes: {
+				draggable: !(mobile || tablet),
+			},
+		});
+
+		buildItem(item, itemWrap);
+
+		const closeIcon = elementBuilder({
+			type: "div",
+			class: "tt-close-icon",
+			children: [PHX()],
+			attributes: { title: "Remove quick access." },
+			events: createSwipeSafeClickEvents(async (event) => {
+				event.stopPropagation();
+				itemWrap.dispatchEvent(new Event("mouseout"));
+				closeIcon.dispatchEvent(new Event("mouseout"));
+				itemWrap.remove();
+
+				await saveQuickItems();
+			}),
+		});
+		itemWrap.appendChild(closeIcon);
+		controlledContainerElements.innerContent.appendChild(itemWrap);
+		return itemWrap;
+	}
+
+	function buildItem(item: QuickItem, wrapper: HTMLElement) {
+		if (typeof item.id === "number") {
+			wrapper.appendChild(
+				elementBuilder({ type: "div", class: "pic", attributes: { style: `background-image: url(/images/items/${item.id}/medium.png)` } }),
+			);
+
+			const staticItem = ITEM_RESOLVER.getStaticItem(item.id);
+			if (staticItem) {
+				wrapper.setAttribute("title", staticItem.name);
+				wrapper.appendChild(elementBuilder({ type: "div", class: styles.name, text: staticItem.name }));
+			} else {
+				wrapper.appendChild(elementBuilder({ type: "div", class: styles.name, text: item.id }));
+			}
+		} else if (isSpecialAction(item.id)) {
+			const specialAction = getSpecialAction(item.id);
+
+			wrapper.setAttribute("title", specialAction.name);
+			wrapper.appendChild(elementBuilder({ type: "div", class: styles.name, text: specialAction.name }));
+		} else if (options.buildAdditionalItems) {
+			options.buildAdditionalItems(item, wrapper);
+		} else {
+			throw new Error("Failed to build the quick item due to a missing additional item builder.");
+		}
+	}
+
+	async function saveQuickItems() {
+		await ttStorage.change({
+			quick: {
+				[options.storageKey]: findAllElements(".item", controlledContainerElements.innerContent)
+					.map((x) => x.dataset.id)
+					.map<QuickItemId>(parseQuickItemId)
+					.map<QuickItem>((x) => ({ id: x })),
+			},
+		});
+	}
+
+	function dispose() {
+		detachEditListeners();
+		controlledContainerElements?.container.remove();
+		controlledContainerElements = undefined;
+	}
+
+	return {
+		create,
+		refreshEditListeners: () => attachEditListeners(),
+		dispose,
+		addQuickItem,
+		saveQuickItems,
+	};
+}
---
+++ src/userscripts/entries/faction-quick-items/metadata.ts
@@ -0,0 +1,12 @@
+import type { UserscriptMetadata } from "@userscripts/entries/userscript-metadata.ts";
+
+const metadata: UserscriptMetadata = {
+	name: "Faction Quick Items",
+	description: "Use items from the faction armory faster.",
+	version: "1.0.0",
+	matches: ["https://*.torn.com/factions.php*"],
+	runAt: "document-end",
+	connect: ["torntools.tornplayground.eu"],
+};
+
+export default metadata;
