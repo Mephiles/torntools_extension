43eaf4e46ccdc81485ee5a95155bc8c09943886a
Kobe De Peuter <development@kobedepeuter.be>
2026-08-19 08:41:14 +0200

    fix: fetch company profile and correctly handle empoyee information

--- src/common/features/last-action/last-action-company.ts
+++ src/common/features/last-action/last-action-company.ts
@@ -12,7 +12,7 @@ import { requireElement } from "@common/utils/functions/requires";
 import { getUsername } from "@common/utils/functions/torn";
 import { TO_MILLIS } from "@common/utils/functions/utilities";
 import { Feature } from "@features/feature";
-import type { CompanyEmployee, CompanyEmployeesResponse, UserJobResponse } from "tornapi-typescript";
+import type { CompanyEmployeesResponse, CompanyProfileResponse, UserJobResponse } from "tornapi-typescript";
 
 function addListener() {
 	addCustomListener(EVENT_CHANNELS.COMPANY_EMPLOYEES_PAGE, async () => {
@@ -21,6 +21,8 @@ function addListener() {
 		await addLastAction(isOwnCompany);
 	});
 }
+
+type FetchedCompany = CompanyEmployeesResponse & CompanyProfileResponse;
 
 async function addLastAction(force: boolean) {
 	if (isOwnCompany && getHashParameters().get("option") !== "employees" && !force) return;
@@ -32,39 +34,61 @@ async function addLastAction(force: boolean) {
 
 	const id = await extractCompanyId();
 
-	let employees: CompanyEmployee[];
-	if (ttCache.hasValue("company-employees", id)) {
-		employees = ttCache.get("company-employees", id);
+	let company: FetchedCompany;
+	if (ttCache.hasValue("company", id)) {
+		company = ttCache.get("company", id);
 	} else {
-		employees = (
-			await fetchData<CompanyEmployeesResponse>("tornv2", {
-				section: "company",
-				id: id,
-				selections: ["employees"],
-				silent: true,
-			})
-		).employees;
+		company = await fetchData<FetchedCompany>("tornv2", { section: "company", id: id, selections: ["employees", "profile"], silent: true });
 
-		ttCache.set({ [id]: employees }, TO_MILLIS.SECONDS * 30, "company-employees");
+		ttCache.set({ [id]: company }, TO_MILLIS.SECONDS * 30, "company-employees");
 	}
 
 	const now = Date.now();
 	const list = document.querySelector(".employee-list-wrap .employee-list, .employees-wrap .employees-list");
 	for (const row of findAllElements(":scope > li", list)) {
 		const { id } = getUsername(row);
-		const days = dropDecimals((now - employees[id].last_action.timestamp * 1000) / TO_MILLIS.DAYS);
+
+		const employee = resolveEmployee(id, company);
+		if (!employee) {
+			console.warn(`TT - Couldn't find the employee information for ${id}!`, company);
+			continue;
+		}
+
+		const days = dropDecimals((now - employee.last_action.timestamp) / TO_MILLIS.DAYS);
 
 		row.insertAdjacentElement(
 			"afterend",
 			elementBuilder({
 				type: "div",
 				class: `tt-last-action ${isOwnCompany ? "" : "joblist"}`,
-				text: `Last action: ${employees[id].last_action.relative}`,
+				text: `Last action: ${employee.last_action.relative}`,
 				dataset: { days },
 			}),
 		);
 	}
 	list.classList.add("tt-modified");
+}
+
+interface ResolvedEmployee {
+	id: number;
+	last_action: {
+		timestamp: number;
+		relative: string;
+	};
+}
+
+function resolveEmployee(id: number, company: FetchedCompany): ResolvedEmployee | null {
+	if (id === company.profile.director.id) {
+		return {
+			id,
+			last_action: { timestamp: company.profile.director.last_action.timestamp * 1000, relative: company.profile.director.last_action.relative },
+		};
+	}
+
+	const employee = company.employees.find((e) => e.id === id);
+	if (!employee) return null;
+
+	return { id, last_action: { timestamp: employee.last_action.timestamp * 1000, relative: employee.last_action.relative } };
 }
 
 async function extractCompanyId(): Promise<number> {
