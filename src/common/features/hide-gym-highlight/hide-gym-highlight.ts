b5b9d8d5ae313e6e4f35ee70865a7e1ae0761886
Kobe De Peuter <development@kobedepeuter.be>
2026-08-21 11:39:22 +0200

    fix: hide gym and newspaper highlights no longer worked after the flyout-sidebar

--- src/common/features/hide-gym-highlight/hide-gym-highlight.ts
+++ src/common/features/hide-gym-highlight/hide-gym-highlight.ts
@@ -1,69 +1,14 @@
 import "./hide-gym-highlight.css";
 import { settings } from "@common/utils/data/database";
-import { hasSidebar, mobile, tablet } from "@common/utils/functions/dom";
-import { requireSidebar } from "@common/utils/functions/requires";
-import { hasDarkMode, isPageWithSidebar } from "@common/utils/functions/torn";
+import { isPageWithSidebar } from "@common/utils/functions/torn";
 import { Feature } from "@features/feature";
 
-let hadHighlight = false;
-
-async function hideGymHighlight() {
-	await requireSidebar();
-
-	const navGym = document.querySelector("#nav-gym, #nav-jail_gym");
-	if (!navGym) return;
-
-	hadHighlight = Array.from(navGym.classList).some((c) => c.startsWith("available___"));
-	if (!hadHighlight) return;
-
-	const svg = navGym.querySelector("svg");
-	if (hasDarkMode()) {
-		if (!hasSidebar) {
-			svg.setAttribute("fill", svg.getAttribute("fill").replace("_green", ""));
-			svg.setAttribute("filter", svg.getAttribute("filter").replace("_green", ""));
-		} else {
-			svg.setAttribute("fill", "url(#sidebar_svg_gradient_regular_mobile)");
-			svg.setAttribute("filter", "url(#svg_sidebar_mobile)");
-		}
-	} else {
-		if (mobile || tablet) {
-			svg.setAttribute("fill", "url(#sidebar_svg_gradient_regular_mobile)");
-			svg.setAttribute("filter", "url(#svg_sidebar_mobile)");
-		} else {
-			svg.setAttribute("fill", "url(#sidebar_svg_gradient_regular_desktop)");
-		}
-	}
-
-	const availableClass = Array.from(navGym.classList).find((className) => className.startsWith("available___"));
-	if (availableClass) navGym.classList.remove(availableClass);
+function hideGymHighlight() {
+	document.documentElement.classList.add("tt-hide-gym-highlight");
 }
 
 function removeHiddenHighlight() {
-	if (!hadHighlight) return;
-
-	const navGym = document.querySelector("#nav-gym, #nav-jail_gym");
-	if (!navGym) return;
-
-	const svg = navGym.querySelector("svg");
-
-	if (hasDarkMode()) {
-		if (mobile || tablet) {
-			svg.setAttribute("fill", "url(#sidebar_svg_gradient_regular_green_mobile)");
-			svg.setAttribute("filter", "url(#svg_sidebar_green_regular_mobile_green_filter)");
-		} else {
-			svg.setAttribute("fill", "url(#sidebar_svg_gradient_regular_green_mobile)");
-			svg.setAttribute("filter", "url(#svg_sidebar_green_mobile)");
-		}
-	} else {
-		if (mobile || tablet) {
-			svg.setAttribute("fill", "url(#sidebar_svg_gradient_regular_green_mobile)");
-			svg.setAttribute("filter", "url(#svg_sidebar_green_regular_mobile_green_filter)");
-		} else {
-			svg.setAttribute("fill", "url(#sidebar_svg_gradient_regular_desktop_green)");
-		}
-	}
-
-	navGym.querySelector("[class*='area-row___']").classList.add("tt-available");
+	document.documentElement.classList.remove("tt-hide-gym-highlight");
 }
 
 export default class HideGymHighlightFeature extends Feature {
@@ -79,8 +24,8 @@ export default class HideGymHighlightFeature extends Feature {
 		return settings.pages.sidebar.hideGymHighlight;
 	}
 
-	async execute() {
-		await hideGymHighlight();
+	execute() {
+		hideGymHighlight();
 	}
 
 	cleanup() {
