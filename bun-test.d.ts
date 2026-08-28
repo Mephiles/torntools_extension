30580b02ae57e979f3312b56c0689c460e76095d
Kobe De Peuter <development@kobedepeuter.be>
2026-08-29 00:38:54 +0200

    chore: exclude bun global runtime types

---
+++ bun-test.d.ts
@@ -0,0 +1,2 @@
+// Provides the `bun:test` module types for *.test.ts files without including the bun runtime globals, as we aren't running in a bun environment.
+/// <reference path="node_modules/bun-types/test.d.ts" />
