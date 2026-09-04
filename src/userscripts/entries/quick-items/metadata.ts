5d49545f0f6f8defd04995d58a09552b6eb54729
Kobe De Peuter <development@kobedepeuter.be>
2026-09-04 21:28:31 +0200

    chore: don't release script version, as it requires a setting which can't be set for now

--- src/userscripts/entries/quick-items/metadata.ts
+++ src/userscripts/entries/quick-items/metadata.ts
@@ -3,7 +3,7 @@ import type { UserscriptMetadata } from "@userscripts/entries/userscript-metadat
 const metadata: UserscriptMetadata = {
 	name: "Quick Items",
 	description: "Use your items faster.",
-	version: "1.0.2",
+	version: "1.0.1",
 	matches: ["https://*.torn.com/item.php*"],
 	runAt: "document-end",
 	connect: ["torntools.tornplayground.eu"],
