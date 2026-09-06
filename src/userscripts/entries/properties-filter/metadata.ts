80c9e6d6a5614654b8b8de87f492907f490ab4f8
Kobe De Peuter <development@kobedepeuter.be>
2026-09-06 03:41:40 +0200

    chore: bump script version after a minor refactor

--- src/userscripts/entries/properties-filter/metadata.ts
+++ src/userscripts/entries/properties-filter/metadata.ts
@@ -3,7 +3,7 @@ import type { UserscriptMetadata } from "@userscripts/entries/userscript-metadat
 const metadata: UserscriptMetadata = {
 	name: "Properties Filter",
 	description: "Filter the properties you and your spouse own.",
-	version: "1.0.7",
+	version: "1.0.8",
 	matches: ["https://*.torn.com/properties.php*"],
 	runAt: "document-end",
 };
--- src/userscripts/entries/target-filter/metadata.ts
+++ src/userscripts/entries/target-filter/metadata.ts
@@ -3,7 +3,7 @@ import type { UserscriptMetadata } from "@userscripts/entries/userscript-metadat
 const metadata: UserscriptMetadata = {
 	name: "Target Filter",
 	description: "Filter the target list.",
-	version: "1.0.6",
+	version: "1.0.7",
 	matches: ["https://*.torn.com/page.php?sid=list&type=targets*"],
 	runAt: "document-end",
 };
